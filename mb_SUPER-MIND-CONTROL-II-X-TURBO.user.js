2728ad3e87750f780833330580350e126c420d6b
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2014-07-11 15:14:39 +0200

    FIX. DOUBLE_CLICK_SUBMIT doesn’t work on edit-cover-art page

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,7 +1,7 @@
 (function(){"use strict";var meta={rawmdb:function(){
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2014.7.9.1322
+// @version      2014.7.11.1432
 // @description  musicbrainz.org power-ups (mbsandbox.org too): RELEASE_CLONER / DOUBLE_CLICK_SUBMIT / POWER_RELATE_TO / RELEASE_EDITOR_PROTECTOR / TRACKLIST_TOOLS / ALIAS_SORT_NAME / LAST_SEEN_EDIT / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_SWITCH / USER_STATS / MAX_RECENT_ENTITIES / RETURN_TO_MB_PROPERLY / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE / STATIC_MENU / MERGE_USER_MENUS / SLOW_DOWN_RETRY / CENTER_FLAGS
 // @doc          http://userscripts.org:8080/scripts/show/85790
 // @doc          http://userscripts-mirror.org/scripts/show/85790
@@ -656,7 +656,7 @@
 	## DOUBLE_CLICK_SUBMIT ##
 	==========================================================================*/
 	j2setting("DOUBLE_CLICK_SUBMIT", true, true, "makes the “radio buttons” and “multi-selects” submit forms on double-click (MBS-3229)");
-	if (j2sets.DOUBLE_CLICK_SUBMIT && self.location.pathname.match(/^\/(cdtoc\/|cdstub\/|edit\/|release\/(add(\?release-group=.+)?|[^/+]\/edit)|search|.+\/merge)/)) {
+	if (j2sets.DOUBLE_CLICK_SUBMIT && self.location.pathname.match(/^\/(cdtoc\/|cdstub\/|edit\/|release\/(add(\?release-group=)?|[^/]+\/edit-cover-art\/)|search|.+\/merge)/)) {
 		var objs = document.querySelectorAll("div#page form > *:not(.edit-list) input[type='radio'], select[multiple]");
 		for (var o=0; o < objs.length; o++) {
 			var obj = getParent(objs[o], "label") || objs[o];
@@ -664,7 +664,7 @@
 			obj.addEventListener("dblclick", function(e) {
 				var form = getParent(this, "form");
 				if (form) {
-					var submitbutt = form.querySelector("div#release-editor input#id-next[type='submit'],button.submit.positive,span.buttons > button[type='submit']");
+					var submitbutt = form.querySelector("div#release-editor input#id-next[type='submit'], div.buttons > button[type='submit'], span.buttons > button[type='submit']");
 					if (submitbutt) {
 						submitbutt.style.setProperty("background-color", "yellow");
 						submitbutt.click();
