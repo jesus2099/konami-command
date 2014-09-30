5a759983a12ddea1e7609d4a4672b40f1b4527a2
文科 <wenketel@users.noreply.github.com>
2014-10-01 01:00:52 +0800

    fix

--- greasyfork_APPLIES-TO-LINKS-SEARCH-BY-SITES.user.js
+++ greasyfork_APPLIES-TO-LINKS-SEARCH-BY-SITES.user.js
@@ -1,16 +1,15 @@
 // ==UserScript==
 // @name         greasyfork. APPLIES TO LINKS SEARCH BY SITES
-// @version      2014.6.30.1213
+// @version      2014.9.30.1213
 // @description  greasyfork.org. makes applies to “All site” link to the “*” by site search (JasonBarnabe/greasyfork#146)
 // @namespace    https://github.com/jesus2099/konami-command
-// @downloadURL  https://raw.githubusercontent.com/jesus2099/konami-command/master/greasyfork_APPLIES-TO-LINKS-SEARCH-BY-SITES.user.js
-// @updateURL    https://raw.githubusercontent.com/jesus2099/konami-command/master/greasyfork_APPLIES-TO-LINKS-SEARCH-BY-SITES.user.js
 // @author       PATATE12 aka. jesus2099/shamo
 // @licence      CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/)
-// @since        2014.6.6.
+// @since        2014.9.30.
 // @grant        none
 // @include      https://greasyfork.org/scripts/*
-// @include      https://www.greasyfork.org/scripts/*
+// @include      https://greasyfork.org/*/scripts/*
+// @include      https://www.greasyfork.org/*/scripts/*
 // @exclude      *greasyfork.org/scripts/*/*
 // @exclude      *greasyfork.org/scripts/?
 // @exclude      *greasyfork.org/scripts/by*
@@ -20,7 +19,7 @@
 	var appliesto = document.querySelector("div#script-meta dd.script-show-applies-to");
 	if (appliesto) {
 		var at = appliesto.textContent.trim();
-		if (at.match(/^all sites$/i)) {
+		if (at.match(/^all sites|Alle Seiten|Todos los sitios|Tous les sites|Semua situs|すべてのサイト|Alle websites|Wszystkie strony|Todos sites|Все сайты|所有站点|所有網站$/i)) {
 			appliesto.replaceChild(a(at, "*"), appliesto.firstChild);
 		}
 	}
