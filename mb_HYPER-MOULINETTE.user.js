c1197225c1ebaefc8ec438a73bc0bca3af1fef26
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2014-10-17 17:16:42 +0200

    NEW. displays the release it is adding/removing in the logs. menu placed in more convenient place (YOUR collections page) same place as COLLECTION HIGHLIGHTER, and stop asking for the obvious.

--- mb_HYPER-MOULINETTE.user.js
+++ mb_HYPER-MOULINETTE.user.js
@@ -1,8 +1,8 @@
 (function(){"use strict";var meta={rawmdb:function(){
 // ==UserScript==
 // @name         mb. HYPER MOULINETTE
-// @version      2014.10.10.1909
-// @description  musicbrainz.org: browses pages of MB content to perform some actions on spotted entities
+// @version      2014.10.17.1707
+// @description  musicbrainz.org: Mass PUT or DELETE releases in a collection from an edit search or an other collection
 // @namespace    https://github.com/jesus2099/konami-command
 // @downloadURL  https://raw.githubusercontent.com/jesus2099/konami-command/master/mb_HYPER-MOULINETTE.user.js
 // @updateURL    https://raw.githubusercontent.com/jesus2099/konami-command/master/mb_HYPER-MOULINETTE.user.js
@@ -10,18 +10,10 @@
 // @licence      CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/)
 // @since        2014.9.19
 // @grant        none
-// @include      http*://*musicbrainz.org/*
-// @include      http://*.mbsandbox.org/*
+// @include      http*://*musicbrainz.org/user/*/collections
+// @include      http://*.mbsandbox.org/user/*/collections
 // @exclude      *//*/*mbsandbox.org*
 // @exclude      *//*/*musicbrainz.org*
-// @exclude      *blog.musicbrainz.org*
-// @exclude      *bugs.musicbrainz.org*
-// @exclude      *chatlogs.musicbrainz.org*
-// @exclude      *forums.musicbrainz.org*
-// @exclude      *geordi.musicbrainz.org*
-// @exclude      *musicbrainz.org/ws*
-// @exclude      *tickets.musicbrainz.org*
-// @exclude      *wiki.musicbrainz.org*
 // @run-at       document-end
 // ==/UserScript==
 	}};
@@ -40,60 +32,38 @@
 	var stre_GUID = "[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}";
 	var re_GUID = new RegExp(stre_GUID);
 	var account = document.querySelector("div#header-menu li.account");
-	var target, action, source, client, loaders = [];
+	var target, method, source, client, loaders = [];
 	var crawlType = {
 		"^/collection/": "div#content table.tbl a[href*='/release/']",
 		"^/search/edits": "div.edit-details a[href*='/release/']",
 	};
 	var genuineTitle = document.title;
-	if (!self.opera && account) {
-		document.head.appendChild(document.createElement("style")).setAttribute("type", "text/css");
-		/*==========================================================================
-		## MENU ITEM ##
-		find this script in MB "Editing" menu
-		==========================================================================*/
-		var j2hypermoulinette = {
-			css: document.styleSheets[document.styleSheets.length-1],
-			menu: {
-				addItem: function(item) {
-					j2hypermoulinette.menu.lastItem = addAfter(createTag("li", {a:{"class":"jesus2099"}}, item), j2hypermoulinette.menu.getLastItem());
-				},
-				getLastItem: function() {
-					if (j2hypermoulinette.menu.lastItem) return j2hypermoulinette.menu.lastItem;
-					else {
-						var head, MBmenu = document.querySelector("div#header-menu li.editing > ul") || document.querySelector("div#header-menu li.about > ul");
-						if (MBmenu && (head = MBmenu.parentNode.querySelector("a"))) {
-							j2hypermoulinette.menu.lastItem = MBmenu.appendChild(createTag("li", {a:{"class":"jesus2099 separator"}}));
-							return j2hypermoulinette.menu.lastItem;
-						}
-					}
-				},
-			},
-		};
-		j2hypermoulinette.menu.addItem(createTag("a", {e:{click:mouli}}, "HYPER MOULINETTE™"));
+	/*==========================================================================
+	## MENU ITEM ##
+	find this script in MB "Editing" menu
+	==========================================================================*/
+	if (self.location.href.match(/\/collections/) && document.querySelector("h1").textContent == account.querySelector("a").textContent) {
+		var hdr = document.querySelector("table.tbl > thead > tr").appendChild(createTag("th", {a:{"class":"hypermouli-header"}}, [meta.name.replace(/.+\. /, "")+(self.opera?" (does not work in Opera!)":""),createTag("br"),meta.description.replace(/.+:/, "")]));
+		var colls = document.querySelectorAll("table.tbl > tbody > tr");
+		for (var c=0; c<colls.length; c++) {
+			colls[c].appendChild(createTag("td", {}, [createTag("a", {e:{click:mouli}}, "Put"), " | ", createTag("a", {e:{click:mouli}}, "Delete")]));
+		}
 	}
 	/*==========================================================================
 	## THE BIG ONE ## goes here
 	==========================================================================*/
 	function mouli() {
-		this.parentNode.parentNode.style.removeProperty("left");
-		target = prompt("This allows to batch PUT/DELETE releases from an edit search into a collection.\n\nPlease paste the “TARGET” collection URL or MBID.\nThis is the collection that will be modified (either add or remove releases).", localStorage.getItem(meta.key+"target")||"");
-		action = prompt("Please tell if you want to PUT or DELETE releases in this collection.", localStorage.getItem(meta.key+"method")||"put");
-		source = prompt("Please paste your “SOURCE” (release edit search, another collection) URL here.\nIt will parse these pages to modify the previously mentionned collection.", localStorage.getItem(meta.key+"source")||"");
+		target = this.parentNode.parentNode.querySelector("a[href*='/collection/']").getAttribute("href").match(re_GUID);
+		method = this.textContent.toLowerCase();
+		source = prompt("Please paste your release edit search or other collection URL here.\nIt will parse these pages to modify the previously mentionned collection.", localStorage.getItem(meta.key+"source")||"");
 		client = meta.name.replace(/^mb\. /, "").replace(/ /g, ".").toLowerCase()+"-"+meta.version;
-		if (
-			target && (target = target.match(re_GUID)) &&
-			action && (action.match(/^(put|delete)$/i)) &&
-			source
-		) {
-			localStorage.setItem(meta.key+"target", target);
-			localStorage.setItem(meta.key+"method", action);
+		if (target && method.match(/^(put|delete)$/i) && source) {
 			localStorage.setItem(meta.key+"source", source);
 			source = source.replace(/^(https?:\/\/[^\/]+)?(\/.+)/, "$2");
-			modal(createTag("fragment", {}, [createTag("a", {a:{href:meta.namespace, target:"_blank"}}, meta.name), " ", createTag("b", {}, meta.version), " (reload this page to abort)"]), 2);
+			modal(createTag("h3", {}, [createTag("a", {a:{href:meta.namespace, target:"_blank"}}, meta.name), " ", createTag("b", {}, meta.version), " (reload this page to abort)"]));
 			loadForExtract(source.replace(/([\?&])page=\d+&*/g, "$1")+(source.match(/\?/)?"&":"?")+"page=1");
 		} else {
-			alert("syntax error\ntarget: "+target+"\naction: "+action+" (should be either “put” or “delete”)\nsource: "+source);
+			alert("syntax error\ntarget: "+target+"\nmethod: "+method+" (should be either “put” or “delete”)\nsource: "+source);
 		}
 	}
 	function loadForExtract(page) {
@@ -101,18 +71,22 @@
 		loaders[xhr.getID()] = {maxRetry:5, url:page};
 		xhr.addEventListener("load", function(e) {
 			var p = parseInt(loaders[this.getID()].url.match(/page=(\d+)$/)[1], 10);
-			modal(createTag("fragment", {}, [createTag("a", {a:{href:loaders[this.getID()].url, target:"_blank"}}, "Page "+p), " loaded."]));
+			var ploaded = modal(createTag("h4", {}, [createTag("a", {a:{href:loaders[this.getID()].url, target:"_blank"}}, "Page "+p), " loaded"]));
 			document.title = "Page "+p+" loaded (HYPER MOULINETTE) "+genuineTitle;
 			var res = document.createElement("html"); res.innerHTML = this.responseText;
 			var releases;
 			for (var type in crawlType) if (crawlType.hasOwnProperty(type) && loaders[this.getID()].url.match(new RegExp(type))) {
 				releases = res.querySelectorAll(crawlType[type]);/*crawlTypes*/
+				ploaded.appendChild(document.createTextNode(" ("+releases.length+" release"+(releases.length==1?"":"s")+"):"));
 				if (releases.length > 0) {
 					var url = "/ws/2/collection/"+target+"/releases/";
+					var ol = modal(createTag("ol"));
 					for (var r=0; r < releases.length; r++) {
-						url += (r==0?"":";")+releases[r].getAttribute("href").match(re_GUID);
+						var guid = releases[r].getAttribute("href").match(re_GUID);
+						ol.appendChild(createTag("li", {}, createTag("a", {a:{href:releases[r].getAttribute("href"),target:"_blank"}}, [createTag("img", {a:{src:"//coverartarchive.org/release/"+guid+"/front-250",height:"12px",width:"12px"},s:{"box-shadow":"1px 1px 4px black"},e:{error:function(){this.parentNode.replaceChild(createTag("span", {s:{"background-color":"#ccc",display:"inline-block",width:"12px"}}, " "), this);}}})," "+releases[r].textContent])));
+						url += (r==0?"":";")+guid;
 					}
-					requestForAction(action, url+"?client="+client);
+					requestForAction(method, url+"?client="+client);
 				}
 				break;
 			}
@@ -124,8 +98,7 @@
 				loadForExtract(page.replace(/(page=)\d+$/, "$1"+(p+1)));
 			} else {
 				document.title = genuineTitle;
-				alert("Last page processed.");
-				modal();
+				modal(createTag("h3", {}, ["Last page processed (", createTag("a",{e:{click:function(){location.reload()}}},"RELOAD"), " page to quit this crap)."]));
 			}
 		});
 		xhr.addEventListener("error", function(e) {
@@ -140,37 +113,40 @@
 		xhr.sendDebug(null);
 	}
 	function requestForAction(method, url) {
-		var xhr = new XMLHttpRequest();
-		loaders[xhr.getID()] = {method:method, url:url, maxRetry:5};
-		xhr.addEventListener("load", function(e) {
-			var node, res = this.responseXML.documentElement;
-			var msg = createTag("fragment", {}, ["Releases “", createTag("a", {a:{title:loaders[this.getID()].method, href:loaders[this.getID()].url, target:"_blank"}}, loaders[this.getID()].method), "” on collection "]);
-			if ((node = res.querySelector("message text")) && node.textContent.match(/ok/i)) {
-				modal(createTag("fragment", {}, [msg, "OK."]));
-			} else {
-				modal(createTag("fragment", {}, [msg, "failed.\n\n"+res.textContent]));
-			}
-		});
-		xhr.addEventListener("error", function(e) {
-			if (--loaders[this.getID()].maxRetry > 0) {
-				modal(createTag("fragment", {}, ["Error performing ", createTag("a", {a:{title:loaders[this.getID()].method, href:loaders[this.getID()].url, target:"_blank"}}, "“"+loaders[this.getID()].method+"” action"), ", retrying…"]));
-				loadForAction(loaders[this.getID()].method, loaders[this.getID()].url);
-			} else {
-				alert("XHR-"+this.getID()+" ERROR "+this.status+"\nStopped retrying.\n"+loaders[this.getID].url+"\n\n"+this.responseText);
-			}
-		});
-		xhr.openDebug(method, url);
-		xhr.overrideMimeType("text/xml");
-		xhr.sendDebug(null);
+		if (self.opera) { modal(createTag("p", {}, ["Will not perform ",createTag("a", {a:{href:url,target:"_blank"}}, method)," (auth-digest does not work in Opera)."])); }
+		else {
+			var xhr = new XMLHttpRequest();
+			loaders[xhr.getID()] = {method:method, url:url, maxRetry:5};
+			xhr.addEventListener("load", function(e) {
+				var node, res = this.responseXML.documentElement;
+				var msg = createTag("fragment", {}, ["Releases “", createTag("a", {a:{title:loaders[this.getID()].method, href:loaders[this.getID()].url, target:"_blank"}}, loaders[this.getID()].method), "” on collection "]);
+				if ((node = res.querySelector("message text")) && node.textContent.match(/ok/i)) {
+					modal(createTag("p", {}, [msg, "OK."]));
+				} else {
+					modal(createTag("p", {}, [msg, "failed.\n\n"+res.textContent]));
+				}
+			});
+			xhr.addEventListener("error", function(e) {
+				if (--loaders[this.getID()].maxRetry > 0) {
+					modal(createTag("fragment", {}, ["Error performing ", createTag("a", {a:{title:loaders[this.getID()].method, href:loaders[this.getID()].url, target:"_blank"}}, "“"+loaders[this.getID()].method+"” method"), ", retrying…"]));
+					loadForAction(loaders[this.getID()].method, loaders[this.getID()].url);
+				} else {
+					alert("XHR-"+this.getID()+" ERROR "+this.status+"\nStopped retrying.\n"+loaders[this.getID].url+"\n\n"+this.responseText);
+				}
+			});
+			xhr.openDebug(method, url);
+			xhr.overrideMimeType("text/xml");
+			xhr.sendDebug(null);
+		}
 	}
 	/*======================================
 	## hacked from COLLECTION HIGHLIGHTER ##
 	======================================*/
-	function modal(txt, brs) {
+	function modal(txt) {
 		var obj = document.getElementById(meta.key+"modal");
 		if (txt && !obj) {
 			coolstuff("div", "50", "100%", "100%", "black", ".6");
-			obj = coolstuff("div", "55", "600px", "50%", "white");
+			obj = coolstuff("div", "55", "800px", "600px", "white");
 			obj.setAttribute("id", meta.key+"modal");
 			obj.style.padding = "4px";
 			obj.style.overflow = "auto";
@@ -180,19 +156,14 @@
 			obj.addEventListener("mouseout", function(e) { this.style.borderColor = "black"; }, false);
 		}
 		if (txt && obj) {
-			var br = 1;
-			if (brs) { br = brs; }
-			obj.appendChild(typeof txt=="string"?document.createTextNode(txt):txt);
-			for (var ibr=0; ibr<br; ibr++) {
-				obj.appendChild(document.createElement("br"));
-			}
+			var ret = obj.appendChild(typeof txt=="string"?document.createTextNode(txt):txt);
 			if (obj.style.borderColor == "black") { obj.scrollTop = obj.scrollHeight; }
 		}
 		if (!txt && obj) {
 			obj.parentNode.removeChild(obj.previousSibling);
 			obj.parentNode.removeChild(obj);
 		}
-		return obj;
+		return ret;
 		function coolstuff(t,z,x,y,b,o,a) {
 			var truc = document.getElementsByTagName("body")[0].appendChild(document.createElement(t));
 			truc.style.position = "fixed";
@@ -249,9 +220,9 @@
 		var t = (tag=="fragment"?document.createDocumentFragment():document.createElement(tag));
 		if(t.tagName) {
 			if (gadgets) {
-				for (var attri in gadgets.a) { if (gadgets.a.hasOwnProperty(attri)) { t.setAttribute(attri, gadgets.a[attri]); } }
-				for (var style in gadgets.s) { if (gadgets.s.hasOwnProperty(style)) { t.style.setProperty(style.replace(/!/,""), gadgets.s[style], style.match(/!/)?"important":""); } }
-				for (var event in gadgets.e) { if (gadgets.e.hasOwnProperty(event)) { t.addEventListener(event, gadgets.e[event], false); } }
+				for (var attri in gadgets.a) if (gadgets.a.hasOwnProperty(attri)) { t.setAttribute(attri, gadgets.a[attri]); }
+				for (var style in gadgets.s) if (gadgets.s.hasOwnProperty(style)) { t.style.setProperty(style.replace(/!/g,""), gadgets.s[style].replace(/!/g,""), style.match(/!/)||gadgets.s[style].match(/!/)?"important":""); }
+				for (var event in gadgets.e) if (gadgets.e.hasOwnProperty(event)) { t.addEventListener(event, gadgets.e[event], false); }
 			}
 			if (t.tagName == "A" && !t.getAttribute("href") && !t.style.getPropertyValue("cursor")) { t.style.setProperty("cursor", "pointer"); }
 		}
