77300d3a810589d430ce4e6c84f632a822cd0a2a
jesus2099 <jesus2099@users.noreply.github.com>
2014-11-14 17:03:04 +0100

    github. INSTALL USER SCRIPT! Convenient direct “raw” download link (leftmost file icon) to “Install” user scripts from github.com file lists

---
+++ github_INSTALL-USER-SCRIPT.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         github. INSTALL USER SCRIPT
+// @version      2014.11.14.1209
+// @description  github.com: Convenient direct “raw” download link (leftmost file icon) to “Install” user scripts from file lists
+// @supportURL   https://github.com/jesus2099/konami-command/issues
+// @namespace    https://github.com/jesus2099/konami-command
+// @downloadURL  https://raw.githubusercontent.com/jesus2099/konami-command/master/github_INSTALL-USER-SCRIPT.user.js
+// @updateURL    https://raw.githubusercontent.com/jesus2099/konami-command/master/github_INSTALL-USER-SCRIPT.user.js
+// @author       PATATE12 aka. jesus2099/shamo
+// @licence      CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/)
+// @since        2014-11-14
+// @icon         data:image/gif;base64,R0lGODlhEAAQAKEDAP+/3/9/vwAAAP///yH/C05FVFNDQVBFMi4wAwEAAAAh/glqZXN1czIwOTkAIfkEAQACAwAsAAAAABAAEAAAAkCcL5nHlgFiWE3AiMFkNnvBed42CCJgmlsnplhyonIEZ8ElQY8U66X+oZF2ogkIYcFpKI6b4uls3pyKqfGJzRYAACH5BAEIAAMALAgABQAFAAMAAAIFhI8ioAUAIfkEAQgAAwAsCAAGAAUAAgAAAgSEDHgFADs=
+// @grant        none
+// @include      http://github.com/*/*
+// @include      https://github.com/*/*
+// @run-at       document-end
+// ==/UserScript==
+setInterval(function() {"use strict";
+	[].forEach.call(document.querySelectorAll("table.files tbody a.js-directory-link[title$='.user.js']:not(.j2gIUS)"), function(ujsfile) {
+		ujsfile.className += " j2gIUS";
+		var icon = ujsfile.parentNode.parentNode.parentNode.querySelector("td.icon span.octicon.octicon-file-text");
+		if (icon) {
+			var install = document.createElement("a");
+			install.className = "octicon octicon-file-code"; // https://octicons.github.com
+			install.setAttribute("href", ujsfile.getAttribute("href").replace(/(\/[^/]+\/[^/]+)\/blob\//, "$1/raw/"));
+			install.setAttribute("title", "Install “"+ujsfile.getAttribute("title")+"”");
+			install.style.setProperty("color", "green");
+			install.addEventListener("click", function(e) {
+				e.cancelBubble = true;
+				if (e.stopPropagation) e.stopPropagation();
+				return false;
+			});
+			icon.parentNode.replaceChild(install, icon);
+		}
+	});
+}, 1000);
