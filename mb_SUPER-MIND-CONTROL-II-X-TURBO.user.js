8ac8cf39a6a67e6c743f8348532f499ea047cd6f
jesus2099 <jesus2099@users.noreply.github.com>
2014-11-18 23:22:34 +0100

    FIX. date range was not explained.

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,7 +1,7 @@
 (function(){"use strict";var meta={rawmdb:function(){
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2014.11.18.2057
+// @version      2014.11.18.2323
 // @description  musicbrainz.org power-ups (mbsandbox.org too): RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / POWER_RELATE_TO. auto-focus and remember last used types in "relate to" inline search / RELEASE_EDITOR_PROTECTOR. prevent accidental cancel by better tab key navigation / TRACKLIST_TOOLS. search→replace, track length parser, set selected works date / ALIAS_SORT_NAME. clever auto fill in / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_SWITCH / USER_STATS / MAX_RECENT_ENTITIES / RETURN_TO_MB_PROPERLY / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / MERGE_USER_MENUS / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP
 // @homepage     http://userscripts-mirror.org/scripts/show/85790
 // @supportURL   https://github.com/jesus2099/konami-command/issues
@@ -1045,7 +1045,7 @@
 			var tabs, re = document.querySelector("div.rel-editor");
 			if (re && (tabs = re.querySelector("ul.tabs"))) {
 				j2superturbo.menu.addItem(createTag("a", {e:{click:function(e){
-					var date = prompt("Type an YYYY-MM-DD, YYYY-MM or YYYY formated date that will be applied to all selected work relationships below.");
+					var date = prompt("Type an YYYY-MM-DD, YYYY-MM or YYYY formated date that will be applied to all selected work relationships below.\nYou can type two dates, separated by at least one any character (example: “2014-12-31 2015-01”). This will set a date ranged relationship.");
 					if (date = date.match(new RegExp(re_date.ISO+"(?:.+"+re_date.ISO+")?"))) {
 						// jquery stuff from reosarevok https://chatlogs.musicbrainz.org/musicbrainz/2014/2014-07/2014-07-08.html#T14-46-11-334532 who got it from bitmap :)
 						try {
