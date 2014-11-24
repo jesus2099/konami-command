f4be64171007f947406d91533170e56d47bd3ba5
jesus2099 <jesus2099@users.noreply.github.com>
2014-11-24 13:43:01 +0100

    NEW. links from settings panel to online documentation

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,7 +1,7 @@
 (function(){"use strict";var meta={rawmdb:function(){
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2014.11.24.1323
+// @version      2014.11.24.1342
 // @description  musicbrainz.org power-ups (mbsandbox.org too): RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / POWER_RELATE_TO. auto-focus and remember last used types in "relate to" inline search / RELEASE_EDITOR_PROTECTOR. prevent accidental cancel by better tab key navigation / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / ALIAS_SORT_NAME. clever auto fill in / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_SWITCH / USER_STATS / MAX_RECENT_ENTITIES / RETURN_TO_MB_PROPERLY / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / MERGE_USER_MENUS / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
 // @supportURL   https://github.com/jesus2099/konami-command/issues
@@ -110,7 +110,7 @@
 					var tr = table.appendChild(document.createElement("tr"));
 					tr.appendChild(createTag("th", {s:{"background-color":"#ccc","text-align":"left","padding-left":alphakeys[a].match(/[a-z]/)?"2em":"inherit"}}, j2settinput(alphakeys[a])));
 					tr.appendChild(createTag("td", {s:{opacity:".666", "text-align":"center"}}, typeof j2defs[alphakeys[a]]=="boolean"?(j2defs[alphakeys[a]]?"☑":"☐"):j2defs[alphakeys[a]]));
-					if (j2docs[alphakeys[a]]) { tr.appendChild(createTag("td", {s:{"margin-bottom":".4em"}}, j2docit(j2docs[alphakeys[a]]))); }
+					tr.appendChild(createTag("td", {s:{"margin-bottom":".4em"}}, j2docit(j2docs[alphakeys[a]]).concat([" — ", createTag("a", {a:{href:meta.homepage+"#"+alphakeys[a].toLowerCase(),target:"_blank"}}, "more help…")])));
 				}
 			}
 		}}}, meta.name+" settings")
@@ -179,7 +179,7 @@
 			arr.splice(1, 0, createTag("a", {a:{href:"http://tickets.musicbrainz.org/browse/"+jira[1].toUpperCase(),target:"_blank",title:"opens in new window"}}, jira[1]));
 			return arr;
 		}
-		else return txt;
+		else return [txt];
 	}
 	/*==================================================================== LINK+
 	## RELEASE_CLONER ##
