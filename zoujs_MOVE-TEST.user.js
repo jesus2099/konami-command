5698f4833ea36c6c82775e71e41ca6f2ff486fc4
jesus2099 <jesus2099@users.noreply.github.com>
2014-12-29 18:50:31 +0100

    ver.2 (UTF-8 repair, syntax error, downloadURL adjust)

--- zoujs_MOVE-TEST.user.js
+++ zoujs_MOVE-TEST.user.js
@@ -1,10 +1,10 @@
 (function(){"use strict";var meta={rawmdb:function(){
 // ==UserScript==
 // @name         zoujs. MOVE TEST
-// @version      1
-// @description  please don’t install this is just a test to see how to manage script file move with auto update maintain (on github, gf and oujs).
-// @downloadURL  https://raw.githubusercontent.com/jesus2099/konami-command/master/z_MOVE-TEST.user.js
-// @updateURL    https://raw.githubusercontent.com/jesus2099/konami-command/master/z_MOVE-TEST.user.js
+// @version      2
+// @description  please donâ€™t install this is just a test to see how to manage script file move with auto update maintain (on github, gf and oujs).
+// @downloadURL  https://raw.githubusercontent.com/jesus2099/konami-command/master/zoujs_MOVE-TEST.user.js
+// @updateURL    https://raw.githubusercontent.com/jesus2099/konami-command/master/zoujs_MOVE-TEST.user.js
 // ==/UserScript==
 	}};
 	if (meta.rawmdb && meta.rawmdb.toString && (meta.rawmdb = meta.rawmdb.toString())) {
@@ -17,6 +17,6 @@
 		}
 	}
 	var aMalibu = meta.name+" ("+meta.version+")";
-	try { aMalibu += "\n\n"+meta.downloadURL; } catch() {}
+	try { aMalibu += "\n\n"+meta.downloadURL; } catch(e) {}
 	alert(aMalibu);
 })();
