21d10d0e7ffcda39217b2cdab297d0e64fe3b025
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2014-12-29 18:46:26 +0100

    testing file moving and maintain auto updates

---
+++ zgf_MOVE-TEST.user.js
@@ -0,0 +1,22 @@
+(function(){"use strict";var meta={rawmdb:function(){
+// ==UserScript==
+// @name         zgf. MOVE TEST
+// @version      1
+// @description  please don’t install this is just a test to see how to manage script file move with auto update maintain (on github, gf and oujs).
+// @downloadURL  https://raw.githubusercontent.com/jesus2099/konami-command/master/z_MOVE-TEST.user.js
+// @updateURL    https://raw.githubusercontent.com/jesus2099/konami-command/master/z_MOVE-TEST.user.js
+// ==/UserScript==
+	}};
+	if (meta.rawmdb && meta.rawmdb.toString && (meta.rawmdb = meta.rawmdb.toString())) {
+		var kv/*key,val*/, row = /\/\/\s+@(\S+)\s+(.+)/g;
+		while ((kv = row.exec(meta.rawmdb)) !== null) {
+			if (meta[kv[1]]) {
+				if (typeof meta[kv[1]] == "string") meta[kv[1]] = [meta[kv[1]]];
+				meta[kv[1]].push(kv[2]);
+			} else meta[kv[1]] = kv[2];
+		}
+	}
+	var aMalibu = meta.name+" ("+meta.version+")";
+	try { aMalibu += "\n\n"+meta.downloadURL; } catch() {}
+	alert(aMalibu);
+})();
