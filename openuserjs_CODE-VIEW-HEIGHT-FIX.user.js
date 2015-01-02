4cb38f49484dec1946c8248ebfd8b4a215858ba8
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2015-01-02 15:02:02 +0100

    FIX. code zone does not update its height on window resize

--- openuserjs_CODE-VIEW-HEIGHT-FIX.user.js
+++ openuserjs_CODE-VIEW-HEIGHT-FIX.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         openuserjs. CODE VIEW HEIGHT FIX
-// @version      2015.1.2.1111
+// @version      2015.1.2.1459
 // @description  openuserjs.org. Source code tab height fix (for Opera 12 and other css "calc" and "vh" unit unabled browsers)
 // @supportURL   https://github.com/jesus2099/konami-command/issues
 // @namespace    https://github.com/jesus2099/konami-command
 // @downloadURL  https://raw.githubusercontent.com/jesus2099/konami-command/master/openuserjs_CODE-VIEW-HEIGHT-FIX.user.js
 // @updateURL    https://raw.githubusercontent.com/jesus2099/konami-command/master/openuserjs_CODE-VIEW-HEIGHT-FIX.user.js
-// @author       PATATE12 aka. jesus2099/shamo
+// @author       Tristan “PATATE12” Daniel
 // @licence      CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/)
-// @since        2014-06-06
+// @since        2015-01-02
 // @icon         data:image/gif;base64,R0lGODlhEAAQAKEDAP+/3/9/vwAAAP///yH/C05FVFNDQVBFMi4wAwEAAAAh/glqZXN1czIwOTkAIfkEAQACAwAsAAAAABAAEAAAAkCcL5nHlgFiWE3AiMFkNnvBed42CCJgmlsnplhyonIEZ8ElQY8U66X+oZF2ogkIYcFpKI6b4uls3pyKqfGJzRYAACH5BAEIAAMALAgABQAFAAMAAAIFhI8ioAUAIfkEAQgAAwAsCAAGAAUAAgAAAgSEDHgFADs=
 // @grant        none
 // @include      *://openuserjs.org/scripts/*/*/source
@@ -19,10 +19,19 @@
 var editor = document.querySelector("pre#editor");
 if (editor) {
 	var minHeight = getComputedStyle(editor).getPropertyValue("min-height");
-	if (minHeight && (minHeight = minHeight.match(/(\d+)px/)) && (minHeight = parseInt(minHeight[1], 10)) && editor.offsetHeight <= minHeight) {
+	if (minHeight && (minHeight = minHeight.match(/(\d+)px/)) && (minHeight = parseInt(minHeight[1], 10)) && editor.offsetHeight == minHeight) {
 		var margins = document.getElementsByTagName("head")[0].querySelector("style");
-		if (margins && (margins = margins.textContent.match(/#editor[^}]+height: calc\(100vh\s*-\s*(\d+)px\);/)) && (margins = parseInt(margins[1], 10))) {
-			editor.style.setProperty("height", (document.documentElement.clientHeight - margins) + "px");
+		if (margins && (margins = margins.textContent.match(/#editor[^}]+height: calc\((\d+)vh\s*-\s*(\d+)px\);/))) {
+			document.addEventListener("resize", setEditorHeight);
+			sendEvent(editor, "resize");
 		}
 	}
+}
+function setEditorHeight() {
+	editor.style.setProperty("height", (document.documentElement.clientHeight * 100 / margins[1] - margins[2]) + "px");
+}
+function sendEvent(element, eventName){
+	var event = document.createEvent("HTMLEvents");
+	event.initEvent(eventName, true, true);
+	node.dispatchEvent(event);
 }
