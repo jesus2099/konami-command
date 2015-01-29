67740044173a4b1c1f0c069954308f08b39d98bd
jesus2099 <jesus2099@users.noreply.github.com>
2015-01-29 11:45:08 +0100

    FIX. « text selects when I double‑click » regression was not fixed by 2aa5db2cd6a58d2b4f05b0d55f8bd0f32a667af7.

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,7 +1,7 @@
 (function(){"use strict";var meta={rawmdb:function(){
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2015.1.27.8.24
+// @version      2015.1.29.10.57
 // @description  musicbrainz.org power-ups (mbsandbox.org too): RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / POWER_RELATE_TO. auto-focus and remember last used types in "relate to" inline search / RELEASE_EDITOR_PROTECTOR. prevent accidental cancel by better tab key navigation / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / ALIAS_SORT_NAME. clever auto fill in / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_SWITCH / USER_STATS / MAX_RECENT_ENTITIES / RETURN_TO_MB_PROPERLY / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / MERGE_USER_MENUS / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
 // @supportURL   https://github.com/jesus2099/konami-command/issues
@@ -717,10 +717,8 @@
 		var objs = document.querySelectorAll("div#page form > *:not(.edit-list) input[type='radio'], select[multiple]");
 		for (var o=0; o < objs.length; o++) {
 			var obj = getParent(objs[o], "label") || objs[o];
-			if (objs[0].tagName == "LABEL") obj.addEventListener("mousedown", stop, false);
-			obj.addEventListener("dblclick", function(e) {
-				parentFormSubmit(this, e);
-			}, false);
+			if (obj.tagName == "LABEL") obj.addEventListener("mousedown", function(e) { e.preventDefault(); });
+			obj.addEventListener("dblclick", function(e) { parentFormSubmit(this, e); });
 			obj.setAttribute("title", "double-click here to submit the form");
 		}
 	}
