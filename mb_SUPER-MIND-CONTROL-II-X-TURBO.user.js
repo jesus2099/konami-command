f1a35a86e6b12f22c1e88e1793babea0e7a929d0
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2015-03-06 15:55:25 +0100

    FIX. SLOW_DOWN_RETRY does not detect “read timeout” search errors.

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,7 +1,7 @@
 (function(){"use strict";var meta={rawmdb:function(){
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2015.3.4.14.34
+// @version      2015.3.6.15.45
 // @description  musicbrainz.org power-ups (mbsandbox.org too): RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / RELEASE_EDITOR_PROTECTOR. prevent accidental cancel by better tab key navigation / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / ALIAS_SORT_NAME. clever auto fill in / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_SWITCH / USER_STATS / MAX_RECENT_ENTITIES / RETURN_TO_MB_PROPERLY / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / MERGE_USER_MENUS / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / UNLINK_ENTITY_HEADER
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
 // @supportURL   https://github.com/jesus2099/konami-command/issues
@@ -995,8 +995,8 @@
 	if (j2sets.SLOW_DOWN_RETRY) {
 		var errortype = document.title.match(/^(slow down!|search error) - musicbrainz$/i);
 		if (errortype) {
-			var retrydelay=20;
-			switch (errortype[1]) {
+			var retrydelay;
+			switch (errortype[1].toLowerCase()) {
 				case "slow down!":
 					retrydelay = 20;
 					break;
@@ -1007,10 +1007,10 @@
 					break;
 			}
 			if (retrydelay) {
-				document.querySelector("div#content h1").appendChild(createTag("fragment", {}, [" (retrying", createTag("span", {a:{"class":"countdown"}}, delayMsg(retrydelay)), ")"]));
+				document.querySelector("div#page h1, div#content h1").appendChild(createTag("fragment", {}, [" (retrying", createTag("span", {a:{"class":"countdown"}}, delayMsg(retrydelay)), ")"]));
 				setInterval(function(e) {
 					retrydelay--;
-					replaceChildren(document.createTextNode(delayMsg(retrydelay)), document.querySelector("div#content h1 > span.countdown"));
+					replaceChildren(document.createTextNode(delayMsg(retrydelay)), document.querySelector("h1 > span.countdown"));
 					if (retrydelay == 0) { location.reload(false); }
 				}, 1000);
 			}
