7b608931adbb661fdd0d2bbcff7b2614980fba3a
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2015-04-30 17:26:35 +0200

    FIX. submits when selecting votes (regression brought by f30eda37afdb0ff80500ae4fb0462621b9b17958)

--- mb_POWER-VOTE.user.js
+++ mb_POWER-VOTE.user.js
@@ -1,7 +1,7 @@
 (function(){"use strict";var meta={rawmdb:function(){
 // ==UserScript==
 // @name         mb. POWER VOTE
-// @version      2015.4.30.1619
+// @version      2015.4.30.1725
 // @description  musicbrainz.org: Adds some buttons to check all unvoted edits (Yes/No/Abs/None) at once in the edit search page. You can also collapse/expand (all) edits for clarity. A handy reset votes button is also available + Double click radio to vote single edit + range click with shift to vote a series of edits. , Hidden (collapsed) edits will never be voted (even if range click or shift+click force vote).
 // @homepage     http://userscripts-mirror.org/scripts/show/57765
 // @supportURL   https://github.com/jesus2099/konami-command/issues
@@ -311,13 +311,15 @@
 		return obj;
 	}
 	function shortcut(vote, txt) {
-		var button = document.createElement("button");
+		var button = document.createElement("input");
+		button.setAttribute("type", "button");
+		button.className = "styled-button";
 		if (onlySubmitTabIndexed) { button.setAttribute("tabindex", "-1"); }
 		button.style.setProperty("padding", FF?"0 2px":"0 3px");
 		button.style.setProperty("margin", FF?"0 3px 0 0":"0 3px");
 		button.style.setProperty("float", "none");
-		button.appendChild(document.createTextNode(txt));
-		button.addEventListener("click", function(e) { doitdoit(e, vote); }, false);
+		button.value = txt;
+		button.addEventListener("click", function(e) { doitdoit(e, vote); });
 		return button;
 	}
 	function doitdoit(e, vote, min, max) {
@@ -327,8 +329,7 @@
 					click(radios[i]);
 				}
 			}
-		}
-		else { for (i = 0; i < radiosafe.length; i++) { click(radiosafe[i]); } }
+		} else { for (i = 0; i < radiosafe.length; i++) { click(radiosafe[i]); } }
 	}
 	function isOkToVote(radiox) {
 		return getParent(radiox, "div", "voteopts").querySelector("input[type='radio'][value='-2']").checked;
