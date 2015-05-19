jesus2099/konami-command

commit f30eda37afdb0ff80500ae4fb0462621b9b17958

MBS. buttons style has changed (#68)

mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,7 +1,7 @@
 (function(){"use strict";var meta={rawmdb:function(){
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2015.4.21.2254
+// @version      2015.4.30.1652
 // @description  musicbrainz.org power-ups (mbsandbox.org too): RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / RELEASE_EDITOR_PROTECTOR. prevent accidental cancel by better tab key navigation / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / ALIAS_SORT_NAME. clever auto fill in / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_SWITCH / USER_STATS / MAX_RECENT_ENTITIES / RETURN_TO_MB_PROPERLY / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / MERGE_USER_MENUS / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
 // @supportURL   https://github.com/jesus2099/konami-command/issues
@@ -511,7 +511,7 @@
 		EASY_DATE_calmDOMto = setTimeout(EASY_DATE_init, 100);
 	}
 	function EASY_DATE_init() {
-		for (var years=document.querySelectorAll("*.partial-date > input[placeholder='YYYY'][maxlength='4'][size='4']:not(."+userjs+"easydate)"), y=0; y<years.length; y++) {
+		for (var years=document.querySelectorAll(".partial-date > input[placeholder='YYYY'][maxlength='4'][size='4']:not(."+userjs+"easydate)"), y=0; y<years.length; y++) {
 			addAfter(
 				createTag("input",{
 					a:{value:years[y].value, placeholder:"YYY+", size:"4", title:"EASY_DATE®\n"+j2docs.EASY_DATE},
@@ -1231,11 +1231,11 @@
 		re.addEventListener("DOMNodeInserted", function(e) {
 			var tps = this.querySelectorAll("#tracklist-tools button[data-click='openTrackParser']");
 			for (var tp=0; tp<tps.length; tp++) {
-				if (!tps[tp].parentNode.querySelector("*."+userjs+"track-length-parser")) {
-					addAfter(createTag("input", {a:{type:"button","class":userjs+"track-length-parser",value:"Time Parser","_ctrlValue":"Erase times",title:"CONTROL key to ERASE track times\nSHIFT key to alter all open tracklists"},s:{"background-color":"yellow"}}), tps[tp]);
+				if (!tps[tp].parentNode.querySelector("."+userjs+"track-length-parser")) {
+					addAfter(createTag("button", {a:{type:"button","class":userjs+"track-length-parser","_ctrlValue":"Erase times",title:"CONTROL key to ERASE track times\nSHIFT key to alter all open tracklists"},s:{"background-color":"yellow"}}, "Time Parser"), tps[tp]);
 				}
-				if (!tps[tp].parentNode.querySelector("*."+userjs+"search-replace")) {
-					addAfter(createTag("input", {a:{type:"button","class":userjs+"search-replace",value:"Search→replace",title:"SHIFT key to alter all open tracklists"},s:{"background-color":"yellow"}}), tps[tp]);
+				if (!tps[tp].parentNode.querySelector("."+userjs+"search-replace")) {
+					addAfter(createTag("button", {a:{type:"button","class":userjs+"search-replace",title:"SHIFT key to alter all open tracklists"},s:{"background-color":"yellow"}}, "Search→replace"), tps[tp]);
 				}
 			}
 		}, false);

mb_ELEPHANT-EDITOR.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. ELEPHANT EDITOR
-// @version      2015.2.18.12.0
+// @version      2015.4.30.1642
 // @description  musicbrainz.org + acoustid.org: Remember last edit notes and dates
 // @homepage     http://userscripts-mirror.org/scripts/show/94629
 // @supportURL   https://github.com/jesus2099/konami-command/issues
@@ -273,7 +273,7 @@ function createTag(tag, gadgets, children) {
 	return t;
 }
 function createButtor(label, width) {
-	var butt = createTag("input", {a:{type:"button",value:label,tabindex:"-1"}, s:{display:"inline",padding:"2px","float":"none"}});
+	var butt = createTag("input", {a:{type:"button",value:label,tabindex:"-1","class":"styled-button"}, s:{display:"inline",padding:"2px","float":"none"}});
 	if (width) { butt.style.setProperty("width", width); }
 	return butt;
 }

mb_POWER-VOTE.user.js
@@ -1,7 +1,7 @@
 (function(){"use strict";var meta={rawmdb:function(){
 // ==UserScript==
 // @name         mb. POWER VOTE
-// @version      2015.1.23.19.2
+// @version      2015.4.30.1619
 // @description  musicbrainz.org: Adds some buttons to check all unvoted edits (Yes/No/Abs/None) at once in the edit search page. You can also collapse/expand (all) edits for clarity. A handy reset votes button is also available + Double click radio to vote single edit + range click with shift to vote a series of edits. , Hidden (collapsed) edits will never be voted (even if range click or shift+click force vote).
 // @homepage     http://userscripts-mirror.org/scripts/show/57765
 // @supportURL   https://github.com/jesus2099/konami-command/issues
@@ -46,8 +46,8 @@
 		var voteColours = true;
 		/* - --- - --- - --- - END  OF  CONFIGURATION - --- - --- - --- - */
 		var userjs = "jesus2099userjs57765";
-		var FFF = /firefox/i.test(navigator.userAgent) && !/opera/i.test(navigator.userAgent);/*FFF has bugs*/
-		if (FFF) { FFF = {"1":"#b1ebb0", "0":"#ebb1ba", "-1":"#f2f0a5"}; }
+		var FF = /firefox/i.test(navigator.userAgent) && !/opera/i.test(navigator.userAgent);/*FF has bugs*/
+		if (FF) { FF = {"1":"#b1ebb0", "0":"#ebb1ba", "-1":"#f2f0a5"}; }
 		document.head.appendChild(document.createElement("style")).setAttribute("type", "text/css");
 		var j2css = document.styleSheets[document.styleSheets.length-1];
 		j2css.insertRule("div.edit-list."+userjs+"force, div.edit-list."+userjs+"ninja > div.edit-actions, div.edit-list."+userjs+"ninja > div.edit-details, div.edit-list."+userjs+"ninja > div.edit-notes { overflow: hidden !important; height: 0 !important; !important; padding: 0 !important; margin: 0 !important; }", j2css.cssRules.length);
@@ -89,10 +89,10 @@
 		for (var i = 0; i < inputs.length; i++) {
 			if (onlySubmitTabIndexed) { inputs[i].setAttribute("tabindex", "-1"); }
 			radios.push(inputs[i]);
-			if (voteColours) {/*FFF is LIFO*/
+			if (voteColours) {/*FF is LIFO*/
 				inputs[i].addEventListener("change", function(e) {
 					var actions = getParent(this, "div", "edit-actions");
-					if (this.value != -2) { actions.style.setProperty("background-color", FFF?FFF[this.value]:self.getComputedStyle(getParent(this, "div", "vote")).getPropertyValue("background-color")); }
+					if (this.value != -2) { actions.style.setProperty("background-color", FF?FF[this.value]:self.getComputedStyle(getParent(this, "div", "vote")).getPropertyValue("background-color")); }
 					else { actions.style.removeProperty("background-color"); }
 				}, false);
 				if (inputs[i].checked) { sendEvent(inputs[i], "change"); }
@@ -311,20 +311,18 @@
 		return obj;
 	}
 	function shortcut(vote, txt) {
-		var button = document.createElement("input");
-		button.setAttribute("type", "button");
+		var button = document.createElement("button");
 		if (onlySubmitTabIndexed) { button.setAttribute("tabindex", "-1"); }
-		button.style.setProperty("padding", "0 3px");
-		button.style.setProperty("margin", "0");
+		button.style.setProperty("padding", FF?"0 2px":"0 3px");
+		button.style.setProperty("margin", FF?"0 3px 0 0":"0 3px");
 		button.style.setProperty("float", "none");
-		button.appendChild( document.createTextNode(txt) );
-		button.setAttribute("value", txt);
+		button.appendChild(document.createTextNode(txt));
 		button.addEventListener("click", function(e) { doitdoit(e, vote); }, false);
 		return button;
 	}
 	function doitdoit(e, vote, min, max) {
 		if (vote != "omgcancel") {
-			for (i = (min?min+(FFF?0:1):0); i < (max?max+1:radios.length); i++) {/*FFF shift+click label NG*/
+			for (i = (min?min+(FF?0:1):0); i < (max?max+1:radios.length); i++) {/*FF shift+click label NG*/
 				if (radios[i].getAttribute("value") == vote && !radios[i].checked && !ninja(e, getParent(radios[i], "div", "edit-list")) && (e.shiftKey || isOkToVote(radios[i]))) {
 					click(radios[i]);
 				}

commit 17ca15c926d4063290d3eacc4b0438107383daff

FIX. “buttons style has changed (MBS #68)” f30eda37afdb0ff80500ae4fb0462621b9b17958 broke CTRL and SHIFT key displays in TRACKLIST_TOOLS

mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,7 +1,7 @@
 (function(){"use strict";var meta={rawmdb:function(){
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2015.5.12.1844
+// @version      2015.5.19.1718
 // @description  musicbrainz.org power-ups (mbsandbox.org too): RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / RELEASE_EDITOR_PROTECTOR. prevent accidental cancel by better tab key navigation / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / ALIAS_SORT_NAME. clever auto fill in / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_SWITCH / USER_STATS / MAX_RECENT_ENTITIES / RETURN_TO_MB_PROPERLY / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / MERGE_USER_MENUS / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
 // @supportURL   https://github.com/jesus2099/konami-command/issues
@@ -1186,7 +1186,7 @@
 						localStorage.setItem(userjs+"search-replace", JSON.stringify(searchrep));
 					}
 				} else if (e.target.className == userjs+"track-length-parser") {
-					var erase = e.target.value.match(/erase/i) || e.ctrlKey;
+					var erase = e.target.textContent.match(/erase/i) || e.ctrlKey;
 					var inputs = TRACKLIST_TOOLS_getInputs("td.length > input.track-length[type='text']", e.target, e);
 					var times = !erase && prompt("Track length parser\n\nPlease paste your huge text including track times below.\n“1:23” and “1′23″” and even incorrect “1’23”” and “1'23\"” will be parsed.\nYou can for instance copy from your foobar2000 tracklist, minc.or.jp, etc.\nWARNING. You must understand that all current times will be overwritten in the tracklist editor.");
 					if (erase && confirm("Are you sure you want to ERASE all track times?") || times && (times = times.match(/\b\d{1,3}[:′’']\d\d\b[″”"]?/g))) {
@@ -1209,23 +1209,23 @@
 					}
 				}
 			} else if (e.type.match(/^mouse(over|out)$/)) {
-				var _value = e.target.getAttribute("_value");
-				var _ctrlValue = e.target.getAttribute("_ctrlValue");
+				var _text = e.target.getAttribute("_text");
+				var _ctrlText = e.target.getAttribute("_ctrlText");
 				switch (e.type) {
 					case "mouseover":
-						if (!_value) { e.target.setAttribute("_value", e.target.value); }
-						if (e.ctrlKey && _ctrlValue) {
+						if (!_text) { e.target.setAttribute("_text", e.target.textContent); }
+						if (e.ctrlKey && _ctrlText) {
 							e.target.style.setProperty("background-color", "pink");
-							e.target.value = _ctrlValue;
+							e.target.replaceChild(document.createTextNode(_ctrlText), e.target.firstChild);
 						}
 						if (e.shiftKey) {
-							if (!(e.ctrlKey && _ctrlValue)) { e.target.style.setProperty("background-color", "gold"); }
-							e.target.value += " (all)";
+							if (!(e.ctrlKey && _ctrlText)) { e.target.style.setProperty("background-color", "gold"); }
+							e.target.replaceChild(document.createTextNode(e.target.textContent+" (all)"), e.target.firstChild);
 						}
 						break;
 					case "mouseout":
 						e.target.style.setProperty("background-color", "yellow");
-						e.target.value = _value;
+						e.target.replaceChild(document.createTextNode(_text), e.target.firstChild);
 						break;
 				}
 			}
@@ -1242,7 +1242,7 @@
 			var tps = this.querySelectorAll("#tracklist-tools button[data-click='openTrackParser']");
 			for (var tp=0; tp<tps.length; tp++) {
 				if (!tps[tp].parentNode.querySelector("."+userjs+"track-length-parser")) {
-					addAfter(createTag("button", {a:{type:"button","class":userjs+"track-length-parser","_ctrlValue":"Erase times",title:"CONTROL key to ERASE track times\nSHIFT key to alter all open tracklists"},s:{"background-color":"yellow"}}, "Time Parser"), tps[tp]);
+					addAfter(createTag("button", {a:{type:"button","class":userjs+"track-length-parser","_ctrlText":"Erase times",title:"CONTROL key to ERASE track times\nSHIFT key to alter all open tracklists"},s:{"background-color":"yellow"}}, "Time Parser"), tps[tp]);
 				}
 				if (!tps[tp].parentNode.querySelector("."+userjs+"search-replace")) {
 					addAfter(createTag("button", {a:{type:"button","class":userjs+"search-replace",title:"SHIFT key to alter all open tracklists"},s:{"background-color":"yellow"}}, "Search→replace"), tps[tp]);