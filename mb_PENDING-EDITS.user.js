jesus2099/konami-command

commit 1e1235336daea9430d9d713470b89bbc87a8347c

event and series support better pending edit tooltips  (more compact, better new lines, less word wrap?, show editor names) cleanup

mb_PENDING-EDITS.user.js
@@ -1,27 +1,48 @@
 // ==UserScript==
 // @name         mb. PENDING EDITS
 // @version      2015.6.18.123
+// @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_PENDING-EDITS.user.js
 // @description  musicbrainz.org: Adds/fixes links to entity (pending) edits (if any); optionally adds links to associated artist(s) (pending) edits
 // @homepage     http://userscripts-mirror.org/scripts/show/42102
 // @supportURL   https://github.com/jesus2099/konami-command/issues
+// @compatible   opera(12.17)+violentmonkey  my own browsing setup
+// @compatible   firefox(39)+greasemonkey    quickly tested
+// @compatible   chromium(46)+tampermonkey   quickly tested
+// @compatible   chrome+tampermonkey         should be same as chromium
 // @namespace    https://github.com/jesus2099/konami-command
 // @downloadURL  https://github.com/jesus2099/konami-command/raw/master/mb_PENDING-EDITS.user.js
 // @updateURL    https://github.com/jesus2099/konami-command/raw/master/mb_PENDING-EDITS.user.js
 // @author       PATATE12
 // @licence      CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/)
 // @since        2009-02-09
-// @icon         data:image/gif;base64,R0lGODlhEAAQAKEDAP+/3/9/vwAAAP///yH/C05FVFNDQVBFMi4wAwEAAAAh/glqZXN1czIwOTkAIfkEAQACAwAsAAAAABAAEAAAAkCcL5nHlgFiWE3AiMFkNnvBed42CCJgmlsnplhyonIEZ8ElQY8U66X+oZF2ogkIYcFpKI6b4uls3pyKqfGJzRYAACH5BAEIAAMALAgABQAFAAMAAAIFhI8ioAUAIfkEAQgAAwAsCAAGAAUAAgAAAgSEDHgFADs=
+// @icon         data:image/gif;base64,R0lGODlhEAAQAMIDAAAAAIAAAP8AAP///////////////////yH5BAEKAAQALAAAAAAQABAAAAMuSLrc/jA+QBUFM2iqA2ZAMAiCNpafFZAs64Fr66aqjGbtC4WkHoU+SUVCLBohCQA7
 // @grant        none
 // @include      http*://*musicbrainz.org/area/*
 // @include      http*://*musicbrainz.org/artist/*
 // @include      http*://*musicbrainz.org/collection/*
+// @include      http*://*musicbrainz.org/event/*
 // @include      http*://*musicbrainz.org/label/*
 // @include      http*://*musicbrainz.org/place/*
 // @include      http*://*musicbrainz.org/recording/*
 // @include      http*://*musicbrainz.org/release/*
 // @include      http*://*musicbrainz.org/release-group/*
+// @include      http*://*musicbrainz.org/series/*
 // @include      http*://*musicbrainz.org/url/*
 // @include      http*://*musicbrainz.org/work/*
+// @include      http://*.mbsandbox.org/area/*
+// @include      http://*.mbsandbox.org/artist/*
+// @include      http://*.mbsandbox.org/collection/*
+// @include      http://*.mbsandbox.org/event/*
+// @include      http://*.mbsandbox.org/label/*
+// @include      http://*.mbsandbox.org/place/*
+// @include      http://*.mbsandbox.org/recording/*
+// @include      http://*.mbsandbox.org/release/*
+// @include      http://*.mbsandbox.org/release-group/*
+// @include      http://*.mbsandbox.org/series/*
+// @include      http://*.mbsandbox.org/url/*
+// @include      http://*.mbsandbox.org/work/*
+// @exclude      *//*/*mbsandbox.org/*
+// @exclude      *//*/*musicbrainz.org/*
 // @run-at       document-end
 // ==/UserScript==
 (function(){"use strict";
@@ -33,28 +54,32 @@ It will add other request(s) to MB server, this is why it is an option.*/
 	var userjs = "jesus2099userjs42102";
 	var RE_GUID = "[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}";
 	var loc, entity, checked = [], xhrPendingEdits = {};
-	var MBS = self.location.protocol+"//"+self.location.host;
-	var account = document.querySelector("div#header li.account a[href^='"+MBS+"/user/']");
+	var MBS = location.protocol + "//" + location.host;
+	var account = document.querySelector("div#header li.account a[href^='" + MBS + "/user/']");
 /*EDITING HISTORY*/
 	if (
 		account &&
+		(account = unescape(account.getAttribute("href").match(/[^/]+$/))) &&
 		document.querySelector("div#sidebar") &&
-		(loc = self.location.pathname.match(new RegExp("^/(area|artist|collection|label|place|release-group|release|recording|work|url)/("+RE_GUID+")"))) &&
+		(loc = location.pathname.match(new RegExp("^/(area|artist|collection|event|label|place|release-group|release|recording|series|work|url)/(" + RE_GUID + ")"))) &&
 		(entity = document.querySelector("div#content > div.tabs > ul.tabs > li > a")) &&
 		(entity = a2obj(entity))
 	) {
-		entity.editinghistory = document.querySelector("div#sidebar ul.links a[href='"+MBS+entity.base+"/edits']");
+		entity.editinghistory = document.querySelector("div#sidebar ul.links a[href='" + MBS + entity.base + "/edits']");
 		if (entity.editinghistory) {
 			entity.ul = getParent(entity.editinghistory, "ul");
 		} else {
 			entity.ul = document.querySelector("div#sidebar ul.links");
-			entity.editinghistory = createLink(entity, "edits");/*reverts MBS-57 drawback*/
+			entity.editinghistory = createLink(entity, "edits"); /*reverts MBS-57 drawback*/
 		}
 		entity.li = getParent(entity.editinghistory, "li");
 /*OPEN EDITS*/
-		entity.openedits = document.querySelector("div#sidebar a[href='"+MBS+entity.base+"/open_edits']");
-		if (entity.openedits) { entity.openedits = createLink(entity.openedits, "open_edits"); }/*fixes MBS-2298*/
-		else { entity.openedits = createLink(entity, "open_edits"); }/*fixes MBS-3386*/
+		entity.openedits = document.querySelector("div#sidebar a[href='" + MBS + entity.base + "/open_edits']");
+		if (entity.openedits) {
+			entity.openedits = createLink(entity.openedits, "open_edits"); /*fixes MBS-2298*/
+		} else {
+			entity.openedits = createLink(entity, "open_edits"); /*fixes MBS-3386*/
+		}
 		checked.push(entity.base);
 		checkOpenEdits(entity);
 /*ARTIST LINKS*/
@@ -64,10 +89,10 @@ It will add other request(s) to MB server, this is why it is an option.*/
 				case "release-group":
 				case "release":
 				case "recording":
-					artists = document.querySelectorAll("p.subheader a[href^='"+MBS+"/artist/']");
+					artists = document.querySelectorAll("p.subheader a[href^='" + MBS + "/artist/']");
 					break;
 				case "url":
-					artists = document.querySelectorAll("div#content a[href^='"+MBS+"/artist/'], div#content a[href^='"+MBS+"/label/']");
+					artists = document.querySelectorAll("div#content a[href^='" + MBS + "/artist/'], div#content a[href^='" + MBS + "/label/']");
 					break;
 				case "work":
 					artists = workMainPerformer();
@@ -75,7 +100,7 @@ It will add other request(s) to MB server, this is why it is an option.*/
 					break;
 			}
 			if (artists && artists.length && artists.length > 0) {
-				for (var arti=artists.length-1; arti>=0; arti--) {
+				for (var arti = artists.length - 1; arti >= 0; arti--) {
 					var art = a2obj(artists[arti]);
 					if (checked.indexOf(art.base) < 0) {
 						checked.push(art.base);
@@ -94,42 +119,42 @@ It will add other request(s) to MB server, this is why it is an option.*/
 			smp.className = "mp";
 			ent.parentNode.replaceChild(smp.appendChild(ent.cloneNode(true)).parentNode, ent);
 			return smp.firstChild;
-		}
-		else {
+		} else {
 			var obj = art || ent;
-			var txt = (typ=="edits"?"editing\u00a0history":"open\u00a0edits");
-			txt = (art?obj.name+" "+txt:txt.substr(0, 1).toUpperCase()+txt.substr(1));
+			var txt = (typ == "edits" ? "editing\u00a0history" : "open\u00a0edits");
+			txt = (art ? obj.name + " " + txt : txt.substr(0, 1).toUpperCase() + txt.substr(1));
 			var newLink = document.createElement("li").appendChild(document.createElement("span")).appendChild(document.createElement("a")).appendChild(document.createTextNode(txt)).parentNode;
-			newLink.setAttribute("href", obj.base+"/"+typ);
-			if (art) { addAfter(newLink.parentNode.parentNode, ent.li); }
-			else if (!art && typ == "edits") {
+			newLink.setAttribute("href", obj.base + "/" + typ);
+			if (art) {
+				addAfter(newLink.parentNode.parentNode, ent.li);
+			} else if (!art && typ == "edits") {
 				ent.ul.appendChild(document.createElement("hr"));
 				ent.ul.appendChild(newLink.parentNode.parentNode);
-			} else { ent.ul.insertBefore(newLink.parentNode.parentNode, ent.li); }
+			} else {
+				ent.ul.insertBefore(newLink.parentNode.parentNode, ent.li);
+			}
 			return newLink;
 		}
 	}
 	function checkOpenEdits(obj) {
 		var smp = getParent(obj.openedits, "li").firstChild;
-		var ret = smp.querySelector("span."+userjs+"ret");
-		if (!ret) {
+		var count = smp.querySelector("span." + userjs + "count");
+		if (!count) {
 			smp.appendChild(document.createTextNode("\u00a0("));
-			ret = smp.appendChild(document.createElement("span")).appendChild(document.createTextNode("⌛")).parentNode;
-			ret.className = userjs+"ret";
+			count = smp.appendChild(document.createElement("span")).appendChild(document.createTextNode("⌛")).parentNode;
+			count.className = userjs + "count";
 			smp.appendChild(document.createTextNode(")"));
 		}
 		xhrPendingEdits[obj.base] = {
-			"object": obj,
-			"xhr": new XMLHttpRequest()
+			object: obj,
+			xhr: new XMLHttpRequest()
 		};
 		xhrPendingEdits[obj.base].xhr.onreadystatechange = function() {
 			if (this.readyState == 4) {
 				var xhrpe;
-				for (var x in xhrPendingEdits) {
-					if (xhrPendingEdits.hasOwnProperty(x) && xhrPendingEdits[x].xhr == this) {
-						xhrpe = xhrPendingEdits[x];
-						break;
-					}
+				for (var x in xhrPendingEdits) if (xhrPendingEdits.hasOwnProperty(x) && xhrPendingEdits[x].xhr == this) {
+					xhrpe = xhrPendingEdits[x];
+					break;
 				}
 				if (this.status == 200) {
 					var editc = this.responseText.match(/found (at least )?(\d+) edits?/i), editDetails;
@@ -137,11 +162,15 @@ It will add other request(s) to MB server, this is why it is an option.*/
 						editc = [null, editc[1], parseInt(editc[2], 10)];
 						editDetails = {
 							types: this.responseText.match(/[^<>]+(?=<\/bdi><\/a><\/h2>)/g),
-							editors: this.responseText.match(new RegExp("Edit by <a href=\""+MBS+"/user/[^/]+\">", "g"))
+							editors: this.responseText.match(new RegExp("Edit by <a href=\"" + MBS + "/user/[^/]+\">", "g"))
 						};
-					} else { editc = [null, false, 0]; }
+					} else {
+						editc = [null, false, 0];
+					}
 					updateLink(xhrpe.object, editc[2], editDetails, editc[1]);
-				} else { updateLink(xhrpe.object, this); }
+				} else {
+					updateLink(xhrpe.object, this);
+				}
 			}
 		};
 		xhrPendingEdits[obj.base].xhr.open("get", obj.openedits.getAttribute("href"), true);
@@ -150,48 +179,55 @@ It will add other request(s) to MB server, this is why it is an option.*/
 	}
 	function updateLink(obj, pecount, details, more) {
 		var txt;
-		var tit = "pending edit";
+		var tit;
 		var li = getParent(obj.openedits, "li");
-		var ret = li.querySelector("span."+userjs+"ret");
+		var count = li.querySelector("span." + userjs + "count");
 		if (typeof pecount == "number") {
 			txt = pecount;
 			if (more) txt += "+";
-			tit = txt + " " + tit + (pecount!=1?"s":"");
+			var countTitle = (txt != 0 ? txt : "no") + " pending edit" + (txt != 1 ? "s" : "");
+			count.setAttribute("title", countTitle);
 			if (pecount == 0) {
 				mp(obj.openedits, false);
+				tit = countTitle;
 			} else if (pecount > 0) {
 				mp(obj.openedits, true);
 				if (details.types.length > 0 && details.types.length == details.editors.length) {
-					tit += ":\n\n";
 					var titarray = [], dupcount = 0, dupreset;
 					for (var d = 0; d < details.types.length; d++) {
 						var thistit = details.types[d].replace(/^Edit #\d+ /, "");
-						if (details.editors[d].replace(/^.+\/user\/|">$/g, "") == account.getAttribute("href").match(/[^/]+$/)) thistit += "*";
+						var editor = unescape(details.editors[d].replace(/^.+\/user\/|">$/g, ""));
+						if (editor != account) {
+							thistit += " (" + editor + ")";
+						}
 						if (thistit != titarray[titarray.length - 1]) {
 							titarray.push(thistit);
-							if (d > 0) dupreset = true;
+							if (d > 0) {
+								dupreset = true;
+							}
 						} else {
 							dupcount++;
 						}
 						var last = (d == details.types.length - 1);
 						if (dupcount > 0 && (dupreset || last)) {
-							titarray[titarray.length - 2 + (!dupreset && last ? 1 : 0)] += " (×" + (dupcount + 1) + ")";
+							titarray[titarray.length - 2 + (!dupreset && last ? 1 : 0)] += " ×" + (dupcount + 1);
 							dupcount = 0;
 						}
 						dupreset = false;
 					}
-					tit += titarray.join("\n");
-					if (pecount > 50) tit += "\n…";
-					if (tit.match(/\*/)) tit += "\n\n*: my edits";
+					tit = titarray.join("\r\n");
+					if (pecount > 50) tit += "\r\n…";
 				}
 			}
 		} else {
 			txt = pecount.status;
 			tit = pecount.responseText;
-			ret.style.setProperty("background-color", "pink");
+			count.style.setProperty("background-color", "pink");
 		}
-		ret.replaceChild(document.createTextNode(txt), ret.firstChild);
-		obj.openedits.setAttribute("title", tit);
+		count.replaceChild(document.createTextNode(txt), count.firstChild);
+		if (tit) {
+			obj.openedits.setAttribute("title", tit);
+		}
 	}
 	function mp(o, set) {
 		var li = getParent(o, "li");
@@ -201,7 +237,9 @@ It will add other request(s) to MB server, this is why it is an option.*/
 			if (set && !mp(o)) {
 				li.firstChild.className = "mp";
 			} else if (!set) {
-				if (mp(o)) { li.firstChild.classList.remove("mp"); }
+				if (mp(o)) {
+					li.firstChild.classList.remove("mp");
+				}
 				o.style.setProperty("text-decoration", "line-through");
 				li.style.setProperty("opacity", ".5");
 			}
@@ -209,24 +247,24 @@ It will add other request(s) to MB server, this is why it is an option.*/
 	}
 	function a2obj(a) {
 		return {
-			"a": a,
-			"name": a.textContent,
-			"base": a.getAttribute("href").match(new RegExp("^"+MBS+"(/[^/]+/"+RE_GUID+")"))[1]
+			a: a,
+			name: a.textContent,
+			base: a.getAttribute("href").match(new RegExp("^" + MBS + "(/[^/]+/" + RE_GUID + ")"))[1]
 		};
 	}
 	function workMainPerformer() {
 		var sections = document.querySelectorAll("div#content > table.details th");
 		var found, fartists = {}, foundartist;
-		for (var sec=0; sec < sections.length; sec++) {
+		for (var sec = 0; sec < sections.length; sec++) {
 			var type = sections[sec].textContent.match(/^(.+):$/);
 			if (type && type[1] == "recordings") {
 				found = sections[sec];
-				break;/*TOTO: fallback to live / instr / etc.*/
+				break; /*TODO: fallback to live / instr / etc.*/
 			}
 		}
 		if (found) {
-			var perfs = getParent(found, "tr").querySelectorAll("td a[href^='"+MBS+"/artist/']");
-			for (var perf=0; perf<perfs.length; perf++) {
+			var perfs = getParent(found, "tr").querySelectorAll("td a[href^='" + MBS + "/artist/']");
+			for (var perf = 0; perf < perfs.length; perf++) {
 				var href = perfs[perf].getAttribute("href");
 				if (!fartists[href]) {
 					fartists[href] = [];
@@ -234,11 +272,9 @@ It will add other request(s) to MB server, this is why it is an option.*/
 				fartists[href].push(perfs[perf]);
 			}
 			var max = 0;
-			for (var w in fartists) {
-				if (fartists.hasOwnProperty(w) && fartists[w].length > max) {
-					max = fartists[w].length;
-					foundartist = fartists[w][0];
-				}
+			for (var w in fartists) if (fartists.hasOwnProperty(w) && fartists[w].length > max) {
+				max = fartists[w].length;
+				foundartist = fartists[w][0];
 			}
 		}
 		return foundartist;

commit 2ac7e0bec9d6f61ea818dbbb2a0f7964455fba0d

fixed the broken associated artist edit history links (regression since 1e1235336daea9430d9d713470b89bbc87a8347c)

mb_PENDING-EDITS.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. PENDING EDITS
-// @version      2015.8.5.1555
+// @version      2015.8.6.1049
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_PENDING-EDITS.user.js
 // @description  musicbrainz.org: Adds/fixes links to entity (pending) edits (if any); optionally adds links to associated artist(s) (pending) edits
 // @homepage     http://userscripts-mirror.org/scripts/show/42102
@@ -57,7 +57,7 @@ It will add other request(s) to MB server, this is why it is an option.*/
 /*END OF CONFIGURATION - --- - --- - --- - */
 	var userjs = "jesus2099userjs42102";
 	var RE_GUID = "[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}";
-	var loc, entity, checked = [], xhrPendingEdits = {};
+var loc, pageEntity, checked = [], xhrPendingEdits = {};
 	var MBS = location.protocol + "//" + location.host;
 	var account = document.querySelector("div#header li.account a[href^='" + MBS + "/user/']");
 /*EDITING HISTORY*/
@@ -66,26 +66,26 @@ It will add other request(s) to MB server, this is why it is an option.*/
 		(account = unescape(account.getAttribute("href").match(/[^/]+$/))) &&
 		document.querySelector("div#sidebar") &&
 		(loc = location.pathname.match(new RegExp("^/(area|artist|collection|event|label|place|release-group|release|recording|series|work|url)/(" + RE_GUID + ")"))) &&
-		(entity = document.querySelector("div#content > div.tabs > ul.tabs > li > a")) &&
-		(entity = a2obj(entity))
+		(pageEntity = document.querySelector("div#content > div.tabs > ul.tabs > li > a")) &&
+		(pageEntity = a2obj(pageEntity))
 	) {
-		entity.editinghistory = document.querySelector("div#sidebar ul.links a[href='" + MBS + entity.base + "/edits']");
-		if (entity.editinghistory) {
-			entity.ul = getParent(entity.editinghistory, "ul");
+		pageEntity.editinghistory = document.querySelector("div#sidebar ul.links a[href='" + MBS + pageEntity.base + "/edits']");
+		if (pageEntity.editinghistory) {
+			pageEntity.ul = getParent(pageEntity.editinghistory, "ul");
 		} else {
-			entity.ul = document.querySelector("div#sidebar ul.links");
-			entity.editinghistory = createLink(entity, "edits"); /*reverts MBS-57 drawback*/
+			pageEntity.ul = document.querySelector("div#sidebar ul.links");
+			pageEntity.editinghistory = createLink(pageEntity, "edits"); /*reverts MBS-57 drawback*/
 		}
-		entity.li = getParent(entity.editinghistory, "li");
+		pageEntity.li = getParent(pageEntity.editinghistory, "li");
 /*OPEN EDITS*/
-		entity.openedits = document.querySelector("div#sidebar a[href='" + MBS + entity.base + "/open_edits']");
-		if (entity.openedits) {
-			entity.openedits = createLink(entity.openedits, "open_edits"); /*fixes MBS-2298*/
+		pageEntity.openedits = document.querySelector("div#sidebar a[href='" + MBS + pageEntity.base + "/open_edits']");
+		if (pageEntity.openedits) {
+			pageEntity.openedits = createLink(pageEntity.openedits, "open_edits"); /*fixes MBS-2298*/
 		} else {
-			entity.openedits = createLink(entity, "open_edits"); /*fixes MBS-3386*/
+			pageEntity.openedits = createLink(pageEntity, "open_edits"); /*fixes MBS-3386*/
 		}
-		checked.push(entity.base);
-		checkOpenEdits(entity);
+		checked.push(pageEntity.base);
+		checkOpenEdits(pageEntity);
 /*ARTIST LINKS*/
 		if (addArtistLinks && !/(area|artist|collection|label)/.test(loc[1])) {
 			var artists;
@@ -108,34 +108,34 @@ It will add other request(s) to MB server, this is why it is an option.*/
 					var art = a2obj(artists[arti]);
 					if (checked.indexOf(art.base) < 0) {
 						checked.push(art.base);
-						art.editinghistory = createLink(entity, "edits", art);
-						art.openedits = createLink(entity, "open_edits", art);
-						addAfter(document.createElement("hr"), entity.li);
+						art.editinghistory = createLink(pageEntity, "edits", art);
+						art.openedits = createLink(pageEntity, "open_edits", art);
+						addAfter(document.createElement("hr"), pageEntity.li);
 						checkOpenEdits(art);
 					}
 				}
 			}
 		}
 	}
-	function createLink(ent, typ, art) {
-		if (ent == entity.openedits && typ == "open_edits" && art == null && ent.parentNode.tagName == "LI") {
-			var smp = createTag("span", {a: {class: "mp"}});
-			ent.parentNode.replaceChild(smp.appendChild(ent.cloneNode(true)).parentNode, ent);
-			return smp.firstChild;
+	function createLink(entity, historyType, associatedArtist) {
+		if (entity == pageEntity.openedits && historyType == "open_edits" && !associatedArtist && entity.parentNode.tagName == "LI") {
+			var pendingEditsMarkedLink = createTag("span", {a: {class: "mp"}});
+			entity.parentNode.replaceChild(pendingEditsMarkedLink.appendChild(entity.cloneNode(true)).parentNode, entity);
+			return pendingEditsMarkedLink.firstChild;
 		} else {
-			var obj = art || ent;
-			var txt = (typ == "edits" ? "editing\u00a0history" : "open\u00a0edits");
-			txt = (art ? obj.name + " " + txt : txt.substr(0, 1).toUpperCase() + txt.substr(1));
-			var newLink = createTag("li", null, createTag("span", null, createTag("a", {a: {href: obj.base + "/" + typ}}, txt)));
-			if (art) {
-				addAfter(newLink, ent.li);
-			} else if (!art && typ == "edits") {
-				ent.ul.appendChild(document.createElement("hr"));
-				ent.ul.appendChild(newLink);
+			var currentEntity = associatedArtist || entity;
+			var linkLabel = (historyType == "edits" ? "editing\u00a0history" : "open\u00a0edits");
+			linkLabel = associatedArtist ? currentEntity.name + " " + linkLabel : linkLabel.replace(/(.)(.*)/, function(match, g1, g2, offset, string) { return g1.toUpperCase() + g2; });
+			var newLink = createTag("li", null, createTag("span", null, createTag("a", {a: {href: currentEntity.base + "/" + historyType}}, linkLabel)));
+			if (associatedArtist) {
+				addAfter(newLink, entity.li);
+			} else if (!associatedArtist && historyType == "edits") {
+				entity.ul.appendChild(document.createElement("hr"));
+				entity.ul.appendChild(newLink);
 			} else {
-				ent.ul.insertBefore(newLink, ent.li);
+				entity.ul.insertBefore(newLink, entity.li);
 			}
-			return newLink;
+			return newLink.firstChild.firstChild;
 		}
 	}
 	function checkOpenEdits(obj) {