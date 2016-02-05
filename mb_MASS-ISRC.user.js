46f78e13fbd89e7d312a016afd4cf86e48d8f824
jesus2099 <jesus2099@users.noreply.github.com>
2016-02-05 12:41:50 +0100

    some cleanup

--- mb_MASS-ISRC.user.js
+++ mb_MASS-ISRC.user.js
@@ -20,62 +20,58 @@
 // @include      https://musicbrainz.org/ws/1/track/
 // @run-at       document-end
 // ==/UserScript==
-(function(){"use strict";
-	var home = "http://notlob.eu/isrc/";
-	var dummySkip = "JESUS2099999";
-	var inputs, submit = document.querySelector("form > input[type='submit']");
-	if (self.location.href.match(/^https?:\/\/[^/]+\/ws\/1\//)) {
-		var p = document.body.querySelector("pre, p");
-		if (p && p.textContent.trim() == "") {
-			document.body.appendChild(document.createTextNode("ISRC submission seems OK. "));
-			document.body.appendChild(document.createElement("a")).appendChild(document.createTextNode("Going back to submission page (in 4 seconds).")).parentNode.setAttribute("href", home);
-			setTimeout(function(){self.location=home;}, 4000);
-		}
+"use strict";
+var home = "http://notlob.eu/isrc/";
+var dummySkip = "JESUS2099999";
+var inputs, submit = document.querySelector("form > input[type='submit']");
+if (location.href.match(/^https?:\/\/[^/]+\/ws\/1\//)) {
+	var p = document.body.querySelector("pre, p");
+	if (p && p.textContent.trim() == "") {
+		document.body.appendChild(document.createTextNode("ISRC submission seems OK. "));
+		document.body.appendChild(document.createElement("a")).appendChild(document.createTextNode("Going back to submission page (in 4 seconds).")).parentNode.setAttribute("href", home);
+		setTimeout(function() { location=home; }, 4000);
 	}
-	else if (inputs = document.querySelector("form input[name='mbid']")) {
-		inputs.addEventListener("focus", function(e) { this.select(); }, false);
-		inputs.focus();
+} else if (inputs = document.querySelector("form input[name='mbid']")) {
+	inputs.addEventListener("focus", function(event) { this.select(); }, false);
+	inputs.focus();
+} else if ((inputs = document.querySelectorAll("form table input[type='text'][name^='track'][name$='-isrc']")) && inputs.length > 0) {
+	for (var input = 0; input < inputs.length; input++) {
+		inputs[input].style.setProperty("font-family", "monospace");
+		inputs[input].setAttribute("maxlength", "12");
+		inputs[input].setAttribute("size", "12");
 	}
-	else if ((inputs = document.querySelectorAll("form table input[type='text'][name^='track'][name$='-isrc']")) && inputs.length > 0) {
-		for (var input=0; input<inputs.length; input++) {
-			inputs[input].style.setProperty("font-family", "monospace");
-			inputs[input].setAttribute("maxlength", "12");
-			inputs[input].setAttribute("size", "12");
-		}
-		var f = document.querySelector("form");
-		f.appendChild(document.createElement("strong")).appendChild(document.createTextNode(" - Paste your batch of ISRCs below — dummy ISRC if you need to skip a track: "+dummySkip));
-		f.appendChild(document.createElement("br"));
-		var t = f.appendChild(document.createElement("textarea"));
-		t.style.setProperty("width", "100%");
-		t.style.setProperty("height", "400px");
-		t.addEventListener("keyup", function(e) {
-			var isrcs = this.value.match(/[a-z]{2}\-?[a-z0-9]{3}\-?[0-9]{2}\-?[0-9]{5}/gi);
-			if (isrcs && isrcs.length > 0 && inputs.length > 0) {
-				this.style.setProperty("background-color", arrHasDupes(isrcs)?"pink":"transparent");
-				for (var isrc=0, input=0; input<inputs.length; input++) {
-					inputs[input].value = (isrc<isrcs.length&&isrcs[isrc]!=dummySkip?isrcs[isrc]:"");
-					if (isrc>0) {
-						inputs[input].style.setProperty("background-color", inputs[input].value==inputs[input-1].value&&inputs[input].value!=""?"pink":"transparent");
-					}
-					isrc++;
+	var f = document.querySelector("form");
+	f.appendChild(document.createElement("strong")).appendChild(document.createTextNode(" - Paste your batch of ISRCs below — dummy ISRC if you need to skip a track: " + dummySkip));
+	f.appendChild(document.createElement("br"));
+	var t = f.appendChild(document.createElement("textarea"));
+	t.style.setProperty("width", "100%");
+	t.style.setProperty("height", "400px");
+	t.addEventListener("keyup", function(event) {
+		var isrcs = this.value.match(/[a-z]{2}\-?[a-z0-9]{3}\-?[0-9]{2}\-?[0-9]{5}/gi);
+		if (isrcs && isrcs.length > 0 && inputs.length > 0) {
+			this.style.setProperty("background-color", arrHasDupes(isrcs) ? "pink" : "transparent");
+			for (var isrc = 0, input = 0; input < inputs.length; input++) {
+				inputs[input].value = (isrc < isrcs.length && isrcs[isrc] != dummySkip ? isrcs[isrc] : "");
+				if (isrc > 0) {
+					inputs[input].style.setProperty("background-color", inputs[input].value==inputs[input-1].value && inputs[input].value != "" ? "pink" : "transparent");
 				}
-			} else { this.style.setProperty("background-color", "transparent"); }
-			if (submit && e.ctrlKey && e.keyCode == 86) { submit.focus(); }
-		}, false);
-		t.select();
-	}
-	else if (submit && (inputs = document.querySelectorAll("form input:not([type='submit']):not([type='hidden'])")) && inputs.length == 0) {
-		submit.focus();
-	}
-	/* http://www.groggyjava.tv/freebies/duplicates.html */
-	function arrHasDupes(arr) {
-		var i, j, n;
-		n = arr.length;
-		for (i=0; i<n; i++) {
-			for (j=i+1; j<n; j++) {
-				if (arr[i]==arr[j]&&arr[i]!=dummySkip) return true;
+				isrc++;
 			}
+		} else { this.style.setProperty("background-color", "transparent"); }
+		if (submit && event.ctrlKey && event.keyCode == 86) { submit.focus(); }
+	}, false);
+	t.select();
+} else if (submit && (inputs = document.querySelectorAll("form input:not([type='submit']):not([type='hidden'])")) && inputs.length == 0) {
+	submit.focus();
+}
+/* http://www.groggyjava.tv/freebies/duplicates.html */
+function arrHasDupes(arr) {
+	var i, j, n;
+	n = arr.length;
+	for (i = 0; i < n; i++) {
+		for (j = i + 1; j < n; j++) {
+			if (arr[i] == arr[j] && arr[i] != dummySkip) return true;
 		}
-		return false;
 	}
-})();
+	return false;
+}
