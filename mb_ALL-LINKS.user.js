b39874c5a1d6bb41250edfc705a1ce628d10def5
jesus2099 <jesus2099@users.noreply.github.com>
2016-02-29 04:38:14 +0100

    Add/edit/remove user autolinks. Hide/show default autolinks. Only works with mb_LOCAL-STORAGE-MANAGER at the moment (no edit GUI yet). First half of “Save customised links to local storage” (#61).

--- mb_ALL-LINKS.user.js
+++ mb_ALL-LINKS.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         mb. ALL LINKS
-// @version      2015.12.6
+// @version      2016.2.29
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_ALL-LINKS.user.js
-// @description  Hidden links include fanpage, social network, etc. (NO duplicates) Generated links (configurable) includes plain web search, auto last.fm, Discogs and LyricWiki searches, etc. Shows begin/end dates on URL and provides edit link. Expands Wikidata links to wikipedia articles.
+// @description  Hidden links include fanpage, social network, etc. (NO duplicates) Generated autolinks (configurable) includes plain web search, auto last.fm, Discogs and LyricWiki searches, etc. Shows begin/end dates on URL and provides edit link. Expands Wikidata links to wikipedia articles.
 // @homepage     http://userscripts-mirror.org/scripts/show/108889
 // @coming-soon  https://github.com/jesus2099/konami-command/labels/mb_ALL-LINKS
 // @supportURL   https://github.com/jesus2099/konami-command/issues
-// @compatible   opera(12.17)+violentmonkey  my setup
+// @compatible   opera(12.18)+violentmonkey  my setup
 // @compatible   firefox(39)+greasemonkey    tested sometimes
 // @compatible   chromium(46)+tampermonkey   tested sometimes
 // @compatible   chrome+tampermonkey         should be same as chromium
@@ -31,63 +31,144 @@
 // ==/UserScript==
 "use strict";
 /* hint for Opera 12 users allow opera:config#UserPrefs|Allowscripttolowerwindow and opera:config#UserPrefs|Allowscripttoraisewindow */
-/*------------settings*/
-var nonLatinName = /[\u0384-\u1cf2\u1f00-\uffff]/; /*U+2FA1D is currently out of js range*/
-var autolinksOpacity = ".5"; /*can be dimmer than existing links*/
-var wikipediaLanguages = ["fr", "en", "ja", "vi"];
-	/*
-	%artist-id% (MBID)
-	%arist-name%
-	%artist-sort-name%
-	%artist-family-name-first%
-*/
-var artist_autolinks = {
+var userjs = "jesus2099_all-links_";
+var nonLatinName = /[\u0384-\u1cf2\u1f00-\uffff]/; // U+2FA1D is currently out of js range
+var autolinksOpacity = ".5";
+var languages = JSON.parse(localStorage.getItem(userjs + "languages")) || ["en", "ja", "fr", "de", "es"]; // http://musicbrainz.org/statistics/languages-scripts
+// %artist-id% (MBID)
+// %arist-name%
+// %artist-sort-name%
+// %artist-family-name-first%
+var autolinks = {default: {
 		"Web pages": "//duckduckgo.com/?q=%artist-name%",
 		"Web pages (strict)": "//duckduckgo.com/?q=%2B%22%artist-name%%22",
-	/*"Fun stuff": null, // you can insert headers this way (IMPORTANT: don't use space as first character)*/
 		"Images": "//duckduckgo.com/?q=%artist-name%+!i",
 		"Videos": "//duckduckgo.com/?q=%artist-name%+!v",
-	"Credits": null,
-		"SACEM (Interprète)": {"accept-charset":"ISO-8859-1", "action":"http://www.sacem.fr/oeuvres/oeuvre/rechercheOeuvre.do", "parameters":{"ftin":"true","tiers":"%artist-name%"}},
-		"SACEM (Auteur‐Compositeur‐Éditeur)": {"accept-charset":"ISO-8859-1", "action":"http://www.sacem.fr/oeuvres/oeuvre/rechercheOeuvre.do", "parameters":{"ftad":"true","tiers":"%artist-name%"}},
-		"JASRAC（アーティスト）": {"title":"requires JASRAC direct link", "method":"post", "accept-charset":"Shift_JIS", "enctype":"multipart/form-data", "action":"http://www2.jasrac.or.jp/eJwid/main.jsp?trxID=A00401-3", "parameters":{"IN_ARTIST_NAME_OPTION1":"0","IN_ARTIST_NAME1":"%artist-name%","IN_DEFAULT_WORKS_KOUHO_MAX":"100","IN_DEFAULT_WORKS_KOUHO_SEQ":"1","IN_DEFAULT_SEARCH_WORKS_NAIGAI":"0","RESULT_CURRENT_PAGE":"1"}},
-		"JASRAC（著作者）": {"title":"requires JASRAC direct link", "method":"post", "accept-charset":"Shift_JIS", "enctype":"multipart/form-data", "action":"http://www2.jasrac.or.jp/eJwid/main.jsp?trxID=A00401-3", "parameters":{"IN_KEN_NAME_OPTION1":"0","IN_KEN_NAME1":"%artist-family-name-first%","IN_KEN_NAME_JOB1":"0","IN_DEFAULT_WORKS_KOUHO_MAX":"100","IN_DEFAULT_WORKS_KOUHO_SEQ":"1","IN_DEFAULT_SEARCH_WORKS_NAIGAI":"0","RESULT_CURRENT_PAGE":"1"}},
+		"Credits": null,
+		"SACEM (Interprète)": {
+			acceptCharset: "ISO-8859-1",
+			action: "http://www.sacem.fr/oeuvres/oeuvre/rechercheOeuvre.do",
+			parameters: {
+				"ftin": "true",
+				"tiers": "%artist-name%"
+			}
+		},
+		"SACEM (Auteur‐Compositeur‐Éditeur)": {
+			acceptCharset: "ISO-8859-1",
+			action: "http://www.sacem.fr/oeuvres/oeuvre/rechercheOeuvre.do",
+			parameters: {
+				"ftad": "true",
+				"tiers": "%artist-name%"
+			}
+		},
+		"JASRAC（アーティスト）": {
+			title: "requires JASRAC direct link",
+			method: "post",
+			acceptCharset: "Shift_JIS",
+			enctype: "multipart/form-data",
+			action: "http://www2.jasrac.or.jp/eJwid/main.jsp?trxID=A00401-3",
+			parameters: {
+				"IN_ARTIST_NAME_OPTION1": "0",
+				"IN_ARTIST_NAME1": "%artist-name%",
+				"IN_DEFAULT_WORKS_KOUHO_MAX": "100",
+				"IN_DEFAULT_WORKS_KOUHO_SEQ": "1",
+				"IN_DEFAULT_SEARCH_WORKS_NAIGAI": "0",
+				"RESULT_CURRENT_PAGE": "1"
+			}
+		},
+		"JASRAC（著作者）": {
+			title: "requires JASRAC direct link",
+			method: "post",
+			acceptCharset: "Shift_JIS",
+			enctype: "multipart/form-data",
+			action: "http://www2.jasrac.or.jp/eJwid/main.jsp?trxID=A00401-3",
+			parameters: {
+				"IN_KEN_NAME_OPTION1": "0",
+				"IN_KEN_NAME1": "%artist-family-name-first%",
+				"IN_KEN_NAME_JOB1": "0",
+				"IN_DEFAULT_WORKS_KOUHO_MAX": "100",
+				"IN_DEFAULT_WORKS_KOUHO_SEQ": "1",
+				"IN_DEFAULT_SEARCH_WORKS_NAIGAI": "0",
+				"RESULT_CURRENT_PAGE": "1"
+			}
+		},
 		"音楽の森（アーティスト）": "//www.minc.gr.jp/db/ArtNmSrch.aspx?ArtNm=%artist-name%",
 		"音楽の森（著作者）": "//www.minc.gr.jp/db/KenriSrch.aspx?KENRISYANM=%artist-family-name-first%",
-	"Lyrics": null,
+		"Lyrics": null,
 		"decoda": "http://decoda.com/search?q=%artist-name%",
 		"LyricWiki": "//lyrics.wikia.com/%artist-name%",
-		"うたまっぷ（アーティスト）": {"accept-charset":"euc-jp", "action":"http://www.utamap.com/searchkasi.php", "parameters":{"searchname":"artist","word":"%artist-name%"}},
-		"うたまっぷ（作詞者）": {"accept-charset":"euc-jp", "action":"http://www.utamap.com/searchkasi.php", "parameters":{"searchname":"sakusi","word":"%artist-name%"}},
-		"うたまっぷ（作曲者）": {"accept-charset":"euc-jp", "action":"http://www.utamap.com/searchkasi.php", "parameters":{"searchname":"sakyoku","word":"%artist-name%"}},
+		"うたまっぷ（アーティスト）": {
+			acceptCharset: "euc-jp",
+			action: "http://www.utamap.com/searchkasi.php",
+			parameters: {
+				"searchname": "artist",
+				"word": "%artist-name%"
+			}
+		},
+		"うたまっぷ（作詞者）": {
+			acceptCharset: "euc-jp",
+			action: "http://www.utamap.com/searchkasi.php",
+			parameters: {
+				"searchname": "sakusi",
+				"word": "%artist-name%"
+			}
+		},
+		"うたまっぷ（作曲者）": {
+			acceptCharset: "euc-jp",
+			action: "http://www.utamap.com/searchkasi.php",
+			parameters: {
+				"searchname": "sakyoku",
+				"word":"%artist-name%"
+			}
+		},
 		"J-Lyric（歌手）": "http://j-lyric.net/index.php?ka=%artist-name%",
 		"歌詞タイム": "//duckduckgo.com/?q=site%3Akasi-time.com+subcat+intitle:%artist-name%",
-	"Japanese stuff": null,
+		"Japanese stuff": null,
 		"VGMdb": "http://vgmdb.net/search?q=%artist-name%",
 		"ja.Wikipedia": "//ja.wikipedia.org/w/index.php?search=%artist-name%",
-		"CDJournal search": {"accept-charset":"euc-jp", "action":"https://cdjournal.com/search/do/", "parameters":{"k":"%artist-name%","target":"a"}},
-		"Joshinweb search": {"accept-charset":"Shift_JIS", "action":"//joshinweb.jp/cdshops/Dps", "parameters":{"KEY":"ARTIST","FM":"0","KEYWORD":"%artist-name%"}},
+		"CDJournal search": {
+			acceptCharset: "euc-jp",
+			action: "https://cdjournal.com/search/do/",
+			parameters: {
+				"k": "%artist-name%",
+				"target": "a"
+			}
+		},
+		"Joshinweb search": {
+			acceptCharset: "Shift_JIS",
+			action: "//joshinweb.jp/cdshops/Dps",
+			parameters: {
+				"KEY": "ARTIST",
+				"FM": "0",
+				"KEYWORD": "%artist-name%"
+			}
+		},
 		"Yunisan": "//duckduckgo.com/?q=site:www22.big.or.jp+%22%2F%7Eyunisan%2Fvi%2F%22+%artist-name%",
 		"VKDB": "//duckduckgo.com/?q=site:vkdb.jp+%artist-name%",
-	"Vietnamese stuff": null,
+		"Vietnamese stuff": null,
 		"vi.Wikipedia": "//vi.wikipedia.org/w/index.php?search=%artist-name%",
 		"nhạc số": "http://nhacso.net/tim-kiem-nghe-si.html?keyName=%artist-name%",
-	"Korean stuff": null,
+		"Korean stuff": null,
 		"maniadb": "http://www.maniadb.com/search.asp?sr=PO&q=%artist-name%",
-	"Other databases": null,
+		"Other databases": null,
 		"AllMusic": "http://www.allmusic.com/search/artist/%artist-name%",
 		"Discogs": "http://www.discogs.com/search?q=%artist-name%&type=artist",
 		"ISNI": "//isni.oclc.nl/xslt/CMD?ACT=SRCHA&IKT=8006&TRM=%artist-name%",
 		"Rate Your Music": "//rateyourmusic.com/search?searchtype=a&searchterm=%artist-name%", 
 		"Second hand songs": "http://secondhandsongs.com/search?search_text=%artist-name%",
 		"WhoSampled": "//www.whosampled.com/search/artists/?h=1&q=%artist-name%", 
-	"Other stuff": null,
+		"Other stuff": null,
 		"en.Wikipedia": "//en.wikipedia.org/w/index.php?search=%artist-name%",
 		"*.Wikipedia": "//duckduckgo.com/?q=site:wikipedia.org+%22%artist-name%%22",
 		"Lastfm (mbid)": "http://last.fm/mbid/%artist-id%",
 		"Lastfm (name)": "http://last.fm/music/%artist-name%",
 		"BBC Music": "http://www.bbc.co.uk/music/artists/%artist-id%",
-};
+}};
+var enabledDefaultAutolinks = {};
+var loadedSettings = JSON.parse(localStorage.getItem(userjs + "enabled-default-autolinks")) || {};
+for (var link in autolinks.default) if (autolinks.default.hasOwnProperty(link)) {
+	enabledDefaultAutolinks[link] = typeof loadedSettings[link] != "undefined" ? loadedSettings[link] : true;
+}
 var favicons = {
 	"allmusic.com": "http://allmusic.com/img/favicon.ico",
 	"ameblo.jp": "http://ameblo.jp/favicon.ico",
@@ -109,9 +190,7 @@ var favicons = {
 	"wikipedia.org": "http://en.wikipedia.org/favicon.ico",
 	"yahoo.": "http://blogs.yahoo.co.jp/favicon.ico",
 };
-var guessOtherFavicons = true;
-/*------------end of settings (don't edit below) */
-var userjs = "j2ujs108889";
+var guessOtherFavicons = false;
 var sidebar = document.getElementById("sidebar");
 var arelsws = "/ws/2/artist/%artist-id%?inc=url-rels";
 var existingLinks, extlinks;
@@ -123,7 +202,7 @@ for (var s = 0; s < document.styleSheets.length; s++)
 			hrStyle.css += hrStyle.match[2];
 if (hrStyle.css) {
 	document.head.appendChild(document.createElement("style")).setAttribute("type", "text/css");
-	document.styleSheets[document.styleSheets.length - 1].insertRule("div#sidebar ul.external_links hr { margin-top:8px!important; width:inherit!important; " + hrStyle.css + "}", 0);
+	document.styleSheets[document.styleSheets.length - 1].insertRule("div#sidebar ul.external_links hr { margin-top: 8px !important; width: inherit !important; " + hrStyle.css + "}", 0);
 }
 function main() {
 	if (sidebar) {
@@ -180,33 +259,35 @@ function main() {
 									}
 								}
 							}
-							/*artist_autolinks*/
+							// Default and user autolinks
 							extlinksOpacity = autolinksOpacity;
-							haslinks = false;
-							for (var link in artist_autolinks) if (artist_autolinks.hasOwnProperty(link)) {
-								var target = artist_autolinks[link];
-								var sntarget = null;
-								if (target) {
-									if (typeof target == "string") {
-										target = target.replace(/%artist-id%/, artistid);
-										if (target.match(/%artist-name%/) && artistname != artistsortnameSwapped && artistname.match(nonLatinName)) {
-											sntarget = target.replace(/%artist-name%/, encodeURIComponent(artistsortnameSwapped));
+							autolinks.user = JSON.parse(localStorage.getItem(userjs + "user-autolinks")) || {};
+							for (var defaultOrUser in autolinks) if (autolinks.hasOwnProperty(defaultOrUser)) {
+								haslinks = false;
+								for (var link in autolinks[defaultOrUser]) if (autolinks[defaultOrUser].hasOwnProperty(link) && (defaultOrUser == "user" || enabledDefaultAutolinks[link])) {
+									var target = autolinks[defaultOrUser][link];
+									var sntarget = null;
+									if (target) {
+										if (typeof target == "string") {
+											target = target.replace(/%artist-id%/, artistid);
+											if (target.match(/%artist-name%/) && artistname != artistsortnameSwapped && artistname.match(nonLatinName)) {
+												sntarget = target.replace(/%artist-name%/, encodeURIComponent(artistsortnameSwapped));
+											}
+											target = target.replace(/%artist-name%/, encodeURIComponent(artistname));
+											target = target.replace(/%artist-family-name-first%/, encodeURIComponent(artistname.match(nonLatinName) ? artistname : artistsortname));
+										} else {
+											var aname = target.acceptCharset;
+											aname = aname && aname.match(/iso-8859/i) && artistname != artistsortnameSwapped && artistname.match(nonLatinName) ? artistsortnameSwapped : artistname;
+											for (var param in target.parameters) if (target.parameters.hasOwnProperty(param)) {
+												target.parameters[param] = target.parameters[param].replace(/%artist-id%/, artistid).replace(/%artist-name%/, aname).replace(/%artist-family-name-first%/, artistname.match(nonLatinName) ? artistname : artistsortname);
+											}
 										}
-										target = target.replace(/%artist-name%/, encodeURIComponent(artistname));
-										target = target.replace(/%artist-family-name-first%/, encodeURIComponent(artistname.match(nonLatinName) ? artistname : artistsortname));
 									}
-									else {
-										var aname = target["accept-charset"];
-										aname = aname && aname.match(/iso-8859/i) && artistname != artistsortnameSwapped && artistname.match(nonLatinName) ? artistsortnameSwapped : artistname;
-										for (var param in target.parameters) if (target.parameters.hasOwnProperty(param)) {
-											target.parameters[param] = target.parameters[param].replace(/%artist-id%/, artistid).replace(/%artist-name%/, aname).replace(/%artist-family-name-first%/, artistname.match(nonLatinName) ? artistname : artistsortname);
+									if (addExternalLink({text: link, target: target, sntarget: sntarget})) {
+										if (!haslinks) {
+											haslinks = true;
+											addExternalLink({text: " " + defaultOrUser.substr(0, 1).toUpperCase() + defaultOrUser.substr(1).toLowerCase() + " autolinks"});
 										}
-									}
-								}
-								if (addExternalLink({text: link, target: target, sntarget: sntarget})) {
-									if (!haslinks) {
-										haslinks = true;
-										addExternalLink({text: " Generated links"});
 									}
 								}
 							}
@@ -223,7 +304,7 @@ function main() {
 		}/*artist*/
 		/*wikidata to wikipedia*/
 		//TODO: maybe use https://github.com/dansingerman/jQuery-Browser-Language to detect user supported languages
-		if (wikipediaLanguages && Array.isArray(wikipediaLanguages) && wikipediaLanguages.length > 0) {
+		if (languages && Array.isArray(languages) && languages.length > 0) {
 			var wikidatas = sidebar.querySelectorAll("ul.external_links > li a[href*='wikidata.org/wiki/Q']");
 			for (var wd = 0; wd < wikidatas.length; wd++) {
 				var wikidataID = wikidatas[wd].getAttribute("href").match(/Q\d+$/);
@@ -237,8 +318,8 @@ function main() {
 						removeNode(wikidataListItem.querySelector("img[src$='loading.gif']"));
 						var wikidata = JSON.parse(this.responseText);
 						if (wikidata && wikidata.entities && (wikidata = wikidata.entities[this.id])) {
-							for (var languageCode = 0; languageCode < wikipediaLanguages.length; languageCode++) {
-								var wikiEntry = wikidata.sitelinks[wikipediaLanguages[languageCode] + "wiki"];
+							for (var languageCode = 0; languageCode < languages.length; languageCode++) {
+								var wikiEntry = wikidata.sitelinks[languages[languageCode] + "wiki"];
 								if (wikiEntry) {
 									var href = wikiEntry.url.replace(/^https?:/, "");
 									var ul;
@@ -247,7 +328,7 @@ function main() {
 										if (!ul) {
 											ul = wikidataListItem.appendChild(document.createElement("ul"));
 										}
-										ul.appendChild(createTag("li", {s: {padding: "0"}}, [wikipediaLanguages[languageCode], ": ", createTag("a", {a: {href: href}}, wikiEntry.title)]));
+										ul.appendChild(createTag("li", {s: {padding: "0"}}, [languages[languageCode], ": ", createTag("a", {a: {href: href}}, wikiEntry.title)]));
 									}
 								}
 							}
@@ -275,6 +356,7 @@ function addExternalLink(parameters/*text, target, begin, end, sntarget, mbid*/)
 		}
 	}
 	if (parameters.target) {
+		// This is an autolink
 		var li;
 		if (typeof parameters.target != "string") {
 			var form = createTag("form", {a: {action: parameters.target.action}});
@@ -289,10 +371,10 @@ function addExternalLink(parameters/*text, target, begin, end, sntarget, mbid*/)
 						form.appendChild(createTag("input", {a: {name: param, type: "hidden", value: parameters.target.parameters[param]}}));
 					}
 				} else {
-					if (attr.match(/accept-charset|enctype|method/)) {
+					if (attr.match(/acceptCharset|enctype|method/)) {
 						info = parameters.target[attr] + " " + info;
 					}
-					form.setAttribute(attr, parameters.target[attr]);
+					form.setAttribute(attr.replace(/[A-Z]/g, "-$&").toLowerCase(), parameters.target[attr]);
 				}
 			}
 			form.appendChild(createTag("a", {a: {title: info}, e: {click: function(event) {
@@ -373,11 +455,14 @@ function addExternalLink(parameters/*text, target, begin, end, sntarget, mbid*/)
 		favicontry[ifit].src = favurlfound;
 		favicontry[ifit].to = setTimeout(function(){ favicontry[ifit].src = "/"; }, 5000);
 	} else {
+		// This is a header
 		var li = createTag("li", {s: {fontWeight: "bold"}}, parameters.text);
-		if (parameters.text.indexOf(" ") == 0) {
+		if (parameters.text.indexOf(" ") === 0) {
+			// Level 1 header
 			li.style.setProperty("padding-top", "0px");
 			extlinks.insertBefore(li, extlinks.lastChild);
 		} else {
+			// Level 2 header
 			li.style.setProperty("padding", "0px");
 			li.style.setProperty("float", "right");
 			extlinks.appendChild(li);
