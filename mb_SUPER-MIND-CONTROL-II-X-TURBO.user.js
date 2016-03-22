80c6c6f37caa1120f8008e867f07a0c6176de3c5
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2016-03-22 10:27:12 +0100

    Require clicking on its header to open SERVER_SWITCH (MBS-8843, closes #180)

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -2,7 +2,7 @@
 var meta = {rawmdb: function() {
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2016.2.25
+// @version      2016.3.22
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
 // @description  musicbrainz.org power-ups (mbsandbox.org too): RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / RELEASE_EDITOR_PROTECTOR. prevent accidental cancel by better tab key navigation / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / MAX_RECENT_ENTITIES / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / MERGE_USER_MENUS / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @coming-soon  https://github.com/jesus2099/konami-command/labels/mb_SUPER-MIND-CONTROL-II-X-TURBO
@@ -869,18 +869,22 @@ if (j2sets.SERVER_SWITCH) {
 		} else {
 			servname = "MBS";
 		}
-		var menu = langMenu.parentNode.insertBefore(createTag("li", {a: {class: userjs + "serverSwitch"}, s: {float: "right", position: "relative"}}, [createTag("a", {a: {title: "Server Switch"}}, createTag("code", {}, [meta.icon.cloneNode(), " ", servname])), document.createElement("ul")]), langMenu);
-		menu.addEventListener("mouseover", function(event) {
-			this.firstChild.nextSibling.style.setProperty("left", "inherit");
-			this.firstChild.nextSibling.style.setProperty("right", "0px");
-		});
-		menu.addEventListener("mouseout", function(event) {
-			if (!getParent(event.relatedTarget, "li", userjs + "serverSwitch")) {
-				this.firstChild.nextSibling.style.removeProperty("left");
-				this.firstChild.nextSibling.style.removeProperty("right");
+		var menu = langMenu.parentNode.insertBefore(createTag("li", {a: {class: userjs + "serverSwitch"}, s: {float: "right", position: "relative"}}, [createTag("span", {a: {title: "Server Switch", class: "menu-header"}}, createTag("code", {}, [meta.icon.cloneNode(), " ", servname])), document.createElement("ul")]), langMenu);
+		menu.addEventListener("click", function(event) {
+			stop(event);
+			for (var openMenus = document.querySelectorAll(".header ul.menu li.fake-active"), m = 0; m < openMenus.length; m++) {
+				sendEvent(openMenus[m], "click");
+			}
+			if (this.lastChild.style.getPropertyValue("left").match(/inherit|-\d{1,4}px/)) {
+				this.classList.remove("fake-active");
+				this.lastChild.style.setProperty("left", "-10000px");
+			} else {
+				this.classList.add("fake-active");
+				var ulStyle = self.getComputedStyle(this.lastChild);
+				this.lastChild.style.setProperty("left", "-" + (ulStyle.getPropertyValue("width").match(/\d+/) - self.getComputedStyle(this).getPropertyValue("width").match(/\d+/) + 2 * ulStyle.getPropertyValue("padding").match(/\d+/)) + "px");
 			}
 		});
-		//MB.Control.HeaderMenu(menu);
+		menu.lastChild.addEventListener("click", function(event) { event.stopPropagation(); });
 		menu = menu.firstChild.nextSibling;
 		var mbMains = ["", "beta."];
 		for (var mb = 0; mb < mbMains.length; mb++) {
