ce0dce415529976b20440045e49bc6c3d297b255
jesus2099 <jesus2099@users.noreply.github.com>
2016-04-13 17:15:26 +0200

    cleanup (2) favourite icons

--- mb_ALL-LINKS.user.js
+++ mb_ALL-LINKS.user.js
@@ -40,154 +40,154 @@ var languages = JSON.parse(localStorage.getItem(userjs + "languages")) || ["en",
 // %artist-sort-name%
 // %artist-family-name-first%
 var autolinks = {default: {
-		"Web pages": "//duckduckgo.com/?q=%artist-name%",
-		"Web pages (strict)": "//duckduckgo.com/?q=%2B%22%artist-name%%22",
-		"Images": "//duckduckgo.com/?q=%artist-name%+!i",
-		"Videos": "//duckduckgo.com/?q=%artist-name%+!v",
-		"Credits": null,
-		"SACEM (Interprète)": {
-			acceptCharset: "ISO-8859-1",
-			action: "http://www.sacem.fr/oeuvres/oeuvre/rechercheOeuvre.do",
-			parameters: {
-				"ftin": "true",
-				"tiers": "%artist-name%"
-			}
-		},
-		"SACEM (Auteur‐Compositeur‐Éditeur)": {
-			acceptCharset: "ISO-8859-1",
-			action: "http://www.sacem.fr/oeuvres/oeuvre/rechercheOeuvre.do",
-			parameters: {
-				"ftad": "true",
-				"tiers": "%artist-name%"
-			}
-		},
-		"JASRAC（アーティスト）": {
-			title: "requires JASRAC direct link",
-			method: "post",
-			acceptCharset: "Shift_JIS",
-			enctype: "multipart/form-data",
-			action: "http://www2.jasrac.or.jp/eJwid/main.jsp?trxID=A00401-3",
-			parameters: {
-				"IN_ARTIST_NAME_OPTION1": "0",
-				"IN_ARTIST_NAME1": "%artist-name%",
-				"IN_DEFAULT_WORKS_KOUHO_MAX": "100",
-				"IN_DEFAULT_WORKS_KOUHO_SEQ": "1",
-				"IN_DEFAULT_SEARCH_WORKS_NAIGAI": "0",
-				"RESULT_CURRENT_PAGE": "1"
-			}
-		},
-		"JASRAC（著作者）": {
-			title: "requires JASRAC direct link",
-			method: "post",
-			acceptCharset: "Shift_JIS",
-			enctype: "multipart/form-data",
-			action: "http://www2.jasrac.or.jp/eJwid/main.jsp?trxID=A00401-3",
-			parameters: {
-				"IN_KEN_NAME_OPTION1": "0",
-				"IN_KEN_NAME1": "%artist-family-name-first%",
-				"IN_KEN_NAME_JOB1": "0",
-				"IN_DEFAULT_WORKS_KOUHO_MAX": "100",
-				"IN_DEFAULT_WORKS_KOUHO_SEQ": "1",
-				"IN_DEFAULT_SEARCH_WORKS_NAIGAI": "0",
-				"RESULT_CURRENT_PAGE": "1"
-			}
-		},
-		"音楽の森（アーティスト）": "//www.minc.gr.jp/db/ArtNmSrch.aspx?ArtNm=%artist-name%",
-		"音楽の森（著作者）": "//www.minc.gr.jp/db/KenriSrch.aspx?KENRISYANM=%artist-family-name-first%",
-		"Lyrics": null,
-		"decoda": "http://decoda.com/search?q=%artist-name%",
-		"LyricWiki": "//lyrics.wikia.com/%artist-name%",
-		"うたまっぷ（アーティスト）": {
-			acceptCharset: "euc-jp",
-			action: "http://www.utamap.com/searchkasi.php",
-			parameters: {
-				"searchname": "artist",
-				"word": "%artist-name%"
-			}
-		},
-		"うたまっぷ（作詞者）": {
-			acceptCharset: "euc-jp",
-			action: "http://www.utamap.com/searchkasi.php",
-			parameters: {
-				"searchname": "sakusi",
-				"word": "%artist-name%"
-			}
-		},
-		"うたまっぷ（作曲者）": {
-			acceptCharset: "euc-jp",
-			action: "http://www.utamap.com/searchkasi.php",
-			parameters: {
-				"searchname": "sakyoku",
-				"word":"%artist-name%"
-			}
-		},
-		"J-Lyric（歌手）": "http://j-lyric.net/index.php?ka=%artist-name%",
-		"歌詞タイム": "//duckduckgo.com/?q=site%3Akasi-time.com+subcat+intitle:%artist-name%",
-		"Japanese stuff": null,
-		"VGMdb": "http://vgmdb.net/search?q=%artist-name%",
-		"ja.Wikipedia": "//ja.wikipedia.org/w/index.php?search=%artist-name%",
-		"CDJournal search": {
-			acceptCharset: "euc-jp",
-			action: "https://cdjournal.com/search/do/",
-			parameters: {
-				"k": "%artist-name%",
-				"target": "a"
-			}
-		},
-		"Joshinweb search": {
-			acceptCharset: "Shift_JIS",
-			action: "//joshinweb.jp/cdshops/Dps",
-			parameters: {
-				"KEY": "ARTIST",
-				"FM": "0",
-				"KEYWORD": "%artist-name%"
-			}
-		},
-		"Yunisan": "//duckduckgo.com/?q=site:www22.big.or.jp+%22%2F%7Eyunisan%2Fvi%2F%22+%artist-name%",
-		"VKDB": "//duckduckgo.com/?q=site:vkdb.jp+%artist-name%",
-		"Vietnamese stuff": null,
-		"vi.Wikipedia": "//vi.wikipedia.org/w/index.php?search=%artist-name%",
-		"nhạc số": "http://nhacso.net/tim-kiem-nghe-si.html?keyName=%artist-name%",
-		"Korean stuff": null,
-		"maniadb": "http://www.maniadb.com/search.asp?sr=PO&q=%artist-name%",
-		"Other databases": null,
-		"AllMusic": "http://www.allmusic.com/search/artist/%artist-name%",
-		"Discogs": "http://www.discogs.com/search?q=%artist-name%&type=artist",
-		"ISNI": "//isni.oclc.nl/xslt/CMD?ACT=SRCHA&IKT=8006&TRM=%artist-name%",
-		"Rate Your Music": "//rateyourmusic.com/search?searchtype=a&searchterm=%artist-name%", 
-		"Second hand songs": "http://secondhandsongs.com/search?search_text=%artist-name%",
-		"WhoSampled": "//www.whosampled.com/search/artists/?h=1&q=%artist-name%", 
-		"Other stuff": null,
-		"en.Wikipedia": "//en.wikipedia.org/w/index.php?search=%artist-name%",
-		"*.Wikipedia": "//duckduckgo.com/?q=site:wikipedia.org+%22%artist-name%%22",
-		"Lastfm (mbid)": "http://last.fm/mbid/%artist-id%",
-		"Lastfm (name)": "http://last.fm/music/%artist-name%",
-		"BBC Music": "http://www.bbc.co.uk/music/artists/%artist-id%",
+	"Web pages": "//duckduckgo.com/?q=%artist-name%",
+	"Web pages (strict)": "//duckduckgo.com/?q=%2B%22%artist-name%%22",
+	"Images": "//duckduckgo.com/?q=%artist-name%+!i",
+	"Videos": "//duckduckgo.com/?q=%artist-name%+!v",
+	"Credits": null,
+	"SACEM (Interprète)": {
+		acceptCharset: "ISO-8859-1",
+		action: "http://www.sacem.fr/oeuvres/oeuvre/rechercheOeuvre.do",
+		parameters: {
+			"ftin": "true",
+			"tiers": "%artist-name%"
+		}
+	},
+	"SACEM (Auteur‐Compositeur‐Éditeur)": {
+		acceptCharset: "ISO-8859-1",
+		action: "http://www.sacem.fr/oeuvres/oeuvre/rechercheOeuvre.do",
+		parameters: {
+			"ftad": "true",
+			"tiers": "%artist-name%"
+		}
+	},
+	"JASRAC（アーティスト）": {
+		title: "requires JASRAC direct link",
+		method: "post",
+		acceptCharset: "Shift_JIS",
+		enctype: "multipart/form-data",
+		action: "http://www2.jasrac.or.jp/eJwid/main.jsp?trxID=A00401-3",
+		parameters: {
+			"IN_ARTIST_NAME_OPTION1": "0",
+			"IN_ARTIST_NAME1": "%artist-name%",
+			"IN_DEFAULT_WORKS_KOUHO_MAX": "100",
+			"IN_DEFAULT_WORKS_KOUHO_SEQ": "1",
+			"IN_DEFAULT_SEARCH_WORKS_NAIGAI": "0",
+			"RESULT_CURRENT_PAGE": "1"
+		}
+	},
+	"JASRAC（著作者）": {
+		title: "requires JASRAC direct link",
+		method: "post",
+		acceptCharset: "Shift_JIS",
+		enctype: "multipart/form-data",
+		action: "http://www2.jasrac.or.jp/eJwid/main.jsp?trxID=A00401-3",
+		parameters: {
+			"IN_KEN_NAME_OPTION1": "0",
+			"IN_KEN_NAME1": "%artist-family-name-first%",
+			"IN_KEN_NAME_JOB1": "0",
+			"IN_DEFAULT_WORKS_KOUHO_MAX": "100",
+			"IN_DEFAULT_WORKS_KOUHO_SEQ": "1",
+			"IN_DEFAULT_SEARCH_WORKS_NAIGAI": "0",
+			"RESULT_CURRENT_PAGE": "1"
+		}
+	},
+	"音楽の森（アーティスト）": "//www.minc.gr.jp/db/ArtNmSrch.aspx?ArtNm=%artist-name%",
+	"音楽の森（著作者）": "//www.minc.gr.jp/db/KenriSrch.aspx?KENRISYANM=%artist-family-name-first%",
+	"Lyrics": null,
+	"decoda": "http://decoda.com/search?q=%artist-name%",
+	"LyricWiki": "//lyrics.wikia.com/%artist-name%",
+	"うたまっぷ（アーティスト）": {
+		acceptCharset: "euc-jp",
+		action: "http://www.utamap.com/searchkasi.php",
+		parameters: {
+			"searchname": "artist",
+			"word": "%artist-name%"
+		}
+	},
+	"うたまっぷ（作詞者）": {
+		acceptCharset: "euc-jp",
+		action: "http://www.utamap.com/searchkasi.php",
+		parameters: {
+			"searchname": "sakusi",
+			"word": "%artist-name%"
+		}
+	},
+	"うたまっぷ（作曲者）": {
+		acceptCharset: "euc-jp",
+		action: "http://www.utamap.com/searchkasi.php",
+		parameters: {
+			"searchname": "sakyoku",
+			"word":"%artist-name%"
+		}
+	},
+	"J-Lyric（歌手）": "http://j-lyric.net/index.php?ka=%artist-name%",
+	"歌詞タイム": "//duckduckgo.com/?q=site%3Akasi-time.com+subcat+intitle:%artist-name%",
+	"Japanese stuff": null,
+	"VGMdb": "http://vgmdb.net/search?q=%artist-name%",
+	"ja.Wikipedia": "//ja.wikipedia.org/w/index.php?search=%artist-name%",
+	"CDJournal search": {
+		acceptCharset: "euc-jp",
+		action: "https://cdjournal.com/search/do/",
+		parameters: {
+			"k": "%artist-name%",
+			"target": "a"
+		}
+	},
+	"Joshinweb search": {
+		acceptCharset: "Shift_JIS",
+		action: "//joshinweb.jp/cdshops/Dps",
+		parameters: {
+			"KEY": "ARTIST",
+			"FM": "0",
+			"KEYWORD": "%artist-name%"
+		}
+	},
+	"Yunisan": "//duckduckgo.com/?q=site:www22.big.or.jp+%22%2F%7Eyunisan%2Fvi%2F%22+%artist-name%",
+	"VKDB": "//duckduckgo.com/?q=site:vkdb.jp+%artist-name%",
+	"Vietnamese stuff": null,
+	"vi.Wikipedia": "//vi.wikipedia.org/w/index.php?search=%artist-name%",
+	"nhạc số": "http://nhacso.net/tim-kiem-nghe-si.html?keyName=%artist-name%",
+	"Korean stuff": null,
+	"maniadb": "http://www.maniadb.com/search.asp?sr=PO&q=%artist-name%",
+	"Other databases": null,
+	"AllMusic": "http://www.allmusic.com/search/artist/%artist-name%",
+	"Discogs": "http://www.discogs.com/search?q=%artist-name%&type=artist",
+	"ISNI": "//isni.oclc.nl/xslt/CMD?ACT=SRCHA&IKT=8006&TRM=%artist-name%",
+	"Rate Your Music": "//rateyourmusic.com/search?searchtype=a&searchterm=%artist-name%", 
+	"Second hand songs": "http://secondhandsongs.com/search?search_text=%artist-name%",
+	"WhoSampled": "//www.whosampled.com/search/artists/?h=1&q=%artist-name%", 
+	"Other stuff": null,
+	"en.Wikipedia": "//en.wikipedia.org/w/index.php?search=%artist-name%",
+	"*.Wikipedia": "//duckduckgo.com/?q=site:wikipedia.org+%22%artist-name%%22",
+	"Lastfm (mbid)": "http://last.fm/mbid/%artist-id%",
+	"Lastfm (name)": "http://last.fm/music/%artist-name%",
+	"BBC Music": "http://www.bbc.co.uk/music/artists/%artist-id%",
 }};
 var enabledDefaultAutolinks = {};
 var loadedSettings = JSON.parse(localStorage.getItem(userjs + "enabled-default-autolinks")) || {};
 for (var link in autolinks.default) if (autolinks.default.hasOwnProperty(link)) {
 	enabledDefaultAutolinks[link] = typeof loadedSettings[link] != "undefined" ? loadedSettings[link] : true;
 }
-var favicons = {
-	"allmusic.com": "http://allmusic.com/img/favicon.ico",
+var favicons = {//TODO: /favicon.ico entries are quite useless as they are auto found otherwise
+	"allmusic.com": "//www.allmusic.com/favicon.ico",
 	"ameblo.jp": "http://ameblo.jp/favicon.ico",
-	"bbc.co.uk": "http://www.bbc.co.uk/favicon.ico",
-	/*"discogs.com": "http://musicbrainz.org/static/images/favicons/discogs-16.png",*/
-	"exblog.jp": "http://exblog.jp/favicon.ico",
-	"joshinweb.jp": "http://joshinweb.jp/favicon.ico",
-	"last.fm": "http://musicbrainz.org/static/images/favicons/lastfm-16.png",
-	"lastfm.": "http://musicbrainz.org/static/images/favicons/lastfm-16.png",
+	"bbc.co.uk": "//www.bbc.co.uk/favicon.ico",
+	"discogs.com": "//www.discogs.com/favicon.ico",
+	"exblog.jp": "//www.exblog.jp/favicon.ico",
+	"joshinweb.jp": "//joshinweb.jp/favicon.ico",
+	"last.fm": "//musicbrainz.org/static/images/favicons/lastfm-16.png",
+	"lastfm.": "//musicbrainz.org/static/images/favicons/lastfm-16.png",
 	"livedoor.jp": "http://blog.livedoor.jp/favicon.ico",
-	"lyrics.wikia.com": "http://lyrics.wikia.com/favicon.ico",
+	"lyrics.wikia.com": "//lyrics.wikia.com/favicon.ico",
 	"metal-archives.com": "http://www.metal-archives.com/favicon.ico",
-	"musicbrainz.org": "http://musicbrainz.org/favicon.ico",
-	"rakuten.co.jp": "http://plaza.rakuten.co.jp/favicon.ico",
-	"secondhandsongs.com": "http://www.secondhandsongs.com/art/icons/shs.png",
-	"soundcloud.com": "http://musicbrainz.org/static/images/favicons/soundcloud-16.png",
+	"musicbrainz.org": "//musicbrainz.org/favicon.ico",
+	"rakuten.co.jp": "//plaza.rakuten.co.jp/favicon.ico",
+	"secondhandsongs.com": "http://secondhandsongs.com/favicon.ico",
+	"soundcloud.com": "//musicbrainz.org/static/images/favicons/soundcloud-16.png",
 	"vgmdb.net": "http://vgmdb.net/favicon.ico",
-	"vkdb.jp": "http://www.vkdb.jp/favicon.ico",
-	"wikipedia.org": "http://en.wikipedia.org/favicon.ico",
+	"vkdb.jp": "//www.vkdb.jp/favicon.ico",
+	"wikipedia.org": "//en.wikipedia.org/favicon.ico",
 	"yahoo.": "http://blogs.yahoo.co.jp/favicon.ico",
 };
 var guessOtherFavicons = false;
