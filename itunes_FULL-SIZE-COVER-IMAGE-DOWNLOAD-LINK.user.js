399ffcd27c594d29cb7b4231d5a5941fd20b07da
jesus2099 <jesus2099@users.noreply.github.com>
2016-04-21 19:26:33 +0200

    FIX: some iTunes album URL include the album name NEW: zoom in cursor instead of link

--- itunes_FULL-SIZE-COVER-IMAGE-DOWNLOAD-LINK.user.js
+++ itunes_FULL-SIZE-COVER-IMAGE-DOWNLOAD-LINK.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         itunes. FULL SIZE COVER IMAGE DOWNLOAD LINK
-// @version      2016.4.21
+// @version      2016.4.21.1924
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/itunes_FULL-SIZE-COVER-IMAGE-DOWNLOAD-LINK.user.js
 // @description  iTunes.com: Adds a small link to download full size cover image of an album
 // @supportURL   https://github.com/jesus2099/konami-command/labels/itunes_FULL-SIZE-COVER-IMAGE-DOWNLOAD-LINK
@@ -16,16 +16,14 @@
 // @since        2016-04-21
 // @icon         data:image/gif;base64,R0lGODlhEAAQAMIDAAAAAIAAAP8AAP///////////////////yH5BAEKAAQALAAAAAAQABAAAAMuSLrc/jA+QBUFM2iqA2ZAMAiCNpafFZAs64Fr66aqjGbtC4WkHoU+SUVCLBohCQA7
 // @grant        none
-// @match        *://itunes.apple.com/*/album/id*
+// @match        *://itunes.apple.com/*/album/*
 // @run-at       document-end
 // ==/UserScript==
 "use strict";
 var img = document.querySelector("div.lockup.product a > div.artwork > img.artwork");
-var a = document.createElement("a");
-a.setAttribute("target", "_blank");
-a.setAttribute("href", img.getAttribute("src-swap").replace(
+img.parentNode.parentNode.setAttribute("href", img.getAttribute("src-swap").replace(
     /^(.*\/\/)\w+(\d+.mzstatic.com)\/\w+\/\w+\/(\w+\/\w+\/\w+\/\w+\/\w+\/[\w-]+)\/cover\d+x\d+.jpeg$/,
     "$1is$2/image/thumb/$3/source/99999999x99999999bb-100.jpg"
 ));
-a.appendChild(document.createTextNode("Full size cover image 🔍"));
-img.parentNode.parentNode.parentNode.insertBefore(a, img.parentNode.parentNode.nextSibling);
+img.parentNode.parentNode.setAttribute("target", "_blank");
+img.parentNode.parentNode.style.setProperty("cursor", "zoom-in");
