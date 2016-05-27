872749db9ad57661352903a421a74878529639af
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2016-05-27 10:40:53 +0200

    Ends work on #215: Huge hexagon icon. Starts work on #226: Better layout.

--- MB. artist credit entity links.user.js
+++ MB. artist credit entity links.user.js
@@ -1,22 +1,29 @@
 // ==UserScript==
-// @name           MB. artist credit entity links
-// @version        2016.5.17
-// @description    Adds links to filtered and searched release groups, releases and recordings  for each artist credit in artist aliases page's artist credits section. Additionally spots duplicate aliases.
-// @namespace      http://userscripts.org/scripts/show/131649
-// @author         Tristan DANIEL (PATATE12 aka. jesus2099/shamo)
-// @licence        CC BY-NC-SA 3.0 FR (http://creativecommons.org/licenses/by-nc-sa/3.0/fr/)
-// @grant          none
-// @match          *://*.musicbrainz.org/artist/*/aliases
-// @run-at         document-end
+// @name         MB. artist credit entity links
+// @version      2016.5.27
+// @changelog    https://github.com/jesus2099/konami-command/commits/master/MB.%20artist%20credit%20entity%20links.user.js
+// @description  Adds links to filtered and searched release groups, releases and recordings  for each artist credit in artist aliases page’s artist credits section. Additionally spots duplicate aliases.
+// @homepage     http://userscripts-mirror.org/scripts/show/131649
+// @supportURL   https://github.com/jesus2099/konami-command/labels/mb_ARTIST-CREDIT-ENTITY-LINKS
+// @compatible   opera(12.18.1872)+violentmonkey  my setup
+// @namespace    http://userscripts.org/scripts/show/131649
+// @downloadURL  https://github.com/jesus2099/konami-command/raw/master/MB.%20artist%20credit%20entity%20links.user.js
+// @updateURL    https://github.com/jesus2099/konami-command/raw/master/MB.%20artist%20credit%20entity%20links.user.js
+// @author       PATATE12
+// @licence      CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/)
+// @since        2012-04-23
+// @grant        none
+// @match        *://*.musicbrainz.org/artist/*/aliases
+// @run-at       document-end
 // ==/UserScript==
 "use strict";
 var entities = {
 	"filtered release groups": {url: "/artist/%aid%?filter.artist_credit_id=%acid%", img: "/static/images/entity/release_group.svg"},
-	"release group search": {url: "/search?query=arid%3A%aid%+AND+artist%3A%22%acname%%22&type=release_group&limit=100&method=advanced", img: "/static/images/icons/search.svg"},
-	"filtered releases": {url: "/artist/%aid%/releases?filter.artist_credit_id=%acid%", img: "/static/images/entity/release.svg"},
-	"release search": {url: "/search?query=arid%3A%aid%+AND+artist%3A%22%acname%%22&type=release&limit=100&method=advanced", img: "/static/images/icons/search.svg"},
-	"filtered recordings": {url: "/artist/%aid%/recordings?filter.artist_credit_id=%acid%", img: "/static/images/entity/recording.svg"},
-	"recording search (tracks)": {url: "/search?query=arid%3A%aid%+AND+artist%3A%22%acname%%22&type=recording&limit=100&method=advanced", img: "/static/images/icons/search.svg"},
+	"release group search": {url: "/search?query=arid%3A%aid%+AND+artist%3A%22%acname%%22&type=release_group&limit=100&method=advanced", img: "/static/images/icons/search.png"},
+	"filtered releases": {url: "/artist/%aid%/releases?filter.artist_credit_id=%acid%", img: "/static/images/entity/release.png"},
+	"release search": {url: "/search?query=arid%3A%aid%+AND+artist%3A%22%acname%%22&type=release&limit=100&method=advanced", img: "/static/images/icons/search.png"},
+	"filtered recordings": {url: "/artist/%aid%/recordings?filter.artist_credit_id=%acid%", img: "/static/images/entity/recording.png"},
+	"recording search (tracks)": {url: "/search?query=arid%3A%aid%+AND+artist%3A%22%acname%%22&type=recording&limit=100&method=advanced", img: "/static/images/icons/search.png"},
 };
 var name = document.querySelector("body > div#page > div#content > div.artistheader > h1 a").textContent;
 var tables = document.querySelectorAll("body > div#page > div#content > table.tbl");
@@ -31,8 +38,16 @@ for (var tab = 0; tab < tables.length; tab++) {
 		if (type == "ac") {
 			var entd = trs[i].insertBefore(createTag("td"), trs[i].querySelector("td:nth-last-of-type(1)"));
 			for (var ent in entities) if (entities.hasOwnProperty(ent)) {
-				entd.appendChild(createTag("a", {title: ent, href: entities[ent].url.replace(/%acname%/, encodeURIComponent(aname)).replace(/%aid%/, location.href.match(/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/)).replace(/%acid%/, trs[i].querySelector("td:nth-last-of-type(1) > a").getAttribute("href").match(/credit\/([0-9]+)\/edit$/)[1])}, {}, {}, ent/*createTag("img", {alt: ent, src: entities[ent].img}, {maxHeight: "16px"})*/));
-				entd.appendChild(document.createTextNode(" | "));
+				if (ent.match(/filtered/)) {
+					entd.appendChild(createTag("img", {alt: "icon", src: entities[ent].img}, {maxHeight: "16px"}));
+					entd.appendChild(document.createTextNode(" "));
+				} else {
+					entd.appendChild(document.createTextNode(" | "));
+				}
+				entd.appendChild(createTag("a", {title: ent, href: entities[ent].url.replace(/%acname%/, encodeURIComponent(aname)).replace(/%aid%/, location.href.match(/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/)).replace(/%acid%/, trs[i].querySelector("td:nth-last-of-type(1) > a").getAttribute("href").match(/credit\/([0-9]+)\/edit$/)[1])}, {}, {}, ent));
+				if (ent.match(/search/)) {
+					entd.appendChild(document.createElement("br"));
+				}
 			}
 		}
 		if (aname == name) {
