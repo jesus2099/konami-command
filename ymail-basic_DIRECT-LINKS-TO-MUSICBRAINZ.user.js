3d1be85304acf4f5d519d5fbd301b1f9a5186a1a
jesus2099 <jesus2099@users.noreply.github.com>
2016-06-06 11:51:35 +0200

    FIX #215*: Huge hexagon icon (*should fix but I can’t test at the moment)

--- ymail-basic_DIRECT-LINKS-TO-MUSICBRAINZ.user.js
+++ ymail-basic_DIRECT-LINKS-TO-MUSICBRAINZ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ymail-basic. DIRECT LINKS TO MUSICBRAINZ
-// @version      2016.4.28
+// @version      2016.6.6
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/ymail-basic_DIRECT-LINKS-TO-MUSICBRAINZ.user.js
 // @description  BASIC Yahoo! Mail only (/neo/b/). Adds links to MusicBrainz edits directly in mail.yahoo.com folders view (including "no votes" and "subscription" emails). No need to open all those e-mails any more. Only one link per edit ID, duplicate ID are coloured and e-mail(s) marked for deletion. Once clicked, the link is faded, to keep trace of already browsed edits. Limitations : only Opera(maybe) and y!mail BASIC I guess.
 // @homepage     http://userscripts-mirror.org/scripts/show/80308
@@ -139,7 +139,8 @@ if (emails) {
 						var im = a.appendChild(document.createElement("img"));
 						var openedits = "/open_edits";
 						if ((type = allparts[4].match(/artist|collection|label|series/))) {
-							im.setAttribute("src", preferredProtocol + "//musicbrainz.org/static/images/entity/%type%.png".replace(/%type%/, type).replace(/collection|series/, "release_group"));
+							im.setAttribute("src", preferredProtocol + "//musicbrainz.org/static/images/entity/%type%.svg".replace(/%type%/, type).replace(/collection|series/, "release_group"));
+							im.setAttribute("height", "12px");
 						} else if (allparts[4].match(/user/)) {
 							im.setAttribute("src", preferredProtocol + "//gravatar.com/avatar/placeholder?d=mm&s=12");
 							openedits = "/edits/open";
