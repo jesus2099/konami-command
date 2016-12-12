231799d423154fd1cec9f4e151df0fc67ceacb4a
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2016-12-12 12:43:08 +0100

    ☠ OBSOLETE ☠

--- laposte.net_BOUGEZ-AVEC-LA-POSTE.user.js
+++ laposte.net_BOUGEZ-AVEC-LA-POSTE.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         laposte.net. BOUGEZ AVEC LA POSTE
-// @version      2016.6.15
+// @version      2016.12.12-obsolete
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/laposte.net_BOUGEZ-AVEC-LA-POSTE.user.js
-// @description  laposte.net webmail power ups: Déconnexion d’un seul clic, moins de pubs
+// @description  ☠ OBSOLETE ☠ laposte.net webmail power ups: Déconnexion d’un seul clic, moins de pubs
 // @homepage     http://userscripts-mirror.org/scripts/show/422068
 // @supportURL   https://github.com/jesus2099/konami-command/labels/laposte.net_BOUGEZ-AVEC-LA-POSTE
 // @compatible   opera(12.18.1872)+violentmonkey  my setup
@@ -12,7 +12,6 @@
 // @author       PATATE12
 // @licence      CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/)
 // @since        2014-03-21
-// @icon         data:image/gif;base64,R0lGODlhEAAQAKEDAP+/3/9/vwAAAP///yH/C05FVFNDQVBFMi4wAwEAAAAh/glqZXN1czIwOTkAIfkEAQACAwAsAAAAABAAEAAAAkCcL5nHlgFiWE3AiMFkNnvBed42CCJgmlsnplhyonIEZ8ElQY8U66X+oZF2ogkIYcFpKI6b4uls3pyKqfGJzRYAACH5BAEIAAMALAgABQAFAAMAAAIFhI8ioAUAIfkEAQgAAwAsCAAGAAUAAgAAAgSEDHgFADs=
 // @grant        none
 // @match        *://webmail.laposte.net/*
 // @match        *://www.laposte.net/*
@@ -21,23 +20,7 @@
 // @run-at       document-end
 // ==/UserScript==
 "use strict";
-document.head.appendChild(document.createElement("style")).setAttribute("type", "text/css");
-var j2css = document.styleSheets[document.styleSheets.length - 1];
-j2css.insertRule([
-	"div.home div#aside div[class*='advertis']",
-	"div.home div#aside div[class*='Shopping']",
-	"iframe#adtopiframe",
-	"iframe.adtopiframe",
-	"td#bottomPub",
-	"td.bottomPub",
-].join(", ") + " { display: none; }", j2css.cssRules.length);
-var maxinterval = 20;
-var interval = setInterval(function(){
-	var quitter = document.querySelector("a#quitLink[onclick]");
-	if (quitter) {
-		quitter.removeAttribute("onclick");
-		quitter.setAttribute("href", "https://compte.laposte.net/logout.do");
-		quitter.style.setProperty("color", "#fc6");
-	}
-	if (quitter || maxinterval-- < 0) clearInterval(interval);
-}, 400);
+if (!document.cookie.match(new RegExp("laposte.net_BOUGEZ-AVEC-LA-POSTE_obsolete=1"))) {
+	alert("☠ OBSOLETE ☠ “laposte.net. BOUGEZ AVEC LA POSTE” ☠ OBSOLETE ☠\r\n\r\nPLEASE UNINSTALL IT to unclutter your userscript list. Thank you for having used this script.\r\n\r\nMerci de bien vouloir DÉSINSTALLER CE SCRIPT afin d’alléger votre navigation. Merci d’avoir utilisé ce script.");
+}
+document.cookie = "laposte.net_BOUGEZ-AVEC-LA-POSTE_obsolete=1; path=/";
