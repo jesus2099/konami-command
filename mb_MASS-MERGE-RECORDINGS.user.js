382e7b925296c46af0e93663f1fc1ee3dee908a0
jesus2099 <jesus2099@users.noreply.github.com>
2017-02-16 12:58:38 +0100

    ENH #294: Turn those « ☞ ↑ / → / ↓ / ←: shift up/down » into actual buttons

--- mb_MASS-MERGE-RECORDINGS.user.js
+++ mb_MASS-MERGE-RECORDINGS.user.js
@@ -477,19 +477,30 @@ function massMergeGUI() {
 	});
 	MMRdiv.appendChild(createTag("p", {}, [
 		"☞ ",
-		createTag("kbd", {}, "↑"),
+		createTag("kbd", {a: {class: MMRid + "arrowButton"}, s: {cursor: "pointer"}}, "↑"),
 		" / ",
-		createTag("kbd", {}, "→"),
+		createTag("kbd", {a: {class: MMRid + "arrowButton"}, s: {cursor: "pointer"}}, "→"),
 		" / ",
-		createTag("kbd", {}, "↓"),
+		createTag("kbd", {a: {class: MMRid + "arrowButton"}, s: {cursor: "pointer"}}, "↓"),
 		" / ",
-		createTag("kbd", {}, "←"),
+		createTag("kbd", {a: {class: MMRid + "arrowButton"}, s: {cursor: "pointer"}}, "←"),
 		": shift up/down",
 		document.createElement("br"),
 		"☞ ",
 		createTag("kbd", {}, "ENTER"),
 		": queue all"
 	]));
+	MMRdiv.addEventListener("click", function(event) {
+		if (event.target.classList.contains(MMRid + "arrowButton")) {
+			startpos.focus();
+			if (event.target.textContent.match(/[↑←]/) && startpos.selectedIndex > 0) {
+				startpos.selectedIndex -= 1;
+			} else if (event.target.textContent.match(/[↓→]/) && startpos.selectedIndex < startpos.length - 1) {
+				startpos.selectedIndex += 1;
+			}
+			sendEvent(startpos, "change");
+		}
+	});
 	matchMode.sequential = createInput("button", "", "Sequential");
 	matchMode.sequential.setAttribute("title", "Restore remote tracks order");
 	matchMode.sequential.addEventListener("click", changeMatchMode);
