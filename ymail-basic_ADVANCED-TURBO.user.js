5b37b6c99114815d9db86bc74a126f6417f566d1
jesus2099 <jesus2099@users.noreply.github.com>
2017-04-19 14:28:36 +0200

    coding style

--- ymail-basic_ADVANCED-TURBO.user.js
+++ ymail-basic_ADVANCED-TURBO.user.js
@@ -22,22 +22,22 @@
 var KEYBOARD_SHORTCUTS = true; /*CTRL+A: select all, DEL: delete e-mail, LEFT ARROW: previous message, RIGHT ARROW: next message*/
 var REMOVE_CRAP = true; /*FULL SCREEN DISPLAY. removes various distracting craps (ads, etc.)*/
 /*---CONFIG---*/
-var userjs = {"key":177655,"name":"ymail-basic. ADVANCED TURBO"};
+var userjs = {key: 177655, name: "ymail-basic. ADVANCED TURBO"};
 var DEBUG = localStorage.getItem("jesus2099debug");
 var shortcuts = {
-	"27": {"key":"ESC","button":["#top_backSearch", "td.main div.contentbuttonbar span.btn > input#cancelbuttontop[name='action_cancel_compose']", "td.navigation ul.folders li.selected > a"]},
-	"37": {"key":"←","button":"td.main > div.container a#top_prev.action:not(.no-prev), td.main div.contentnav > div.pagination > span#top_pagination > a + a"},
-	"39": {"key":"→","button":"td.main > div.container a#top_next.action:not(.no-next), td.main div.contentnav > div.pagination > span#top_pagination > span.pageno > a:not([href^='#'])"},
-	"46": {"key":"DEL","button":"td.main div.contentbuttonbar input[id='top_delete'][type='submit']"},
-	"CTRL+65": {"noreload":true,"key":"A","button":"td.main div.contentnav a#select_all.action"},
-	"65": {"key":"A","button":"td.main div.contentbuttonbar span.btn > input[type='submit'][name='action_msg_replyall']"},
-	"70": {"key":"F","button":"td.main div.contentbuttonbar span.btn > input[type='submit'][name='action_msg_fwd']"},
-	"73": {"key":"I","button":"td.main > div.container div.spamwarning > a[href*='blockimages=none']"},
-	"77": {"key":"M","button":"td.main div.contentbuttonbar select[name='top_action_select'] > option[value='msg.flag']"},
-	"78": {"key":"N","button":"div#globalbuttonbartop.globalbuttonbar span.btn > input.composeicon[type='submit']"},
-	"82": {"key":"R","button":"td.main div.contentbuttonbar span.btn > input[type='submit'][name='action_msg_reply']"},
-	"83": {"key":"S","button":"td.main div.contentbuttonbar span.btn > input#top_spam[type='submit'][name='action_msg_topspam'], td.main div.contentbuttonbar span.btn > input#top_ham[type='submit'][name='msg_topham'], td.main div.contentbuttonbar select[name='top_action_select'] > option[value='msg.ham'], td.main div.contentbuttonbar select[name='top_action_select'] > option[value='msg.spam']"},
-	"85": {"key":"U","button":"td.main div.contentbuttonbar select[name='top_action_select'] > option[value='msg.unread']"},
+	"27": {key: "ESC", button: ["#top_backSearch", "td.main div.contentbuttonbar span.btn > input#cancelbuttontop[name='action_cancel_compose']", "td.navigation ul.folders li.selected > a"]},
+	"37": {key: "←", button: "td.main > div.container a#top_prev.action:not(.no-prev), td.main div.contentnav > div.pagination > span#top_pagination > a + a"},
+	"39": {key: "→", button: "td.main > div.container a#top_next.action:not(.no-next), td.main div.contentnav > div.pagination > span#top_pagination > span.pageno > a:not([href^='#'])"},
+	"46": {key: "DEL", button: "td.main div.contentbuttonbar input[id='top_delete'][type='submit']"},
+	"CTRL+65": {noreload:true, key: "A", button: "td.main div.contentnav a#select_all.action"},
+	"65": {key: "A", button: "td.main div.contentbuttonbar span.btn > input[type='submit'][name='action_msg_replyall']"},
+	"70": {key: "F", button: "td.main div.contentbuttonbar span.btn > input[type='submit'][name='action_msg_fwd']"},
+	"73": {key: "I", button: "td.main > div.container div.spamwarning > a[href*='blockimages=none']"},
+	"77": {key: "M", button: "td.main div.contentbuttonbar select[name='top_action_select'] > option[value='msg.flag']"},
+	"78": {key: "N", button: "div#globalbuttonbartop.globalbuttonbar span.btn > input.composeicon[type='submit']"},
+	"82": {key: "R", button: "td.main div.contentbuttonbar span.btn > input[type='submit'][name='action_msg_reply']"},
+	"83": {key: "S", button: "td.main div.contentbuttonbar span.btn > input#top_spam[type='submit'][name='action_msg_topspam'], td.main div.contentbuttonbar span.btn > input#top_ham[type='submit'][name='msg_topham'], td.main div.contentbuttonbar select[name='top_action_select'] > option[value='msg.ham'], td.main div.contentbuttonbar select[name='top_action_select'] > option[value='msg.spam']"},
+	"85": {key: "U", button: "td.main div.contentbuttonbar select[name='top_action_select'] > option[value='msg.unread']"},
 };
 var emails = document.querySelectorAll("table#datatable > tbody > tr > td > h2 > a.mlink");
 if (emails) {
@@ -47,8 +47,8 @@ if (emails) {
 	var selectall = document.querySelector("td.main div.contentnav a#select_all.action");
 	if (selectall && emcbs) {
 		hackit(selectall, "(TURBO)");
-		selectall.addEventListener("click", function(e){
-			stop(e);
+		selectall.addEventListener("click", function(event){
+			stop(event);
 			if (emcbs.length > 0) {
 				sendEvent(emcbs[0], "ctrl+click");
 			}
@@ -56,7 +56,7 @@ if (emails) {
 	}
 	for (var emcb=0; emcb < emcbs.length; emcb++) {
 		hackit(emcbs[emcb], "SHIFT+CLICK for range-(un)select\nCTRL+CLICK for (un)select all");
-		emcbs[emcb].addEventListener("click", function(e) {
+		emcbs[emcb].addEventListener("click", function(event) {
 			if (e.ctrlKey || e.shiftKey && lastemcb > -1) {
 				var i = 0;
 				var max = emcbs.length;
@@ -77,9 +77,9 @@ if (emails) {
 	}
 	/*auto apply actions (star/read/unread flags, move, etc.)*/
 	var autofire = [
-		{ "triggers": "div.contentbuttonbar select[name='top_action_select']", "submit": "div.contentbuttonbar input.action[type='submit'][name='self_action_msg_topaction']" },
-		{ "triggers": "div.sortpane select[name*='sor']", "submit": "div.sortpane input.action[type='submit'][name='self_action_msg_filter']" },
-		{ "triggers": "table.uh td.uh-rt select.uh-mnu[name='jumpTo']", "submit": "table.uh td.uh-rt button.uh-mnu-btn[type='submit']" }
+		{triggers: "div.contentbuttonbar select[name='top_action_select']", "submit": "div.contentbuttonbar input.action[type='submit'][name='self_action_msg_topaction']" },
+		{triggers: "div.sortpane select[name*='sor']", "submit": "div.sortpane input.action[type='submit'][name='self_action_msg_filter']" },
+		{triggers: "table.uh td.uh-rt select.uh-mnu[name='jumpTo']", "submit": "table.uh td.uh-rt button.uh-mnu-btn[type='submit']" }
 	];
 	for (var af = 0; af < autofire.length; af++) {
 		var triggers = document.querySelectorAll(autofire[af].triggers);
@@ -87,8 +87,8 @@ if (emails) {
 			for (var tr = 0; tr < triggers.length; tr++) {
 				hackit(triggers[tr], "(Action will trigger immediately uppon select)");
 				triggers[tr].setAttribute("afsbmt", autofire[af].submit);
-				triggers[tr].addEventListener("change", function(e) {
-					try { doThis(document.querySelector(this.getAttribute("afsbmt"))); } catch(e) {}
+				triggers[tr].addEventListener("change", function(event) {
+					try { doThis(document.querySelector(this.getAttribute("afsbmt"))); } catch(error) {}
 				}, false);
 			}
 			
@@ -96,24 +96,24 @@ if (emails) {
 	}
 	/*keyboard shortcuts*/
 	if (KEYBOARD_SHORTCUTS) {
-		document.addEventListener("keydown", function(e){
-			var key = (e.ctrlKey?"CTRL+":"")+e.keyCode;
+		document.addEventListener("keydown", function(event){
+			var key = (e.ctrlKey ? "CTRL+" : "")+e.keyCode;
 			try {
-				if (DEBUG) console.log(userjs.name+" key "+key+(shortcuts[key]?" on "+e.target+".\n("+shortcuts[key].key+") → "+shortcuts[key].button:""));
+				if (DEBUG) console.log(userjs.name + " key " + key + (shortcuts[key] ? " on " + e.target + ".\n(" + shortcuts[key].key + ") → " + shortcuts[key].button : ""));
 				if (!e.target || !e.target.tagName || !e.target.tagName.match(/input|select|textarea/i) || e.target.tagName.match(/input/i) && e.target.getAttribute("type") && !e.target.getAttribute("type").match(/password|text/i)) {
 					doThis(shortcuts[key].button, shortcuts[key].noreload);
-					return stop(e);
+					return stop(event);
 				}
-			} catch(e) {}
+			} catch(error) {}
 		}, false);
 		for (var sc in shortcuts) { if (shortcuts.hasOwnProperty(sc)) {
-			try { hackit(shortcuts[sc].button, "", "["+sc.replace(/\d+$/, shortcuts[sc].key)+"]"); } catch(e) {}
+			try { hackit(shortcuts[sc].button, "", "[" + sc.replace(/\d+$/, shortcuts[sc].key) + "]"); } catch(error) {}
 		} }
 	}
 	/*remove crap*/
 	if (REMOVE_CRAP) {
 		document.head.appendChild(document.createElement("style")).setAttribute("type", "text/css");
-		var j2delcrapss = document.styleSheets[document.styleSheets.length-1];
+		var j2delcrapss = document.styleSheets[document.styleSheets.length - 1];
 		j2delcrapss.insertRule([
 			"a[href$='relevantads.html']",
 			"a[href*='//beap.adss.yahoo.com/']",
@@ -127,7 +127,7 @@ if (emails) {
 		].join(",")+"{display:none}", j2delcrapss.cssRules.length);
 		try {
 			getParent(document.querySelector("table.tbl tr > td.spnsr"), "tr").style.setProperty("display", "none");
-		} catch(e) {}
+		} catch(error) {}
 	}
 }
 function hackit(_obj, title, text) {
@@ -135,21 +135,28 @@ function hackit(_obj, title, text) {
 	obj.style.setProperty("background-color", "#cfc");
 	obj.style.setProperty("color", "#030");
 	obj.style.setProperty("cursor", "help");
-	obj.setAttribute("title", (obj.getAttribute("title")?obj.getAttribute("title")+" ":"")+title);
+	obj.setAttribute("title", (obj.getAttribute("title") ? obj.getAttribute("title") + " " : "") + title);
 	if (text) {
 		if (obj.tagName && obj.tagName == "INPUT") {
-			obj.setAttribute("value", obj.getAttribute("value")+" "+text);
-		}
-		else if (obj.tagName && obj.textContent.match(/\w+/)) {
-			var label = obj.firstChild.tagName?obj.firstChild:obj;
-			label.appendChild(document.createTextNode(" "+text));
+			obj.setAttribute("value", obj.getAttribute("value") + " " + text);
+		} else if (obj.tagName && obj.textContent.match(/\w+/)) {
+			var label = obj.firstChild.tagName ? obj.firstChild:obj;
+			label.appendChild(document.createTextNode(" " + text));
 		}
 	}
 }
 function findNode(argh) {
-	if (typeof argh == "string") return document.querySelector(argh);
-	else if (argh.tagName) return argh;
-	else if (argh.length) for (var vrouf,paf=0; paf < argh.length; paf++) if (vrouf = document.querySelector(argh[paf])) return vrouf;
+	if (typeof argh == "string") {
+		return document.querySelector(argh);
+	} else if (argh.tagName) {
+		return argh;
+	} else if (argh.length) {
+		for (var vrouf, paf = 0; paf < argh.length; paf++) {
+			if (vrouf = document.querySelector(argh[paf])) {
+				return vrouf;
+			}
+		}
+	}
 }
 function getParent(obj, tag, cls) {
 	var cur = obj;
@@ -167,10 +174,14 @@ function getParent(obj, tag, cls) {
 function doThis(butt, noreload) {
 	var button = findNode(butt);
 	var opt = (button.tagName == "OPTION");
-	var inp = opt?button.parentNode:button;
-	if (opt) inp.selectedIndex = button.index;
-	if (!noreload) inp.style.setProperty("background-color", "gold");
-	sendEvent(inp, opt?"change":"click");
+	var inp = opt ? button.parentNode : button;
+	if (opt) {
+		inp.selectedIndex = button.index;
+	}
+	if (!noreload) {
+		inp.style.setProperty("background-color", "gold");
+	}
+	sendEvent(inp, opt ? "change" : "click");
 }
 function indexOf(element, array) {
 	var i = -1;
@@ -193,17 +204,18 @@ function sendEvent(n, _e){
 			e = params.mods.pop();
 		}
 		ev = document.createEvent("MouseEvents");
-		ev.initMouseEvent(e, true, true, self, 0, 0, 0, 0, 0, params.mods.indexOf("ctrl")>-1, params.mods.indexOf("alt")>-1, params.mods.indexOf("shift")>-1, params.mods.indexOf("meta")>-1, 0, null);
-	}
-	else {
+		ev.initMouseEvent(e, true, true, self, 0, 0, 0, 0, 0, params.mods.indexOf("ctrl") > -1, params.mods.indexOf("alt") > -1, params.mods.indexOf("shift") > -1, params.mods.indexOf("meta") > -1, 0, null);
+	} else {
 		ev = document.createEvent("HTMLEvents");
 		ev.initEvent(e, true, true);
 	}
 	n.dispatchEvent(ev);
 }
-function stop(e) {
-	e.cancelBubble = true;
-	if (e.stopPropagation) e.stopPropagation();
-	e.preventDefault();
+function stop(event) {
+	event.cancelBubble = true;
+	if (event.stopPropagation) {
+		event.stopPropagation();
+	}
+	event.preventDefault();
 	return false;
 }
