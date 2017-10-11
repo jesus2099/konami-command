5ea40d458c2cb69c4049bd6b3b1616f380d9feff
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2017-10-11 18:32:55 +0200

    Don’t compress collection list (small and useful for debugging)

--- mb_COLLECTION-HIGHLIGHTER.user.js
+++ mb_COLLECTION-HIGHLIGHTER.user.js
@@ -2,7 +2,7 @@
 var meta = {raw: function() {
 // ==UserScript==
 // @name         mb. COLLECTION HIGHLIGHTER
-// @version      2017.6.16
+// @version      2017.10.11
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_COLLECTION-HIGHLIGHTER.user.js
 // @description  musicbrainz.org: Highlights releases, release-groups, etc. that you have in your collections (anyone’s collection can be loaded) everywhere
 // @homepage     http://userscripts-mirror.org/scripts/show/126380
@@ -130,7 +130,7 @@ if (cat) {
 	j2ss.insertRule("." + userjs + "HLitem { text-shadow: 0 0 8px " + highlightColour + "!important; }", 0);
 	j2ss.insertRule("." + userjs + "HLrow ." + userjs + "HLitem { border: 0; padding: 0; }", 0);
 	var MBS = self.location.protocol + "//" + self.location.host;
-	var collectionsID = sfmdecompress(localStorage.getItem(userjs + "collections")) || "";
+	var collectionsID = localStorage.getItem(userjs + "collections") || "";
 	var releaseID;
 	var stuff, collectedStuff = ["collection", "release", "release-group", "recording", "artist", "work", "label"];
 	var strType = "release-group|recording|label|artist|work";
@@ -282,7 +282,7 @@ if (cat) {
 				if (mbid) {
 					mbid = mbid[1];
 					if (!stuff[cstuff].loaded) {
-						stuff[cstuff].rawids = sfmdecompress(localStorage.getItem(userjs + cstuff + "s"));
+						stuff[cstuff].rawids = decompress(localStorage.getItem(userjs + cstuff + "s"));
 						if (stuff[cstuff].rawids) {
 							stuff[cstuff].ids = stuff[cstuff].rawids.split(" ");
 							debug(" \r\n" + stuff[cstuff].ids.length + " " + cstuff.toUpperCase() + (stuff[cstuff].ids.length == 1 ? "" : "S") + " loaded from local storage (" + userjs + cstuff + "s)\r\nMatching: " + path, true);
@@ -362,17 +362,17 @@ function loadCollection(collectionMBID, WSMode, pageOrOffset) {
 	var url = WSMode ? "/ws/2/collection/" + collectionMBID + "/releases?limit=" + limit + "&offset=" + offset : "/collection/" + collectionMBID + "?page=" + page;
 	if (page == 1) {
 		// Add collection MBID to list of highlighted
-		collectionsID = sfmdecompress(localStorage.getItem(userjs + "collections")) || "";
+		collectionsID = localStorage.getItem(userjs + "collections") || "";
 		if (collectionsID.indexOf(collectionMBID) < 0) {
 			collectionsID += collectionMBID + " ";
 		}
-		localStorage.setItem(userjs + "collections", sfmcompress(collectionsID));
+		localStorage.setItem(userjs + "collections", collectionsID);
 		modal(true, "Loading collection " + collectionMBID + "…", 1);
 		modal(true, concat(["WTF? If you want to stop this monster crap, just ", createA("reload", function(event) { self.location.reload(); }), " or close this page."]), 2);
 		modal(true, concat(["<hr>", "Fetching releases…"]), 2);
 		stuff["release-tmp"] = {ids: []};
 		for (var stu in stuff) if (collectedStuff.indexOf(stu) >= 0) {
-			stuff[stu].rawids = sfmdecompress(localStorage.getItem(userjs + stu + "s")) || "";
+			stuff[stu].rawids = decompress(localStorage.getItem(userjs + stu + "s")) || "";
 			stuff[stu].ids = stuff[stu].rawids.length > 0 ? stuff[stu].rawids.split(" ") : [];
 		}
 	}
@@ -411,7 +411,7 @@ function loadCollection(collectionMBID, WSMode, pageOrOffset) {
 				} else if (lastPage && lastPage == page || !nextPage) {
 					modal(true, " ", 1);
 					if (stuff["release-tmp"].ids.length > 0) {
-						localStorage.setItem(userjs + "releases", sfmcompress(stuff["release"].rawids));
+						localStorage.setItem(userjs + "releases", compress(stuff["release"].rawids));
 						modal(true, concat([createTag("b", {}, stuff["release"].ids.length + " release" + (stuff["release"].ids.length == 1 ? "" : "s")), " saved into local storage (" + userjs + "releases)… "]));
 						modal(true, "OK.", 2);
 						retry = 0;
@@ -506,7 +506,7 @@ function fetchReleasesStuff(pi) {
 						modal(true, " ", 1);
 						delete(stuff["release-tmp"]);
 						for (var stu in stuff) if (stu != "release" && stuff.hasOwnProperty(stu)) {
-							localStorage.setItem(userjs + stu + "s", sfmcompress(stuff[stu].rawids));
+							localStorage.setItem(userjs + stu + "s", compress(stuff[stu].rawids));
 							stuff[stu].rawids = "";
 							modal(true, concat([createTag("b", {}, stuff[stu].ids.length + " " + stu + (stuff[stu].ids.length == 1 ? "" : "s")), " saved into local storage (" + userjs + stu + "s)… "]));
 							modal(true, "OK.", 1);
@@ -540,9 +540,9 @@ function collectionUpdater(link, action) {
 		link.addEventListener("click", function(event) {
 			altered = this.getAttribute("href") != self.location.href;
 			modal(true, "Refreshing memory…", 1);
-			collectionsID = sfmdecompress(localStorage.getItem(userjs + "collections")) || "";
+			collectionsID = localStorage.getItem(userjs + "collections") || "";
 			for (var stu in stuff) if (collectedStuff.indexOf(stu) >= 0) {
-				stuff[stu].rawids = sfmdecompress(localStorage.getItem(userjs + stu + "s"));
+				stuff[stu].rawids = decompress(localStorage.getItem(userjs + stu + "s"));
 				stuff[stu].ids = stuff[stu].rawids != null ? (stuff[stu].rawids.length > 0 ? stuff[stu].rawids.split(" ") : []) : null;
 			}
 			if (stuff["release"].ids && releaseID) {
@@ -553,7 +553,7 @@ function collectionUpdater(link, action) {
 						if (stuff["release"].rawids.indexOf(releaseID) == -1) {
 							modal(true, concat([createTag("b", {}, "Adding this release"), " to loaded collection…"]), 1);
 							stuff["release"].rawids += releaseID + " ";
-							localStorage.setItem(userjs + "releases", sfmcompress(stuff["release"].rawids));
+							localStorage.setItem(userjs + "releases", compress(stuff["release"].rawids));
 							altered = true;
 						}
 						for (var c = 0; c < checks.length; c++) {
@@ -563,7 +563,7 @@ function collectionUpdater(link, action) {
 								if (stuff[type].ids && stuff[type].rawids.indexOf(mbid) < 0 && (type != "artist" || skipArtists.indexOf(mbid) < 0)) {
 									modal(true, concat([createTag("b", {}, ["Adding " + type, " ", createA(type != "release-group" ? checks[c].textContent : mbid, checks[c].getAttribute("href"), type)]), "…"]), 1);
 									stuff[type].rawids += mbid + " ";
-									localStorage.setItem(userjs + type + "s", sfmcompress(stuff[type].rawids));
+									localStorage.setItem(userjs + type + "s", compress(stuff[type].rawids));
 									altered = true;
 								}
 							}
@@ -574,7 +574,7 @@ function collectionUpdater(link, action) {
 						if (stuff["release"].rawids.indexOf(releaseID) > -1) {
 							modal(true, concat([createTag("b", {}, "Removing this release"), " from loaded collection…"]), 1);
 							stuff["release"].rawids = stuff["release"].rawids.replace(new RegExp(releaseID + "( |$)"), "");
-							localStorage.setItem(userjs + "releases", sfmcompress(stuff["release"].rawids));
+							localStorage.setItem(userjs + "releases", compress(stuff["release"].rawids));
 							altered = true;
 						}
 						if (checks.length > 0) {
@@ -695,7 +695,7 @@ function stuffRemover(checks, pp) {
 							} else {
 								modal(true, concat([createTag("span", {s: {color: "grey"}}, "not used any more: "), createTag("b", {}, "removing"), "…"]), 1);
 								stuff[checkType].rawids = stuff[checkType].rawids.replace(new RegExp(checkID + "( |$)"), "");
-								localStorage.setItem(userjs + checkType + "s", sfmcompress(stuff[checkType].rawids));
+								localStorage.setItem(userjs + checkType + "s", compress(stuff[checkType].rawids));
 								altered = true;
 								retry = 0;
 								setTimeout(function() { stuffRemover(checks.slice(1)); }, chrono(MBWSRate));
@@ -719,7 +719,7 @@ function stuffRemover(checks, pp) {
 				if (!stuff[checkAgainst].rawids) {/* Protection for some edge cases of new script using old script data */
 					modal(true, concat([createTag("span", {s: {color: "grey"}}, ["no ", checkAgainst, "s at all (", createA("//github.com/jesus2099/konami-command/issues/87", "#87"), "): "]), "removing…"]), 1);
 					stuff[checkType].rawids = stuff[checkType].rawids.replace(new RegExp(checkID + "( |$)"), "");
-					localStorage.setItem(userjs + checkType + "s", sfmcompress(stuff[checkType].rawids));
+					localStorage.setItem(userjs + checkType + "s", compress(stuff[checkType].rawids));
 					altered = true;
 				}
 				retry = 0;
@@ -961,7 +961,8 @@ function nsr(prefix) {
 	};
 	return ns[prefix] || null;
 }
-function sfmcompress(s) {
+/* compress and decompress from https://labs.ft.com/2012/06/text-re-encoding-for-optimising-storage-capacity-in-the-browser/ (localStorage is limited per browser) */
+function compress(s) {
 	if (s === null) return null;
 	if (s === "") return "";
 	var i, l, out = "";
@@ -972,7 +973,7 @@ function sfmcompress(s) {
 	// Add a snowman prefix to mark the resulting string as encoded
 	return String.fromCharCode(9731) + out;
 }
-function sfmdecompress(s) {
+function decompress(s) {
 	if (s === "") return "";
 	if (s === null) return null;
 	var i, l, n, m, out = "";
