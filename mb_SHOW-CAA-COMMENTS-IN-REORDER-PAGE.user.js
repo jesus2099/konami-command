41332c229785a2c130cabde2c55de0022c87b4c5
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2017-11-21 12:45:40 +0100

    split image type(s) and comment

--- mb_SHOW-CAA-COMMENTS-IN-REORDER-PAGE.user.js
+++ mb_SHOW-CAA-COMMENTS-IN-REORDER-PAGE.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         mb. SHOW CAA COMMENTS IN REORDER PAGE
-// @version      2017.8.24
+// @version      2017.11.21
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_SHOW-CAA-COMMENTS-IN-REORDER-PAGE.user.js
 // @description  Show cover art comments to ease reordering, especially when thumbnails have not been generated yet.
 // @homepage     http://forums.musicbrainz.org/viewtopic.php?pid=31473#p31473
 // @supportURL   https://github.com/jesus2099/konami-command/labels/mb_SHOW-CAA-COMMENTS-IN-REORDER-PAGE
-// @compatible   opera(12.18.1872)+violentmonkey  my setup
+// @compatible   opera(12.18.1872)+violentmonkey     my setup
+// @compatible   vivaldi(1.12.955.48)+violentmonkey  my setup
 // @namespace    jesus2099/shamo
 // @downloadURL  https://github.com/jesus2099/konami-command/raw/master/mb_SHOW-CAA-COMMENTS-IN-REORDER-PAGE.user.js
 // @updateURL    https://github.com/jesus2099/konami-command/raw/master/mb_SHOW-CAA-COMMENTS-IN-REORDER-PAGE.user.js
@@ -22,6 +23,15 @@
 "use strict";
 var imageLoaders = document.querySelectorAll("div.thumb-position > a > span.cover-art-image[data-title]")
 for (var i = 0; i < imageLoaders.length; i++) {
-	imageLoaders[i].parentNode.parentNode.insertBefore(document.createElement("div").appendChild(document.createTextNode(imageLoaders[i].getAttribute("data-title"))).parentNode, imageLoaders[i].parentNode.parentNode.firstChild.nextSibling.nextSibling).style.setProperty("background-color", "#ff6");
+	var subtitle = document.createElement("div");
+	var splitText = imageLoaders[i].getAttribute("data-title").match(/^([^(]+)?(?: \(([\w\W]+)\))?$/);
+	if (splitText) {
+		for (var j = 1; j < splitText.length; j++) {
+			if (splitText[j]) {
+				subtitle.appendChild(document.createElement("div").appendChild(document.createTextNode(splitText[j])).parentNode).style.setProperty("border-top", (j - 1) + "px dashed black");
+			}
+		}
+	}
+	imageLoaders[i].parentNode.parentNode.insertBefore(document.createElement("div").appendChild(subtitle).parentNode, imageLoaders[i].parentNode.parentNode.firstChild.nextSibling.nextSibling).style.setProperty("background-color", "#ff6");
 	imageLoaders[i].parentNode.parentNode.style.setProperty("height", "165px");
 }
