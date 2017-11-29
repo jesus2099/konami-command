1721429ee977cabf4e2bc81ec7907d878187a4f6
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2017-11-29 14:37:26 +0100

    cleanup

--- mb_INLINE-TRACK-ARTIST.user.js
+++ mb_INLINE-TRACK-ARTIST.user.js
@@ -5,7 +5,11 @@
 // @description  musicbrainz.org: highlights track title, length and artist differences in recording page
 // @homepage     http://userscripts-mirror.org/scripts/show/166877
 // @supportURL   https://github.com/jesus2099/konami-command/labels/mb_INLINE-TRACK-ARTIST
-// @compatible   opera(12.18.1872)+violentmonkey  my setup
+// @compatible   opera(12.18.1872)+violentmonkey      my setup
+// @compatible   vivaldi(1.0.435.46)+violentmonkey    my setup (ho.)
+// @compatible   vivaldi(1.13.1008.32)+violentmonkey  my setup (of.)
+// @compatible   firefox(47)+greasemonkey             tested sometimes
+// @compatible   chrome+violentmonkey                 should be same as vivaldi
 // @namespace    https://github.com/jesus2099/konami-command
 // @downloadURL  https://github.com/jesus2099/konami-command/raw/master/mb_INLINE-TRACK-ARTIST.user.js
 // @updateURL    https://github.com/jesus2099/konami-command/raw/master/mb_INLINE-TRACK-ARTIST.user.js
@@ -13,6 +17,7 @@
 // @licence      CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/)
 // @requester    culinko
 // @since        2013-05-07
+// @require      https://github.com/jesus2099/konami-command/raw/ec7e4fcbaea01784b86159ee50d7098c314202b7/lib/SUPER.js
 // @require      https://greasyfork.org/scripts/20120-cool-bubbles/code/COOL-BUBBLES.js?version=128868
 // @grant        none
 // @match        *://*.mbsandbox.org/recording/*
@@ -28,16 +33,16 @@ if (mbid && tracks.length > 0) {
 	var accol = -1;
 	var lecol = -1;
 	var ticol = -1;
-	for (var thi=0; thi<artistth.length && (ticol == -1 || accol == -1) ; thi++) {
+	for (var thi = 0; thi < artistth.length && (ticol == -1 || accol == -1) ; thi++) {
 		if (artistth[thi].textContent.match(/^title/i)) {
-			ticol = thi+1;
+			ticol = thi + 1;
 		}
 		if (artistth[thi].textContent.match(/length/i)) {
-			lecol = thi+1;
+			lecol = thi + 1;
 		}
 		if (artistth[thi].textContent.match(/release artist/i)) {
 			artistth = artistth[thi];
-			accol = thi+1;
+			accol = thi + 1;
 		}
 	}
 	if (ticol + lecol + accol > 0) {
@@ -49,39 +54,39 @@ if (mbid && tracks.length > 0) {
 				&& (res = res.documentElement)
 			) {
 				var reclen = res.querySelector("recording-list > recording > length");
-				reclen = time(reclen?reclen.textContent:0);
+				reclen = time(reclen ? reclen.textContent : 0);
 				var durspan = document.querySelector("div#sidebar dl.properties dd.length");
 				if (durspan) { durspan.replaceChild(document.createTextNode(reclen), durspan.firstChild); }
 				var wstracks = res.querySelectorAll("recording-list > recording > release-list > release > medium-list > medium > track-list > track");/*recording[id='"+mbid+"'] marche pas!?*/
-				for (var wt=0; wt<wstracks.length; wt++) {
+				for (var wt = 0; wt < wstracks.length; wt++) {
 					var relid = getParent(wstracks[wt], "release");
 					var discnum = wstracks[wt].parentNode.parentNode.querySelector("position");
 					var tracknum = wstracks[wt].querySelector("number");
 					if (
-						relid && (relid = relid.getAttribute("id")) &&
-						discnum && (discnum = discnum.textContent) &&
-						tracknum && (tracknum = tracknum.textContent)
+						relid && (relid = relid.getAttribute("id"))
+						&& discnum && (discnum = discnum.textContent)
+						&& tracknum && (tracknum = tracknum.textContent)
 					) {
-						for (var tt=0; tt<tracks.length; tt++) {
-							if (tracks[tt].querySelector("a[href*='/release/']") && tracks[tt].querySelector("a[href*='/release/']").getAttribute("href").indexOf(relid) > 0 && tracks[tt].querySelector("td:first-of-type").textContent.trim() == discnum+"."+tracknum) {
-								var titlespan = tracks[tt].querySelector("td:nth-child("+ticol+")");
+						for (var tt = 0; tt < tracks.length; tt++) {
+							if (tracks[tt].querySelector("a[href*='/release/']") && tracks[tt].querySelector("a[href*='/release/']").getAttribute("href").indexOf(relid) > 0 && tracks[tt].querySelector("td:first-of-type").textContent.trim() == discnum + "." + tracknum) {
+								var titlespan = tracks[tt].querySelector("td:nth-child(" + ticol + ")");
 								if (titlespan) {
 									var tit = document.querySelector("h1 a");
 									var wtit = wstracks[wt].querySelector("title");
 									if (tit && wtit && tit.textContent != wtit.textContent) {
-										titlespan.setAttribute("title", "≠ "+tit.textContent);
+										titlespan.setAttribute("title", "≠ " + tit.textContent);
 										titlespan.classList.add("name-variation");
 										titlespan.style.setProperty("text-shadow", "1px 2px 2px #999");
 										titlespan.style.setProperty("color", "maroon");
 									}
 								}
-								var lenspan = tracks[tt].querySelector("td:nth-child("+lecol+")");
+								var lenspan = tracks[tt].querySelector("td:nth-child(" + lecol + ")");
 								if (lenspan) {
 									var tlen = wstracks[wt].querySelector("length");
 									if (tlen && (tlen = time(tlen.textContent))) {
 										lenspan.replaceChild(document.createTextNode(tlen), lenspan.firstChild);
 										if (tlen != reclen) {
-											lenspan.setAttribute("title", "≠ "+reclen);
+											lenspan.setAttribute("title", "≠ " + reclen);
 											lenspan.classList.add("name-variation");
 											lenspan.style.setProperty("text-shadow", "1px 2px 2px #999");
 											lenspan.style.setProperty("color", "maroon");
@@ -90,22 +95,22 @@ if (mbid && tracks.length > 0) {
 								}
 								var ac = wstracks[wt].querySelectorAll("artist-credit > name-credit");
 								if (ac.length > 0) {
-									var acdiv = createTag("div", {"s":{"border":"4px solid gold","padding":"1px 2px","text-shadow":"1px 1px 2px #993"}});
+									var acdiv = createTag("div", {s: {border: "4px solid gold", padding: "1px 2px", textShadow: "1px 1px 2px #993"}});
 									if (artistth != null) {
 										artistth.insertBefore(acdiv.cloneNode(true), artistth.firstChild).appendChild(document.createTextNode("Track Artist"));
 										artistth = null;
 									}
-									var artisttd = tracks[tt].querySelector("td:nth-child("+accol+")");
+									var artisttd = tracks[tt].querySelector("td:nth-child(" + accol + ")");
 									if (artisttd) {
 										var acdivc = artisttd.insertBefore(acdiv.cloneNode(true), artisttd.firstChild);
-										for (var nc=0; nc<ac.length; nc++) {
+										for (var nc = 0; nc < ac.length; nc++) {
 											var arn = ac[nc].querySelector("artist");
 											var ari, art;
 											if (arn && (ari = arn.getAttribute("id")) && (art = arn.querySelector("name")) && (art = art.textContent) && (arn = ac[nc].querySelector("name")) && (arn = arn.textContent)) {
-												var acpart = createTag("a", {"a":{"href":"/artist/"+ari}}, arn);
+												var acpart = createTag("a", {a: {href: "/artist/" + ari}}, arn);
 												if (art != arn) {
 													acpart.setAttribute("title", art);
-													acdivc.appendChild(createTag("span", {"a":{"class":"name-variation"}}, acpart));
+													acdivc.appendChild(createTag("span", {a: {class: "name-variation"}}, acpart));
 												}
 												else {
 													acdivc.appendChild(acpart);
@@ -131,8 +136,8 @@ if (mbid && tracks.length > 0) {
 			coolBubble.error("Error " + this.status + (this.statusText ? " “" + this.statusText + "”" : "") + " while fetching inline track stuff.");
 		});
 		coolBubble.info("Loading “" + document.querySelector("h1").textContent + "” shadow recording…");
-		xhr.open("get", "/ws/2/recording?query=rid:"+mbid, true);
-		/* xhr.open("get", "/ws/2/recording/"+mbid+"?inc=releases+artist-credits+mediums", true); */
+		xhr.open("get", "/ws/2/recording?query=rid:" + mbid, true);
+		/* xhr.open("get", "/ws/2/recording/" + mbid + "?inc=releases+artist-credits+mediums", true); */
 		xhr.overrideMimeType("text/xml");
 		xhr.send(null);
 	}
@@ -144,30 +149,4 @@ function time(_ms) {
 		return d.getUTCMinutes() + ":" + (d.getUTCSeconds() / 100).toFixed(2).slice(2) + (d.getUTCMilliseconds() > 0 ? "." + (d.getUTCMilliseconds() / 1000).toFixed(3).slice(2) : "");
 	}
 	return "?:??";
-}
-function createTag(tag, gadgets, children) {
-	var t = document.createElement(tag);
-	if(t.tagName) {
-		if (gadgets) {
-			for (var attri in gadgets.a) { if (gadgets.a.hasOwnProperty(attri)) { t.setAttribute(attri, gadgets.a[attri]); } }
-			for (var style in gadgets.s) { if (gadgets.s.hasOwnProperty(style)) { t.style.setProperty(style.replace(/!/,""), gadgets.s[style], style.match(/!/)?"important":""); } }
-			for (var event in gadgets.e) { if (gadgets.e.hasOwnProperty(event)) { t.addEventListener(event, gadgets.e[event], false); } }
-		}
-		if (t.tagName == "A" && !t.getAttribute("href") && !t.style.getPropertyValue("cursor")) { t.style.setProperty("cursor", "pointer"); }
-		if (children) { var chldrn = children; if (typeof chldrn == "string" || chldrn.tagName) { chldrn = [chldrn]; } for(var child=0; child<chldrn.length; child++) { t.appendChild(typeof chldrn[child]=="string"?document.createTextNode(chldrn[child]):chldrn[child]); } t.normalize(); }
-	}
-	return t;
-}
-function getParent(obj, tag, cls) {
-	var cur = obj;
-	if (cur.parentNode) {
-		cur = cur.parentNode;
-		if (cur.tagName.toUpperCase() == tag.toUpperCase() && (!cls || cls && cur.classList.contains(cls))) {
-			return cur;
-		} else {
-			return getParent(cur, tag, cls);
-		}
-	} else {
-		return null;
-	}
 }
