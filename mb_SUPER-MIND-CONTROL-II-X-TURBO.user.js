41c0101910f076a32d396ee06b6fb1486a32a39c
jesus2099 <hfedscbnsr3uvpe@jetable.org>
2017-12-05 19:39:43 +0100

    FIX: Lag before menu gets stick

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -2,7 +2,7 @@
 var meta = {rawmdb: function() {
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2017.12.4.1740
+// @version      2017.12.5
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
 // @description  musicbrainz.org power-ups (mbsandbox.org too): RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / RELEASE_EDITOR_PROTECTOR. prevent accidental cancel by better tab key navigation / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / MAX_RECENT_ENTITIES / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / MERGE_USER_MENUS / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -1011,9 +1011,8 @@ function tagswitch(cont, urltxt) {
 ==========================================================================*/
 j2setting("STATIC_MENU", true, true, "makes the main MB menu always there when you need it (wihout scrolling top)");
 var mmenu = document.querySelector("div.header");
-var mlogo = document.querySelector("img.logo");
 var etais;
-if (j2sets.STATIC_MENU && mmenu && mlogo) {
+if (j2sets.STATIC_MENU && mmenu) {
 	etais = mmenu.parentNode.insertBefore(document.createElement("div"), mmenu);
 	//TODO: is this even supposed to work with // @run-at document-end?
 	self.addEventListener("load", smenu, false);
@@ -1021,7 +1020,7 @@ if (j2sets.STATIC_MENU && mmenu && mlogo) {
 	self.addEventListener("scroll", smenu, false);
 }
 function smenu(event) {
-	if (document.body.scrollTop + document.documentElement.scrollTop > self.getComputedStyle(mlogo).getPropertyValue("height").match(/\d+/)) {
+	if (document.body.scrollTop + document.documentElement.scrollTop > 0) {
 		mmenu.style.setProperty("position", "fixed");
 		mmenu.style.setProperty("top", "0px");
 		var computedWidth = self.getComputedStyle(mmenu.parentNode).getPropertyValue("width").match(/\d+/)[0];
