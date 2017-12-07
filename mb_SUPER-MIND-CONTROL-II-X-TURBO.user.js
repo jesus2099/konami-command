5dd36090438cf1b2f983a3834cab159f5b8d67c9
jesus2099 <jesus2099@users.noreply.github.com>
2017-12-07 02:05:10 +0100

    STATIC_MENU softer edges

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -2,7 +2,7 @@
 var meta = {rawmdb: function() {
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2017.12.4.1740
+// @version      2017.12.7
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
 // @description  musicbrainz.org power-ups (mbsandbox.org too): RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / RELEASE_EDITOR_PROTECTOR. prevent accidental cancel by better tab key navigation / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / MAX_RECENT_ENTITIES / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / MERGE_USER_MENUS / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -1030,7 +1030,7 @@ function smenu(event) {
 		mmenu.style.setProperty("width", computedWidth + "px");
 		mmenu.style.setProperty("border-bottom", "10px solid white");
 		mmenu.style.setProperty("background-color", "white");
-		mmenu.style.setProperty("box-shadow", "0px 10px 5px -4px #ccc");
+		mmenu.style.setProperty("box-shadow", "0px 10px 5px 8px white");
 		mmenu.style.setProperty("z-index", "9");
 		etais.style.setProperty("display", "block");
 		etais.style.setProperty("height", self.getComputedStyle(mmenu).getPropertyValue("height"));
