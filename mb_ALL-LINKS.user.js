jesus2099/konami-command

commit 372ce2f006e53c9d2b4bb58f8d2aa8eb60094391

MB adds trailing codes to Amazon links (#293)

mb_ALL-LINKS.user.js
@@ -485,7 +485,11 @@ function addExternalLink(parameters/*text, target, begin, end, sntarget, mbid, e
 			form.appendChild(document.createTextNode("*"));
 			li = createTag("li", {a: {ref: parameters.text}}, form);
 		} else {
-			var existingLink = sidebar.querySelector("ul.external_links > li a[href$='" + parameters.target.replace(/^https?:/, "") + "'], ul.external_links > li a[href$='" + parameters.target.replace(/^https?:/, "") + "/']"); // MBS adds a trailing slash to viaf URL https://github.com/jesus2099/konami-command/issues/293#issuecomment-376875307
+			var existingLink = 
+				sidebar.querySelector("ul.external_links > li a[href$='" + parameters.target.replace(/^https?:/, "") + "']")
+				|| parameters.target.match(/\bamazon\b/) && sidebar.querySelector("ul.external_links > li a[href$='" + parameters.target.replace(/^https?:/, "") + "?tag=musicbrainzjp-22']") // MBS adds a trailing code to ASIN URL
+				|| parameters.target.match(/\bviaf\b/) && sidebar.querySelector("ul.external_links > li a[href$='" + parameters.target.replace(/^https?:/, "") + "'/]") // MBS adds a trailing slash to viaf URL https://github.com/jesus2099/konami-command/issues/293#issuecomment-376875307
+			;
 			if (existingLink) {
 				newLink = false;
 				li = getParent(existingLink, "li");

commit 61909343d8c39bd414eb4432160ccdb97720dd7b

FIX: Regression typo in 372ce2f006e53c9d2b4bb58f8d2aa8eb60094391 for #293

mb_ALL-LINKS.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. ALL LINKS
-// @version      2018.3.28.1500
+// @version      2018.3.30
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_ALL-LINKS.user.js
 // @description  Hidden links include fanpage, social network, etc. (NO duplicates) Generated autolinks (configurable) includes plain web search, auto last.fm, Discogs and LyricWiki searches, etc. Shows begin/end dates on URL and provides edit link. Expands Wikidata links to wikipedia articles.
 // @homepage     http://userscripts-mirror.org/scripts/show/108889
@@ -497,7 +497,7 @@ function addExternalLink(parameters/*text, target, begin, end, sntarget, mbid, e
 				|| parameters.target.match(/\bamazon\.it\b/) && sidebar.querySelector("ul.external_links > li a[href$='" + parameters.target.replace(/^https?:/, "") + "?tag=music084d-21']")
 				|| parameters.target.match(/\bamazon\.es\b/) && sidebar.querySelector("ul.external_links > li a[href$='" + parameters.target.replace(/^https?:/, "") + "?tag=music02e-21']")
 				// MBS adds a trailing slash to viaf URL https://github.com/jesus2099/konami-command/issues/293#issuecomment-376875307
-				|| parameters.target.match(/\bviaf\b/) && sidebar.querySelector("ul.external_links > li a[href$='" + parameters.target.replace(/^https?:/, "") + "'/]")
+				|| parameters.target.match(/\bviaf\b/) && sidebar.querySelector("ul.external_links > li a[href$='" + parameters.target.replace(/^https?:/, "") + "/']")
 			;
 			if (existingLink) {
 				newLink = false;