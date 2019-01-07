61f098676194870a27b3e1f0e5ce260816b60053
jesus2099 <jesus2099@users.noreply.github.com>
2019-01-07 12:13:23 +0100

    Small cleanup and rename mb. ALL RELEASE GROUPS (1/2) mb_ALWAYS-SHOW-ALL-RELEASE-GROUPS.user.js → mb_ALL-RELEASE-GROUPS.user.js

--- mb_ALWAYS-SHOW-ALL-RELEASE-GROUPS.user.js
+++ mb_ALWAYS-SHOW-ALL-RELEASE-GROUPS.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         mb. ALWAYS SHOW ALL RELEASE GROUPS
-// @version      2019.1.3.beta
+// @version      2019.1.7.beta
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_ALWAYS-SHOW-ALL-RELEASE-GROUPS.user.js
-// @description  Copy of mb_PREFERRED-MBS, beta test for the moment. cf. https://community.metabrainz.org/t/the-hidden-display-bootlegs-toggle/410173/2?u=jesus2099 and https://chatlogs.metabrainz.org/brainzbot/musicbrainz/msg/4303822/
-// @supportURL   https://github.com/jesus2099/konami-command/labels/mb_ALWAYS-SHOW-ALL-RELEASE-GROUPS
+// @description  Artist overview page (discography): Show all release groups by default, then you can filter out bootlegs to show only official release groups (instead of the opposite default)
+// @supportURL   https://github.com/jesus2099/konami-command/labels/mb_ALL-RELEASE-GROUPS
 // @compatible   vivaldi(1.0.435.46)+violentmonkey   my setup (ho.)
 // @compatible   vivaldi(2.2.1388.37)+violentmonkey  my setup (of.)
 // @compatible   firefox(64.0)+greasemonkey          tested sometimes
 // @compatible   chrome+violentmonkey                should be same as vivaldi
 // @namespace    jesus2099/shamo
-// @downloadURL  https://github.com/jesus2099/konami-command/raw/master/mb_ALWAYS-SHOW-ALL-RELEASE-GROUPS.user.js
-// @updateURL    https://github.com/jesus2099/konami-command/raw/master/mb_ALWAYS-SHOW-ALL-RELEASE-GROUPS.user.js
+// @downloadURL  https://github.com/jesus2099/konami-command/raw/master/mb_ALL-RELEASE-GROUPS.user.js
+// @updateURL    https://github.com/jesus2099/konami-command/raw/master/mb_ALL-RELEASE-GROUPS.user.js
 // @author       jesus2099
 // @licence      CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/
 // @licence      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
@@ -29,7 +29,7 @@ document.addEventListener("mousedown", function(event) {
 		if (element.tagName != "A") {
 			element = getParent(element, "a");
 		}
-		if (element && element.tagName == "A" && !element.classList.contains("jesus2099-bypass-mb_ALWAYS-SHOW-ALL-RELEASE-GROUPS")) {
+		if (element && element.tagName == "A" && !element.classList.contains("jesus2099-bypass-mb_ALL-RELEASE-GROUPS")) {
 			process(element);
 		}
 	}
