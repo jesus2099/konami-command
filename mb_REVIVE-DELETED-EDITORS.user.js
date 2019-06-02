4e91e64f795c50dd11665c02abc09b6adda303e4
jesus2099 <jesus2099@users.noreply.github.com>
2019-06-02 20:48:14 +0200

    Good luck, inhouseuk.

--- mb_REVIVE-DELETED-EDITORS.user.js
+++ mb_REVIVE-DELETED-EDITORS.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. REVIVE DELETED EDITORS
-// @version      2019.1.24
+// @version      2019.6.2
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_REVIVE-DELETED-EDITORS.user.js
 // @description  musicbrainz.org: reveal deleted editors’ names and emphasizes your own name to standout in MB pages
 // @supportURL   https://github.com/jesus2099/konami-command/labels/mb_REVIVE-DELETED-EDITORS
@@ -50,6 +50,7 @@
 var editors = {
 	      "387": ["2001-05-22", "2010-10-29", "tturner"],
 	    "32978": ["2003-12-13", "2005-12-16", "tenebrous"],
+	    "49249": ["2004-05-15", "2017-04-21", "inhouseuk"],
 	    "52273": ["2004-06-14", "2015-06-06", "DrMuller"],
 	    "93418": ["2005-02-08", "2013-03-05", "Rhymeless"],
 	    "95678": ["2005-02-18", "2012-09-24", "brianfreud"],
