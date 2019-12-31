998a7926bd10b2f2631cc4bae1854849765a828d
jesus2099 <jesus2099@users.noreply.github.com>
2019-12-31 12:11:04 +0100

    Display a big easy banner on mobile devices (again)
    Fixes issue #483: Revert “Less intrusive mobile version switch link”
    
    Also restores a mobile device test.
    A better one than the original test that I removed.
    
    Original (deleted) test:
    
        self.innerWidth < 888
    
    Better (?) test:
    
        self.matchMedia("only screen and (max-width: 480px)").matches

--- jira_MOBILE-VIEW-TOGGLE-SWITCH.user.js
+++ jira_MOBILE-VIEW-TOGGLE-SWITCH.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jira. MOBILE VIEW TOGGLE SWITCH
-// @version      2019.11.5
-// @description  Provides a convenient "Switch to mobile version" toggle link in all JIRA sites, in addition to the existing reverse "Switch to desktop version"
+// @version      2019.12.31
+// @description  Provides a convenient "Switch to mobile version" big header toggle link in all JIRA sites, in addition to the existing reverse "Switch to desktop version"
 // @namespace    https://github.com/jesus2099/konami-command
 // @author       jesus2099
 // @since        2019-11-03
@@ -15,22 +15,32 @@ var issuePrefix = {
 	desktop: "/browse/",
 	mobile: "/plugins/servlet/mobile#issue/"
 };
-var desktop = (new RegExp("^" + issuePrefix.desktop)).test(location.pathname);
-var mobile = !desktop;
+var isMobilePage = !(new RegExp("^" + issuePrefix.desktop)).test(location.pathname);
+var isMobileDevice = self.matchMedia("only screen and (max-width: 480px)").matches;
 var issue;
 if (
-	desktop && (issue = location.pathname.match(new RegExp("^" + issuePrefix.desktop + "(" + issuePattern + ")")))
-	|| mobile && (issue = (location.pathname + location.hash).match(new RegExp("^" + issuePrefix.mobile + "(" + issuePattern + ")")))
+	!isMobilePage && (issue = location.pathname.match(new RegExp("^" + issuePrefix.desktop + "(" + issuePattern + ")")))
+	|| isMobilePage && (issue = (location.pathname + location.hash).match(new RegExp("^" + issuePrefix.mobile + "(" + issuePattern + ")")))
 ) {
 	issue = issue[1];
 	var css = document.createElement("style");
 	css.setAttribute("type", "text/css");
 	document.head.appendChild(css);
 	css = css.sheet;
-	css.insertRule("a#key-val + a[href^='" + issuePrefix.mobile + "'], a#switch-to-desktop-footer { background: #ff6; color: black !important; text-decoration: underline; }", 0);
+	css.insertRule("a#switch-to-desktop-header, a#key-val + a[href^='" + issuePrefix.mobile + "'], a#switch-to-desktop-footer { background: #ff6; color: black !important; text-decoration: underline; }", 0);
+	css.insertRule("a#switch-to-desktop-header, a#switch-to-desktop-footer { font-size: 4em; }", 0);
+	css.insertRule("a#switch-to-desktop-header { display: block; padding: 1em; text-align: center; }", 0);
 	var issueLink;
 	if (issueLink = document.querySelector("a[href='" + issuePrefix.desktop + issue + "']")) {
-		issueLink.parentNode.appendChild(document.createTextNode(" / "));
-		issueLink.parentNode.appendChild(document.createElement("a")).appendChild(document.createTextNode("Switch to " + (desktop ? "mobile" : "desktop") + " version")).parentNode.setAttribute("href", (desktop ? issuePrefix.mobile : issuePrefix.desktop) + issue);
+		if (isMobileDevice) {
+			var mobileSwitch = document.createElement("a");
+			mobileSwitch.setAttribute("id", "switch-to-desktop-header");
+			mobileSwitch.appendChild(document.createTextNode("Switch to mobile version"));
+			mobileSwitch.setAttribute("href", "/plugins/servlet/mobile#issue/" + issue)
+			document.body.insertBefore(mobileSwitch, document.body.firstChild);
+		} else {
+			issueLink.parentNode.appendChild(document.createTextNode(" / "));
+			issueLink.parentNode.appendChild(document.createElement("a")).appendChild(document.createTextNode("Switch to mobile version")).parentNode.setAttribute("href", issuePrefix.mobile + issue);
+		}
 	}
 }
