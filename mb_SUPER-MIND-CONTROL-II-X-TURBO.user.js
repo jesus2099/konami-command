354426fa5546a38e45598948c85d9827c3cc75c1
jesus2099 <jesus2099@users.noreply.github.com>
2020-09-25 17:55:58 +0200

    Revert my split to take stat before opening parenthesis
    Regression spotted by @kellnerd

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -2,7 +2,7 @@
 var meta = {rawmdb: function() {
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2020.9.25.1142
+// @version      2020.9.25.1755
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
 // @description  musicbrainz.org power-ups (mbsandbox.org too): RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / RELEASE_EDITOR_PROTECTOR. prevent accidental cancel by better tab key navigation / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -484,7 +484,7 @@ function isEncoded(string) {
 	return result != string;
 }
 function readStat(statsCell) {
-	return parseInt(statsCell.textContent.replace(/\D/g, ""), 10);
+	return parseInt(statsCell.textContent.split("(")[0].replace(/\D/g, ""), 10);
 }
 function writeStat(statsCell, stat, total) {
 	var a = statsCell.getElementsByTagName("a")[0];
