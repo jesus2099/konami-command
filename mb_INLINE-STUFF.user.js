51f2affaf51da8555b85227a605a7c82184f3dff
jesus2099 <jesus2099@users.noreply.github.com>
2020-10-16 10:09:41 +0200

    Trigger ISRC script on editor edit histories
    Fixes #522

--- mb_INLINE-STUFF.user.js
+++ mb_INLINE-STUFF.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. INLINE STUFF
-// @version      2020.2.5
+// @version      2020.10.16
 // @description  musicbrainz.org release page: Inline recording names, comments, ISRC and AcoustID. Displays CAA count and add link if none. Highlights duplicates in releases and edits.
 // @compatible   vivaldi(2.4.1488.38)+violentmonkey  my setup (office)
 // @compatible   vivaldi(1.0.435.46)+violentmonkey   my setup (home, xp)
@@ -20,6 +20,7 @@
 // @include      /^https?://(\w+\.mbsandbox|(\w+\.)?musicbrainz)\.org/edit/subscribed/
 // @include      /^https?://(\w+\.mbsandbox|(\w+\.)?musicbrainz)\.org/release/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}([^/]|$|\/disc\/\d+)/
 // @include      /^https?://(\w+\.mbsandbox|(\w+\.)?musicbrainz)\.org/search/edits\?/
+// @include      /^https?://(\w+\.mbsandbox|(\w+\.)?musicbrainz)\.org/user/[^/]+/edits/
 // @run-at       document-end
 // ==/UserScript==
 "use strict";
