jesus2099/konami-command

commit 55e790e6997868ec417c33ead58cfea991c15f3b

Convert weird times like 2:73 and accept length inferior to 1:00
Also make sure it's able to parse both minc (m's") and MBS (h:m:s) track
parser formats without mistaking track numbers for track durations.

Fixes #458.

mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1404,6 +1404,7 @@ function TRACKLIST_TOOLS_buttonHandler(event) {
 				}
 			/* :::: TRACK LENGTH PARSER :::: */
 			} else if (event.target.classList.contains(userjs + "track-length-parser")) {
+				var durationParser = "(?:(?:(\\b\\d\\b)[°h])?(\\b\\d{1,3}\\b)[′’'m])?(\\b\\d{2}\\b)[″”\"s]|(?:(?:(\\b\\d\\b):)?(\\b\\d{1,3}\\b):)?(\\b\\d{2}\\b)";
 				var erase = event.target.textContent.match(/erase/i) || event.ctrlKey;
 				var inputs = TRACKLIST_TOOLS_getInputs("td.length > input.track-length[type='text']:not(.disabled-hint)", event.target, event);
 				var distitle = document.querySelector("td.length > input.track-length[type='text'].disabled-hint");
@@ -1413,13 +1414,17 @@ function TRACKLIST_TOOLS_buttonHandler(event) {
 					event.target.setAttribute("disabled", "disabled");
 					event.target.setAttribute("title", distitle);
 					alert(distitle);
-				} else if (erase && confirm("Are you sure you want to ERASE all track times?") || times && (times = times.match(/\b\d{1,3}[:′’']\d\d\b[″”"]?/g))) {
+				} else if (erase && confirm("Are you sure you want to ERASE all track times?") || times && (times = times.match(new RegExp(durationParser, "g")))) {
 					if (erase || inputs.length == times.length || confirm("ACHTUNG, detected times and tracks count mismatch.\nThere are " + times.length + " lengths detected in your text, butt\nthere are " + inputs.length + " tracks in the tracklist.\nAre you sure to go on?")) {
 						for (var t = 0, i = 0; (erase || t < times.length) && i < inputs.length; t++, i++) {
 							var time = "";
 							if (!erase) {
-								time = times[t].match(/(\d+)\D+(\d+)/);
-								time = time[1] + ":" + time[2];
+								var date = new Date();
+								time = times[t].match(new RegExp(durationParser));
+								date.setHours(time[1] || time[4] || 0);
+								date.setMinutes(time[2] || time[5] || 0);
+								date.setSeconds(time[3] || time[6]);
+								time = (date.getHours() * 60 + date.getMinutes()) + ":" + (date.getSeconds() < 10 ? "0" : "") + date.getSeconds();
 							}
 							inputs[i].style.removeProperty("background-color");
 							if (inputs[i].value != time) {

commit 371057bcac7ee0390362543afc553a21da32766b

Don't take 2 digit track numbers for durations
Regression brought by 55e790e6997868ec417c33ead58cfea991c15f3b
Still works for minc's second only tracks

Closes #534

mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -2,7 +2,7 @@
 var meta = {rawmdb: function() {
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2020.11.29
+// @version      2020.11.29.2
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
 // @description  musicbrainz.org power-ups: RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -1414,7 +1414,7 @@ function TRACKLIST_TOOLS_buttonHandler(event) {
 				}
 			/* :::: TRACK LENGTH PARSER :::: */
 			} else if (event.target.classList.contains(userjs + "track-length-parser")) {
-				var durationParser = "(?:(?:(\\b\\d\\b)[°h])?(\\b\\d{1,3}\\b)[′’'m])?(\\b\\d{2}\\b)[″”\"s]|(?:(?:(\\b\\d\\b):)?(\\b\\d{1,3}\\b):)?(\\b\\d{2}\\b)";
+				var durationParser = "(?:(?:(\\b\\d\\b)[°h])?(\\b\\d{1,3}\\b)[′’'m])?(\\b\\d{2}\\b)[″”\"s]|(?:(?:(\\b\\d\\b):)?(\\b\\d{1,3}\\b):)(\\b\\d{2}\\b)";
 				var erase = event.target.textContent.match(/erase/i) || event.ctrlKey;
 				var inputs = TRACKLIST_TOOLS_getInputs("td.length > input.track-length[type='text']:not(.disabled-hint)", event.target, event);
 				var distitle = document.querySelector("td.length > input.track-length[type='text'].disabled-hint");