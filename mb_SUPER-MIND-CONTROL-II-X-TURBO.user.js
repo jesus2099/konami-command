573f9613da618910721c950f25415d2f4471ae47
jesus2099 <jesus2099@users.noreply.github.com>
2021-02-10 00:07:54 +0100

    Better .name-variation STABILO BOSS style

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2021.2.5
+// @version      2021.2.9
 // @description  musicbrainz.org power-ups: RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @namespace    https://github.com/jesus2099/konami-command
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -644,9 +644,9 @@ function EASY_DATE_nextField(event) {
 // ## SPOT_AC ##
 // ==========================================================================
 j2setting("SPOT_AC", true, true, "name variations (Artist Credit, track name ≠ recording name, etc.) stand out");
-j2setting("SPOT_AC_css", "border-bottom: 2px dashed maroon;", true, "CSS syntax (on “span.name-variation”)");
+j2setting("SPOT_AC_css", "background-color: #FF6; border-bottom: 2px dashed maroon;", true, "CSS syntax (on “span.name-variation”)");
 if (j2sets.SPOT_AC) {
-	j2superturbo.addCSSRule("span.name-variation { " + j2sets.SPOT_AC_css + " }");
+	j2superturbo.addCSSRule(".name-variation { " + j2sets.SPOT_AC_css + " }");
 }
 // ================================================================= DISPLAY+
 // ## SPOT_CAA ##
