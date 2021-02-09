d4fe89a1cdcb56c5f0fb6074d52620a16e41288c
jesus2099 <jesus2099@users.noreply.github.com>
2021-02-10 00:05:09 +0100

    Remove track artist display in recording page
    Now it’s done by MBS-10355, thanks to reosarevok!
    
    Now remains 3 things:
    
    - millisecond display
    - spot track length variations
    - spot track title variations
    
    That I will soon move to mb_INLINE-STUFF (#129).

--- mb_INLINE-TRACK-ARTIST.user.js
+++ mb_INLINE-TRACK-ARTIST.user.js
@@ -1,16 +1,10 @@
 // ==UserScript==
 // @name         mb. INLINE TRACK ARTIST
-// @version      2021.1.19.2099
-// @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_INLINE-TRACK-ARTIST.user.js
+// @version      2021.2.9
 // @description  musicbrainz.org: highlights track title, length and artist differences in recording page
-// @supportURL   https://github.com/jesus2099/konami-command/labels/mb_INLINE-TRACK-ARTIST
-// @compatible   vivaldi(2.4.1488.38)+violentmonkey   my setup (of.)
-// @compatible   vivaldi(1.0.435.46)+violentmonkey    my setup (ho.)
-// @compatible   firefox(47.0)+greasemonkey           tested sometimes
-// @compatible   chrome+violentmonkey                 should be same as vivaldi
 // @namespace    https://github.com/jesus2099/konami-command
+// @supportURL   https://github.com/jesus2099/konami-command/labels/mb_INLINE-TRACK-ARTIST
 // @downloadURL  https://github.com/jesus2099/konami-command/raw/master/mb_INLINE-TRACK-ARTIST.user.js
-// @updateURL    https://github.com/jesus2099/konami-command/raw/master/mb_INLINE-TRACK-ARTIST.user.js
 // @author       jesus2099
 // @licence      CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/
 // @licence      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
@@ -28,23 +22,17 @@ var mbid = self.location.pathname.match(/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-
 var tracks = document.querySelectorAll("div#content table.tbl > tbody > tr");
 if (mbid && tracks.length > 0) {
 	var releaseArtistColumnHeader = getParent(tracks[0], "table").querySelectorAll("thead > tr > th");
-	var releaseArtistColumnIndex;
 	var lengthColumnIndex;
 	var trackTitleColumnIndex;
 	/* locate length, track title and release artist columns */
 	for (var columnIndex = 0; columnIndex < releaseArtistColumnHeader.length; columnIndex++) {
 		if (releaseArtistColumnHeader[columnIndex].textContent.match(/^tit\w+$/i)) {
 			trackTitleColumnIndex = columnIndex + 1;
-		}
-		if (releaseArtistColumnHeader[columnIndex].classList.contains("treleases")) {
+		} else if (releaseArtistColumnHeader[columnIndex].classList.contains("treleases")) {
 			lengthColumnIndex = columnIndex + 1;
 		}
-		if (releaseArtistColumnHeader[columnIndex].textContent.match(/artie?ste?|künstler/i)) {
-			releaseArtistColumnHeader = releaseArtistColumnHeader[columnIndex];
-			releaseArtistColumnIndex = columnIndex + 1;
-		}
 	}
-	if (trackTitleColumnIndex && lengthColumnIndex && releaseArtistColumnIndex) {
+	if (trackTitleColumnIndex && lengthColumnIndex) {
 		var xhr = new XMLHttpRequest();
 		xhr.addEventListener("load", function(event) {
 			var wsRecording = this.responseXML;
@@ -59,13 +47,12 @@ if (mbid && tracks.length > 0) {
 				var wsTracks = wsRecording.querySelectorAll("recording[id='" + mbid + "'] > release-list > release > medium-list > medium > track-list > track");
 				for (var wst = 0; wst < wsTracks.length; wst++) {
 					var wsRelease = getParent(wsTracks[wst], "release");
-					var wsReleaseMBID, wsReleaseAC;
+					var wsReleaseMBID;
 					var wsPosition = wsTracks[wst].parentNode.parentNode.querySelector("position");
 					var wsTrackPosition = wsTracks[wst].querySelector("position");
 					if (
 						wsRelease
 						&& (wsReleaseMBID = wsRelease.getAttribute("id"))
-						&& (wsReleaseAC = wsRelease.querySelector("artist-credit"))
 						&& wsPosition && (wsPosition = wsPosition.textContent)
 						&& wsTrackPosition && (wsTrackPosition = wsTrackPosition.textContent)
 					) {
@@ -77,10 +64,8 @@ if (mbid && tracks.length > 0) {
 									var trackTitle = document.querySelector("h1 a");
 									var wsTrackTitle = wsTracks[wst].querySelector("title");
 									if (trackTitle && wsTrackTitle && trackTitle.textContent != wsTrackTitle.textContent) {
-										trackTitleCell.setAttribute("title", "≠ " + trackTitle.textContent);
-										trackTitleCell.classList.add("name-variation");
-										trackTitleCell.style.setProperty("text-shadow", "1px 2px 2px #999");
-										trackTitleCell.style.setProperty("color", "maroon");
+										trackTitleCell.firstChild.setAttribute("title", "≠ " + trackTitle.textContent);
+										trackTitleCell.firstChild.classList.add("name-variation");
 									}
 								}
 								/* display recording/track length discrepency */
@@ -88,44 +73,14 @@ if (mbid && tracks.length > 0) {
 								if (trackLengthCell) {
 									var wsTrackLength = wsTracks[wst].querySelector("length");
 									if (wsTrackLength && (wsTrackLength = time(wsTrackLength.textContent))) {
-										trackLengthCell.replaceChild(document.createTextNode(wsTrackLength), trackLengthCell.firstChild);
 										if (wsTrackLength != wsRecordingLength) {
-											trackLengthCell.setAttribute("title", "≠ " + wsRecordingLength);
-											trackLengthCell.classList.add("name-variation");
-											trackLengthCell.style.setProperty("text-shadow", "1px 2px 2px #999");
-											trackLengthCell.style.setProperty("color", "maroon");
-										}
-									}
-								}
-								/* artist credit */
-								if (!wsTracks[wst].querySelector("artist-credit").isEqualNode(wsReleaseAC)) {
-									var wsTrackArtistCredits = wsTracks[wst].querySelectorAll("artist-credit > name-credit");
-									if (wsTrackArtistCredits.length > 0) {
-										var trackArtistCreditHeader = createTag("div", {s: {border: "4px solid gold", padding: "1px 2px", textShadow: "1px 1px 2px #993"}});
-										if (releaseArtistColumnHeader != null) {
-											releaseArtistColumnHeader.insertBefore(trackArtistCreditHeader.cloneNode(true), releaseArtistColumnHeader.firstChild).appendChild(document.createTextNode("Track Artist"));
-											releaseArtistColumnHeader = null;
-										}
-										var releaseArtistCell = tracks[t].querySelector("td:nth-child(" + releaseArtistColumnIndex + ")");
-										if (releaseArtistCell) {
-											var trackArtistCreditFragment = releaseArtistCell.insertBefore(trackArtistCreditHeader.cloneNode(true), releaseArtistCell.firstChild);
-											for (var wstac = 0; wstac < wsTrackArtistCredits.length; wstac++) {
-												var wsArtist = wsTrackArtistCredits[wstac].querySelector("artist");
-												var wsArtistMBID, wsArtistTooltip;
-												if (wsArtist && (wsArtistMBID = wsArtist.getAttribute("id")) && (wsArtistTooltip = wsArtist.querySelector("name")) && (wsArtistTooltip = wsArtistTooltip.textContent) && (wsArtist = wsTrackArtistCredits[wstac].querySelector("name")) && (wsArtist = wsArtist.textContent)) {
-													var trackArtistCreditElement = createTag("a", {a: {href: "/artist/" + wsArtistMBID}}, wsArtist);
-													if (wsArtistTooltip != wsArtist) {
-														trackArtistCreditElement.setAttribute("title", wsArtistTooltip);
-														trackArtistCreditFragment.appendChild(createTag("span", {a: {class: "name-variation"}}, trackArtistCreditElement));
-													} else {
-														trackArtistCreditFragment.appendChild(trackArtistCreditElement);
-													}
-												}
-												var wsTrackArtistCreditJoinPhrase = wsTrackArtistCredits[wstac].getAttribute("joinphrase");
-												if (wsTrackArtistCreditJoinPhrase) {
-													trackArtistCreditFragment.appendChild(document.createTextNode(wsTrackArtistCreditJoinPhrase));
-												}
-											}
+											trackLengthCell.replaceChild(
+												createTag(
+													"span", {a: { class: "name-variation", title: "≠ " + wsRecordingLength,}},
+													document.createTextNode(wsTrackLength)
+												),
+												trackLengthCell.firstChild
+											);
 										}
 									}
 								}
