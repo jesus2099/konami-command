a4d0695089d0029310853028a0670441e97ea471
jesus2099 <jesus2099@users.noreply.github.com>
2021-02-25 22:34:01 +0100

    mb. HIDE DIGITAL RELEASES
    I am over saturated.
    They slow me down.

---
+++ mb_HIDE-DIGITAL-RELEASES.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         mb. HIDE DIGITAL RELEASES
+// @version      2021.2.25
+// @description  musicbrainz.org: (VERY BASIC AT THE MOMENT) Release group page: Hide digital releases
+// @namespace    https://github.com/jesus2099/konami-command
+// @supportURL   https://github.com/jesus2099/konami-command/labels/mb_HIDE-DIGITAL-RELEASES
+// @downloadURL  https://github.com/jesus2099/konami-command/raw/master/mb_HIDE-DIGITAL-RELEASES.user.js
+// @author       jesus2099
+// @licence      CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/
+// @licence      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
+// @since        2021-02-25; https://community.metabrainz.org/t/digital-releases/361875/125?u=jesus2099
+// @icon         data:image/gif;base64,R0lGODlhEAAQAKEDAP+/3/9/vwAAAP///yH/C05FVFNDQVBFMi4wAwEAAAAh/glqZXN1czIwOTkAIfkEAQACAwAsAAAAABAAEAAAAkCcL5nHlgFiWE3AiMFkNnvBed42CCJgmlsnplhyonIEZ8ElQY8U66X+oZF2ogkIYcFpKI6b4uls3pyKqfGJzRYAACH5BAEIAAMALAgABQAFAAMAAAIFhI8ioAUAIfkEAQgAAwAsCAAGAAUAAgAAAgSEDHgFADs=
+// @grant        none
+// @include      /^https?:\/\/(\w+\.)?musicbrainz\.org\/release-group\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/
+// @run-at       document-ready
+// ==/UserScript==
+"use strict";
+let releases = document.querySelectorAll("tbody > tr:not(.subh)");
+for (let r = 0; r < releases.length; r++) {
+	if (releases[r].cells[2].textContent.match(/^digital|numérique$/iu)) {
+		releases[r].style.setProperty("display", "none");
+	}
+}
