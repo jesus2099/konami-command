3899cf75f0913b4345d326c8a87ad2b05e66ae0f
jesus2099 <jesus2099@users.noreply.github.com>
2021-03-03 00:22:28 +0100

    Compatible with opera(12.18.1872)+violentmonkey
    My oldest setup

--- mb_PRINT-ALL-PAGES.user.js
+++ mb_PRINT-ALL-PAGES.user.js
@@ -3,6 +3,7 @@
 // @version      2021.3.2
 // @description  musicbrainz.org: (VERY BASIC AT THE MOMENT) Print your complete collections to make your shopping lists or check lists. It will work on more than just collections.
 // @namespace    https://github.com/jesus2099/konami-command
+// @compatible   opera(12.18.1872)+violentmonkey     my oldest setup
 // @supportURL   https://github.com/jesus2099/konami-command/labels/mb_PRINT-ALL-PAGES
 // @downloadURL  https://github.com/jesus2099/konami-command/raw/master/mb_PRINT-ALL-PAGES.user.js
 // @author       jesus2099
@@ -16,22 +17,22 @@
 // @run-at       document-ready
 // ==/UserScript==
 "use strict";
-const userjs = {
+var userjs = {
 	id: GM_info.script.name.replace(/\.\s/, "_").replace(/\s/g, "-"),
-}
-let lastPage;
+};
+var lastPage;
 
 // locate the pagination toolbar or create it if none
-let form = document.querySelector("div#content h2 + form[method='post']");
+var form = document.querySelector("div#content h2 + form[method='post']");
 if (form) {
-	let pagination = form.querySelector("nav ul.pagination");
+	var pagination = form.querySelector("nav ul.pagination");
 	if (!pagination) {
 		lastPage = 1;
 		pagination = form.insertBefore(document.createElement("nav"), form.firstChild).appendChild(createTag("ul", {a: {class: "pagination"}}));
 		pagination.appendChild(document.createElement("li"));
 	}
 	// put the little button instead of the first page disabled Previous button
-	pagination.firstChild.replaceChildren(createTag("li", {}, createTag(
+	replaceChildren(createTag("li", {}, createTag(
 		"a",
 		{
 			a: {title: GM_info.script.name + " version " + GM_info.script.version},
@@ -39,11 +40,11 @@ if (form) {
 			e: {click: preparePage}
 		},
 		"Load all pages for print"
-	)));
+	)), pagination.firstChild);
 }
 
 function preparePage(event) {
-	let css = {
+	var css = {
 		all: document.createElement("style"),
 		print: document.createElement("style")
 	};
@@ -62,16 +63,16 @@ function preparePage(event) {
 	css.print.insertRule("." + userjs.id + "toolbar { display: none; }", 0); // don't print this script toolbar
 	// hide stuff from both screen and print
 	// -------------------------------------
-	css.all.insertRule("div#content > form > nav > ul { display: none; }");
+	css.all.insertRule("div#content > form > nav > ul { display: none; }", 0);
 	// hide mb_FUNKEY-ILLUSTRATED-RECORDS bigpics
-	let bigpics = document.querySelector("div.jesus2099userjs154481bigbox");
+	var bigpics = document.querySelector("div.jesus2099userjs154481bigbox");
 	if (bigpics) {
 		// remove bigpics to skip further image loadings
 		removeNode(bigpics);
 	}
 	// disable collection highlighter
-	let collectionHighlights = document.querySelectorAll("[class*='collectionHighlighter']");
-	for (let i = 0; i < collectionHighlights.length; i++) {
+	var collectionHighlights = document.querySelectorAll("[class*='collectionHighlighter']");
+	for (var i = 0; i < collectionHighlights.length; i++) {
 		collectionHighlights[i].className = collectionHighlights[i].className.replace(/\bcollectionHighlighter(Box|Item|Row)\b/g);
 	}
 	if (lastPage !== 1) {
@@ -79,20 +80,20 @@ function preparePage(event) {
 	}
 }
 function appendPage(page) {
-	let loading = document.getElementById(userjs.id + "loading");
+	var loading = document.getElementById(userjs.id + "loading");
 	if (!loading) {
 		loading = createTag("div", {a: {id: userjs.id + "loading"}, s: {textAlign: "center"}}, [
 			createTag("img", {a: {alt: "loading", src: "/static/images/icons/loading.gif"}}),
 			" ",
 			document.createElement("span")
 		]);
-		document.querySelector("div#content > h2").after(loading);
+		addAfter(loading, document.querySelector("div#content > h2"));
 	}
-	loading.lastChild.replaceChildren(document.createTextNode("Loading page " + page + "…"));
+	replaceChildren(document.createTextNode("Loading page " + page + "…"), loading.lastChild);
 	// WORK IN PROGRESS
 	if (page < 5) {
 		setTimeout(function() { appendPage(page + 1)}, 1100);
 	} else {
-		setTimeout(function() {document.getElementById(userjs.id + "loading").replaceChildren(document.createTextNode("Thanks for testing, I have yet to copy my page loader from mb_COLLECTION-HIGHLIGHTER. Stay tuned."))}, 1000);
+		setTimeout(function() {replaceChildren(document.createTextNode("Thanks for testing, I have yet to copy my page loader from mb_COLLECTION-HIGHLIGHTER. Stay tuned."), document.getElementById(userjs.id + "loading") ) ; }, 1000);
 	}
 }
