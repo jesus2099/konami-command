ba4c331de51ac4f0ebc4718b06cee6778eea9bbd
jesus2099 <jesus2099@users.noreply.github.com>
2021-03-08 11:24:15 +0100

    Learn new localised digital media names
    ARTIFICIAL INTELLIGENCE ;)
    
    Closes #567

--- mb_HIDE-DIGITAL-RELEASES.user.js
+++ mb_HIDE-DIGITAL-RELEASES.user.js
@@ -12,29 +12,52 @@
 // @icon         data:image/gif;base64,R0lGODlhEAAQAKEDAP+/3/9/vwAAAP///yH/C05FVFNDQVBFMi4wAwEAAAAh/glqZXN1czIwOTkAIfkEAQACAwAsAAAAABAAEAAAAkCcL5nHlgFiWE3AiMFkNnvBed42CCJgmlsnplhyonIEZ8ElQY8U66X+oZF2ogkIYcFpKI6b4uls3pyKqfGJzRYAACH5BAEIAAMALAgABQAFAAMAAAIFhI8ioAUAIfkEAQgAAwAsCAAGAAUAAgAAAgSEDHgFADs=
 // @grant        none
 // @include      /^https?:\/\/(\w+\.)?musicbrainz\.org\/release-group\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/
+// @include      /^https?:\/\/(\w+\.)?musicbrainz\.org\/release\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\/edit$/
 // @run-at       document-ready
 // ==/UserScript==
 "use strict";
-let releases = document.querySelectorAll("tbody > tr:not(.subh)");
-var downlordRelease = {
-	// localised name of medium type number 12
-	de: "Digitales Medium",
-	en: "Digital Media",
-	fr: "Support numérique",
-	it: "Supporto digitale",
-	nl: "digitale media",
-};
-var lang = document.getElementsByTagName("html")[0].getAttribute("lang") || "en";
-if (releases.length > 0) {
-	for (let r = 0; r < releases.length; r++) {
-		if (
-			// don’t match half physical releases
-			!releases[r].cells[2].textContent.match(/\+/)
-			// match fully digital releases
-			&& releases[r].cells[2].textContent.match(new RegExp("([0-9]+×)?" + downlordRelease[lang], "iu"))
-		) {
-			releases[r].style.setProperty("display", "none");
+var MBGlossary = JSON.parse(localStorage.getItem("MBGlossary"));
+if (!MBGlossary) {
+	MBGlossary = {
+		"disc-format": {
+			12: {
+				de: "Digitales Medium",
+				en: "Digital Media",
+				fr: "Support numérique",
+				it: "Supporto digitale",
+				nl: "digitale media",
+			}
 		}
-	}
-	// TODO: recompute odd/even rows
+	};
+	localStorage.setItem("MBGlossary", JSON.stringify(MBGlossary));
+}
+var lang = document.getElementsByTagName("html")[0].getAttribute("lang");
+switch (location.pathname.match(/\/[^/]+\//)[0]) {
+	case "/release-group/":
+		var releases = document.querySelectorAll("tbody > tr:not(.subh)");
+		if (releases.length > 0) {
+			for (var r = 0; r < releases.length; r++) {
+				if (
+					// don’t match half physical releases
+					!releases[r].cells[2].textContent.match(/\+/)
+					// match fully digital releases
+					&& releases[r].cells[2].textContent.match(new RegExp("([0-9]+×)?" + MBGlossary["disc-format"][12][lang], "iu"))
+				) {
+					releases[r].style.setProperty("display", "none");
+				}
+			}
+			// TODO: recompute odd/even rows
+		}
+		break;
+	case "/release/": // release editor (see @include)
+		// ARTIFICIAL INTELLIGENCE: Learn new localised digital media names
+		var digitalMediaFormat = document.querySelector("#release-editor select[id^='disc-format'] option[value='12']");
+		if (digitalMediaFormat) {
+			digitalMediaFormat = digitalMediaFormat.textContent;
+			if (MBGlossary["disc-format"][12][lang] !== digitalMediaFormat) {
+				MBGlossary["disc-format"][12][lang] = digitalMediaFormat;
+				localStorage.setItem("MBGlossary", JSON.stringify(MBGlossary));
+			}
+		}
+		break;
 }
