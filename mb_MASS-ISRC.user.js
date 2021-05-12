3b896ec7d772dac0230dbdc7b689cb0eb83aad22
jesus2099 <jesus2099@users.noreply.github.com>
2021-05-13 01:07:33 +0200

    Add @kepstin MagicISRC link to release pages

--- mb_MASS-ISRC.user.js
+++ mb_MASS-ISRC.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. MASS ISRC
-// @version      2021.1.19.2099
+// @version      2021.5.13
 // @description  kepstin’s magicisrc. Paste a bunch of ISRC instead of one by one
 // @compatible   vivaldi(2.9.1705.41)+violentmonkey  my setup (office)
 // @compatible   vivaldi(1.0.435.46)+violentmonkey   my setup (home, xp)
@@ -14,39 +14,52 @@
 // @icon         data:image/gif;base64,R0lGODlhEAAQAKEDAP+/3/9/vwAAAP///yH/C05FVFNDQVBFMi4wAwEAAAAh/glqZXN1czIwOTkAIfkEAQACAwAsAAAAABAAEAAAAkCcL5nHlgFiWE3AiMFkNnvBed42CCJgmlsnplhyonIEZ8ElQY8U66X+oZF2ogkIYcFpKI6b4uls3pyKqfGJzRYAACH5BAEIAAMALAgABQAFAAMAAAIFhI8ioAUAIfkEAQgAAwAsCAAGAAUAAgAAAgSEDHgFADs=
 // @grant        none
 // @match        *://magicisrc.kepstin.ca/*
+// @include      /^https?:\/\/(\w+\.)?musicbrainz\.org\/release\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/
 // @run-at       document-end
 // ==/UserScript==
 "use strict";
-document.addEventListener("input", function(event) {
-	if (event && event.target && event.target.classList.contains("form-control") && event.target.getAttribute("id").match(/^isrc\d+-\d+$/)) {
-		var isrcList = event.target.value.toUpperCase().match(/[A-Z]{2}-?[A-Z0-9]{3}-?[0-9]{2}-?[0-9]{5}/g);
-		if (isrcList && !arrHasDupes(isrcList) || confirm("Achtung, there are duplicates!")) {
-			var isrcInputs = event.currentTarget.querySelectorAll("table > tbody > tr > td input.form-control[id^='isrc']");
-			var lastUpdatedInput;
-			for (var isrc = 0, input = 0, startingInputIndex = null; input < isrcInputs.length; input += 1) {
-				isrcInputs[input].style.removeProperty("background-color");
-				if (isrc < isrcList.length) {
-					if (isrcInputs[input] === event.target) {
-						startingInputIndex = input;
-					}
-					if (startingInputIndex !== null) {
-						isrcInputs[input].value = isrcList[isrc];
-						sendEvent(isrcInputs[input], "change");
-						if (isrcInputs[input].getAttribute("oldValue") !== isrcInputs[input].value) {
-							isrcInputs[input].style.setProperty("background-color", "#ff6");
-							isrcInputs[input].setAttribute("oldValue", isrcInputs[input].value);
-							lastUpdatedInput = isrcInputs[input];
+if (location.host === "magicisrc.kepstin.ca") {
+	document.addEventListener("input", function(event) {
+		if (event && event.target && event.target.classList.contains("form-control") && event.target.getAttribute("id").match(/^isrc\d+-\d+$/)) {
+			var isrcList = event.target.value.toUpperCase().match(/[A-Z]{2}-?[A-Z0-9]{3}-?[0-9]{2}-?[0-9]{5}/g);
+			if (isrcList && !arrHasDupes(isrcList) || confirm("Achtung, there are duplicates!")) {
+				var isrcInputs = event.currentTarget.querySelectorAll("table > tbody > tr > td input.form-control[id^='isrc']");
+				var lastUpdatedInput;
+				for (var isrc = 0, input = 0, startingInputIndex = null; input < isrcInputs.length; input += 1) {
+					isrcInputs[input].style.removeProperty("background-color");
+					if (isrc < isrcList.length) {
+						if (isrcInputs[input] === event.target) {
+							startingInputIndex = input;
 						}
-						isrc += 1;
+						if (startingInputIndex !== null) {
+							isrcInputs[input].value = isrcList[isrc];
+							sendEvent(isrcInputs[input], "change");
+							if (isrcInputs[input].getAttribute("oldValue") !== isrcInputs[input].value) {
+								isrcInputs[input].style.setProperty("background-color", "#ff6");
+								isrcInputs[input].setAttribute("oldValue", isrcInputs[input].value);
+								lastUpdatedInput = isrcInputs[input];
+							}
+							isrc += 1;
+						}
 					}
 				}
-			}
-			if (lastUpdatedInput) {
-				lastUpdatedInput.focus();
+				if (lastUpdatedInput) {
+					lastUpdatedInput.focus();
+				}
 			}
 		}
+	});
+} else if (location.host.match(/musicbrainz\.org$/)) {
+	var lastEditing = document.querySelector("div#sidebar > h2.editing + ul.links > li.separator");
+	if (lastEditing) {
+		var magicISRCLink = document.createElement("a");
+		magicISRCLink.appendChild(document.createTextNode("MagicISRC"));
+		magicISRCLink.style.setProperty("background", "#ff6");
+		magicISRCLink.setAttribute("target", "_blank");
+		magicISRCLink.setAttribute("href", "//magicisrc.kepstin.ca/?mbid=" + location.pathname.match(/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/));
+		lastEditing.parentNode.insertBefore(document.createElement("li"), lastEditing).appendChild(magicISRCLink);
 	}
-});
+}
 /* http://www.groggyjava.tv/freebies/duplicates.html */
 function arrHasDupes(arr) {
 	var i, j, n;
