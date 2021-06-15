192b095b12776e2eec012255f9db7db636e100fd
jesus2099 <jesus2099@users.noreply.github.com>
2021-06-15 11:00:00 +0200

    Language independent

--- greasyfork_APPLIES-TO-LINKS-SEARCH-BY-SITES.user.js
+++ greasyfork_APPLIES-TO-LINKS-SEARCH-BY-SITES.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         greasyfork. APPLIES TO LINKS SEARCH BY SITES
-// @version      2016.6.15.2099
+// @version      2021.6.15
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/greasyfork_APPLIES-TO-LINKS-SEARCH-BY-SITES.user.js
 // @description  greasyfork.org. makes applies to “All site” link to the “*” by site search (JasonBarnabe/greasyfork#146)
 // @supportURL   https://github.com/jesus2099/konami-command/labels/greasyfork_APPLIES-TO-LINKS-SEARCH-BY-SITES
@@ -14,23 +14,14 @@
 // @since        2014-06-06
 // @icon         data:image/gif;base64,R0lGODlhEAAQAKEDAP+/3/9/vwAAAP///yH/C05FVFNDQVBFMi4wAwEAAAAh/glqZXN1czIwOTkAIfkEAQACAwAsAAAAABAAEAAAAkCcL5nHlgFiWE3AiMFkNnvBed42CCJgmlsnplhyonIEZ8ElQY8U66X+oZF2ogkIYcFpKI6b4uls3pyKqfGJzRYAACH5BAEIAAMALAgABQAFAAMAAAIFhI8ioAUAIfkEAQgAAwAsCAAGAAUAAgAAAgSEDHgFADs=
 // @grant        none
-// @match        *://greasyfork.org/*/scripts/*
-// @match        *://greasyfork.org/scripts/*
-// @exclude      *://greasyfork.org/*/scripts/*/*
-// @exclude      *://greasyfork.org/*/scripts/?
-// @exclude      *://greasyfork.org/*/scripts/by*
-// @exclude      *://greasyfork.org/scripts/*/*
-// @exclude      *://greasyfork.org/scripts/?
-// @exclude      *://greasyfork.org/scripts/by*
+// @include      /https?:\/\/greasyfork\.org\/([a-z]{2}(-[A-Z]{2})?/)?scripts\/\d+/
 // @run-at       document-end
 // ==/UserScript==
 "use strict";
 var appliesto = document.querySelector("div#script-meta dd.script-show-applies-to");
-if (appliesto) {
+if (appliesto && !appliesto.querySelector("ul.inline-list")) {
 	var text = appliesto.textContent.trim();
-	if (text.match(/^(all sites|alle seiten|todos los sitios|tous les sites|semua situs|すべてのサイト|alle websites|wszystkie strony|todos sites|все сайты|所有站点|所有網站)$/i)) {
-		appliesto.replaceChild(appliestoLink(text, "*"), appliesto.firstChild);
-	}
+	appliesto.replaceChild(appliestoLink(text, "*"), appliesto.firstChild);
 }
 function appliestoLink(text, site) {
 	var a = document.createElement("a");
