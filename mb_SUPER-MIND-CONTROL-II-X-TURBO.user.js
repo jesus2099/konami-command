56c2318b6508149e34043a2f4764630d29365b92
jesus2099 <jesus2099@users.noreply.github.com>
2021-07-30 20:39:09 +0200

    USER_STATS: Fix editor name display bugs
    Vote search URL display bug
    (from #300 14f5a4e5556a51d38bdca1522bd69489e05abfca)
    https://musicbrainz.org/user/111111
    
    New edit note search link label display bug
    https://musicbrainz.org/user/Mario%20M%C3%BCller

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2021.7.30
+// @version      2021.5.5
 // @description  musicbrainz.org power-ups: RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @namespace    https://github.com/jesus2099/konami-command
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -406,7 +406,7 @@ if (j2sets.USER_STATS && self.location.pathname.match(/^\/user\/[^/]+$/)) {
 		// Opera 12 alone is surprisingly decoding location.pathname
 		editorPathname = encodeURIComponent(editorPathname);
 	}
-	editorPathname = self.location.pathname.substr(0, editorPathname) + editorPathname;
+	var editorName = decodeURIComponent(editorPathname);
 	if (stats.length == 3) {
 		debug("USER_STATS");
 		// Edits
@@ -451,8 +451,8 @@ if (j2sets.USER_STATS && self.location.pathname.match(/^\/user\/[^/]+$/)) {
 		var myEditNotes = "/search/edits?conditions.0.field=editor&conditions.0.operator=%3D&conditions.0.name=%editorName%&conditions.0.args.0=%editorID%&conditions.1.field=edit_note_author&conditions.1.operator=me";
 		var theirEditNotes = "/search/edits?conditions.0.field=editor&conditions.0.operator=me&conditions.1.field=edit_note_author&conditions.1.operator=%3D&conditions.1.name=%editorName%&conditions.1.args.0=%editorID%";
 		stats[0].parentNode.parentNode.insertBefore(createTag("ul", {}, [
-			createTag("li", {}, createTag("a", {a: {href: myEditNotes.replace(/%editorID%/, editorID).replace(/%editorName%/, editorPathname)}}, "My edit notes on " + editorPathname + " edits")),
-			createTag("li", {}, createTag("a", {a: {href: theirEditNotes.replace(/%editorID%/, editorID).replace(/%editorName%/, editorPathname)}}, editorPathname + " edit notes on my edits")),
+			createTag("li", {}, createTag("a", {a: {href: myEditNotes.replace(/%editorID%/, editorID).replace(/%editorName%/, editorPathname)}}, "My edit notes on " + editorName + " edits")),
+			createTag("li", {}, createTag("a", {a: {href: theirEditNotes.replace(/%editorID%/, editorID).replace(/%editorName%/, editorPathname)}}, editorName + " edit notes on my edits")),
 		]), stats[0].parentNode);
 		// Added entities
 		var addedEntities = 0;
