jesus2099/konami-command

commit 4d4fcd5aa8d50d0d6ac2c91e381f8e73f9b26e7d

Fix local and remote releases parsing (#597)
Only tested on releases with few tracks and mediums

mb_MASS-MERGE-RECORDINGS.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. MASS MERGE RECORDINGS
-// @version      2021.3.8
+// @version      2021.7.2
 // @description  musicbrainz.org: Merges selected or all recordings from release A to release B
 // @compatible   vivaldi(2.4.1488.38)+violentmonkey  my setup (office)
 // @compatible   vivaldi(1.0.435.46)+violentmonkey   my setup (home, xp)
@@ -406,7 +406,7 @@ function massMergeGUI() {
 			localRelease.tracks = [];
 			recid2trackIndex.local = {};
 			removeChildren(startpos);
-			var trs = document.querySelectorAll("div#content > table.tbl > tbody > tr");
+			var trs = document.querySelectorAll("div#content table.tbl.medium > tbody > tr");
 			/* var jsonRelease, scripts = document.querySelectorAll("script:not([src])");
 			for (let s = 0; s < scripts.length && !jsonRelease; s++) {
 				jsonRelease = scripts[s].textContent.match(/MB\.Release\.init\(([^<]+)\)/);
@@ -641,9 +641,9 @@ function loadReleasePage() {
 					var trackLength = trackTimes[t].match(/(\d+:)?\d+:\d+/);
 					if (trackLength) trackLength = strtime2ms(trackLength[0]);
 					remoteRelease.tracks.push({
-						number: trackRows[t].match(new RegExp("<td class=\"pos[\\s\\S]+?<a href=\"" + MBS + "/track/" + sregex_MBID + "\">(.*?)</a>"))[1],
+						number: trackRows[t].match(new RegExp("<td class=\"pos[\\s\\S]+?<a href=\"" + "/track/" + sregex_MBID + "\">(.*?)</a>"))[1],
 						name: HTMLToText(trackInfos[t].match(/<bdi>([^<]*)<\/bdi>/)[1]),
-						artistCredit: trackRows[t].match(/<td>/g).length > 1 ? trackRows[t].match(/[\s\S]*<td>([\s\S]+?)<\/td>/)[1].trim().replace(/<a/g, '<a target="_blank"') : releaseAC[1],
+						artistCredit: trackRows[t].match(/<td>/g) && trackRows[t].match(/<td>/g).length > 1 ? trackRows[t].match(/[\s\S]*<td>([\s\S]+?)<\/td>/)[1].trim().replace(/<a/g, '<a target="_blank"') : releaseAC[1],
 						length: trackLength,
 						recording: {
 							rowid: recIDs[t],

commit be480539a661b394a159046b7357efe531f3a194

Remove track title from remote release track AC
Fix big regression by #597 9addef46ac8022ab66b8762c35de8258cb45f4ef

mb_MASS-MERGE-RECORDINGS.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. MASS MERGE RECORDINGS
-// @version      2021.7.29
+// @version      2021.7.30
 // @description  musicbrainz.org: Merges selected or all recordings from release A to release B
 // @compatible   vivaldi(2.4.1488.38)+violentmonkey  my setup (office)
 // @compatible   vivaldi(1.0.435.46)+violentmonkey   my setup (home, xp)
@@ -651,7 +651,7 @@ function loadReleasePage() {
 					remoteRelease.tracks.push({
 						number: trackRows[t].match(new RegExp("<td class=\"pos[\\s\\S]+?<a href=\"" + "/track/" + sregex_MBID + "\">(.*?)</a>"))[1],
 						name: HTMLToText(trackInfos[t].match(/<bdi>([^<]*)<\/bdi>/)[1]),
-						artistCredit: trackRows[t].match(/<td>/g) && trackRows[t].match(/<td>/g).length === 1 ? trackRows[t].match(/[\s\S]*<td>([\s\S]+?)<\/td>/)[0].trim().replace(/<a/g, '<a target="_blank"') : releaseAC[1],
+						artistCredit: trackRows[t].match(/<td>/g) && trackRows[t].match(/<td>/g).length === 1 ? trackRows[t].match(/[\s\S]*<td>([\s\S]+?)<\/td>/)[1].trim().replace(/<a/g, '<a target="_blank"') : releaseAC[1],
 						length: trackLength,
 						recording: {
 							rowid: recIDs[t],