24370b84f13e25eb9a76acc525466bcee393de3b
jesus2099 <jesus2099@users.noreply.github.com>
2021-12-15 23:44:31 +0100

    Show script badge and help tooltip Show some hints that script is running

--- odigo-ivr-designer_TURBO-BOOST.user.js
+++ odigo-ivr-designer_TURBO-BOOST.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         odigo ivr designer. TURBO BOOST
-// @version      2021.12.14
-// @description  Various quality of life enhancements: Go to List View by default; Show only tables List View by default; Auto stretch narrow tables and modal dialogs; Double-click to View Tree; Click to select row; Press Escape to close modal
+// @version      2021.12.15
+// @description  APPLICATION LIST: Click to select row, Double-click to open application; APPLICATION: Open List View tables by default, Auto stretch narrow tables and modals, Press Escape to close modal
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/odigo-ivr-designer_TURBO-BOOST
 // @downloadURL  https://github.com/jesus2099/konami-command/raw/master/odigo-ivr-designer_TURBO-BOOST.user.js
@@ -9,7 +9,7 @@
 // @licence      CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/
 // @licence      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @since        2021-04-23
-// @grant        none
+// @grant        GM_info
 // @include      /^https?:\/\/ivr-designer\d?\.prosodie\.com\/application\.html/
 // @include      /^https?:\/\/ivr-designer\d?\.prosodie\.com\/appNservices\.html/
 // @run-at       document-ready
@@ -20,9 +20,22 @@ var css = document.createElement("style");
 css.setAttribute("type", "text/css");
 document.head.appendChild(css);
 css = css.sheet;
+var myColour = "#FCF";
+
+// Show script badge and help tooltip
+css.insertRule("span.badge." + GM_info.script.author + " { background-color: " + myColour + "; color: black; cursor: help; position: fixed; top: 3px; right: 3px; box-shadow: inset 1px 1px 3px purple; z-index: 1035; }", 0);
+var doc = document.createElement("span");
+doc.classList.add("badge", GM_info.script.author);
+doc.appendChild(document.createTextNode(GM_info.script.name + " v" + GM_info.script.version));
+doc.setAttribute("title", GM_info.script.description.replace(/[,:]/g, "\n-").replace(/; /g, "\n\n"));
+document.body.appendChild(doc);
 
 switch (self.location.pathname) {
 	case "/appNservices.html":
+
+		// Custom highlight colour
+		css.insertRule(".table-hover > tbody > tr:hover > td, .table-hover > tbody > tr:hover > td, .table > tbody > tr > td.rowselect { background-color: " + myColour + " !important; }", 0);
+		css.insertRule(".table > tbody > tr > td.rowselect * { color: black; }", 0);
 
 		// Click to select row checkbox
 		var container = document.querySelector("div#main-container");
@@ -49,6 +62,7 @@ switch (self.location.pathname) {
 		var applicationTable = document.querySelector("div#main-container table#applications > tbody");
 		var viewTree1 = document.querySelector("div#main-container a#viewOpenTree1");
 		if (applicationTable && viewTree1) {
+			viewTree1.style.setProperty("background-color", myColour);
 			applicationTable.addEventListener("dblclick", function(event) {
 				viewTree1.click();
 			});
@@ -62,7 +76,7 @@ switch (self.location.pathname) {
 		css.insertRule("div#main-container div#modBuilder-form-table-table, div#main-container div#modBuilder-form-table-table div.wtHolder { height: fit-content !important; }", 0);
 
 		// highlight most important item in filter selection
-		css.insertRule("div#main-content div#actions-bar select#arbo-type option[value='table'], div#main-content div#actions-bar select#arbo-type option[value='sound_set'] { background: #FCF; }", 0);
+		css.insertRule("div#main-content div#actions-bar select#arbo-type option[value='table'], div#main-content div#actions-bar select#arbo-type option[value='sound_set'] { background: " + myColour + "; }", 0);
 
 		// Keyboard shortcut handler
 		document.body.addEventListener("keydown", function(event) {
