40c4ef3565548d829d1db1ea77e398a95ddefb7a
jesus2099 <jesus2099@users.noreply.github.com>
2022-01-24 10:41:05 +0100

    Select music release rights, like CD, etc. #499
    Maybe should be in jasrac_DIRECT instead…?

--- jasrac-mb-minc_WORK-IMPORT-CROSS-LINKING.user.js
+++ jasrac-mb-minc_WORK-IMPORT-CROSS-LINKING.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         JASRAC. work importer/editor into MusicBrainz + MB-JASRAC-音楽の森-NexTone links + MB back search links
-// @version      2021.11.14
+// @version      2022.1.24
 // @description  One click imports JASRAC works into MusicBrainz (name, iswc, type, credits, edit note, sort name, search hint) and マス歌詞®（mass-lyrics） and wikipedia links. It will do the same magic in work editor. Work links to both JASRAC and 音楽の森 / ongakunomori / music forest / minc / magic db and back to MB
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/jasrac-mb-minc_WORK-IMPORT-CROSS-LINKING
@@ -413,6 +413,8 @@ if (pagecat && !document.title.match(/slow down!/i)) {
 							addAfter(createTag("sup", {}, createA("MB", workLookupURL("mb", "iswc", iswc.value), "Search this ISWC in MusicBrainz")), iswc.node);
 							addAfter(document.createTextNode(" "), iswc.node);
 						}
+				/* -- vv ------ Select music release rights, like CD, etc. ------ vv -- */
+						document.querySelector("a[href='#tab-00-01']").click();
 					}
 				}
 			}, 4000); // quick and dirty patch
