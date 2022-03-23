c0c7f91e4458d628ed349a0f5db21aba137cd91e
jesus2099 <jesus2099@users.noreply.github.com>
2022-03-23 03:11:31 +0100

    Pencil and eye icons open in new tab on middle-click Ctrl+click and Shift+click

--- odigo-routing_TURBO-BOOST.user.js
+++ odigo-routing_TURBO-BOOST.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         odigo routing. TURBO BOOST
-// @version      2022.3.22
-// @description  ALL LISTS: Click cell to select text, Ctrl+click row to view in new tab, Ctrl+Alt+click row to edit in new tab
+// @version      2022.3.23
+// @description  ALL LISTS: Simple-click cell to select text, Ctrl+click row to view in background tab, Ctrl+Alt+click row to edit in background tab, Pencil and eye icons open in new tab on middle-click Ctrl+click and Shift+click
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/odigo-routing_TURBO-BOOST
 // @downloadURL  https://github.com/jesus2099/konami-command/raw/master/odigo-routing_TURBO-BOOST.user.js
@@ -34,121 +34,147 @@ supportLink.setAttribute("target", "_blank");
 doc.setAttribute("data-title", GM_info.script.description.replace(/:/g, "\n\n‣").replace(/,/g, "\n‣").replace(/; /g, "\n\n"));
 document.body.appendChild(doc);
 
+// Pencil and eye icons open in new (unfortunately not background) tab on middle-click, Ctrl+click and Shift+click
+document.body.addEventListener("mousedown", function(event) {
+	if (event.target.tagName == "IMG" && (event.target.classList.contains("iconModify") || event.target.classList.contains("iconView"))) {
+		if (event.button === 1 || event.ctrlKey || event.shiftKey) {
+			event.preventDefault();
+			openInTab(parentRow(event.target), event.target.classList.contains("iconView") ? "view" : "edit");
+		}
+	}
+});
 
+// Click row for actions
 css.insertRule("tbody div[unselectable='on'] { cursor: pointer; }", 0);
 document.body.addEventListener("click", function(event) {
 	// Is it a viewable/editable Odigo object
 	if (event.target.tagName == "DIV" && event.target.getAttribute("unselectable") == "on") {
-		if (!event.ctrlKey) {
+		if (event.ctrlKey) {
+			// Ctrl+click row to view in background tab
+			// Ctrl+Alt+click row to edit in background tab
+			openInTab(parentRow(event.target), event.altKey ? "edit" : "view");
+		} else {
 			// Simple-click cell to select text
 			self.getSelection().selectAllChildren(event.target);
-		} else {
-			// Ctrl+click row to view in new tab
-			// Ctrl+Alt+click row to edit in new tab
-			var openObjectURL = {
-				agentGroupSearch: {
-					view: "agentGroupEdit?action=VIEW&idString=$key",
-					edit: "agentGroupEdit?action=UPDATE&idString=$key",
-				},
-				agentGroupTreeSearch: {
-					view: "agentGroupOrganizationEdit?id=$id&label=$key&isCreation=0",
-					edit: "agentGroupOrganizationEdit?id=$id&label=$key&isCreation=0",
-				},
-				calendarSearch: {
-					view: "calendarEdit?action=VIEW&CalID=$key",
-					edit: "calendarEdit?action=EDIT&CalID=$key",
-				},
-				/* callbackIdSearch: { // cannot find callerId in page
-					view: "callbackIdEdit?action=VIEW&callerId=61",
-					edit: "callbackIdEdit?action=EDIT&callerId=61",
-				}, */
-				/* caseSearch: { // unknown pattern, no examples
-				}, */
-				channelSearch: {
-					view: "channelEdit?action=VIEW&id=$id",
-					edit: "channelEdit?action=EDIT&id=$id",
-				},
-				/* chatGroupSearch: { // cannot find idString in page
-					view: "chatGroupEdit?action=VIEW&idString=10",
-					edit: "chatGroupEdit?action=UPDATE&idString=10",
-				}, */
-				/* codificationSearch: { // cannot find codificationId in page
-					view: "codificationEdit?action=VIEW&codificationId=3",
-					edit: "codificationEdit?action=EDIT&codificationId=3",
-				}, */
-				/* commonQueueSearch: { // unknown pattern, no examples
-				}, */
-				ddiSearch: {
-					view: "ddiEdit?action=VIEW&keyWord=$base64key",
-					edit: "ddiEdit?action=EDIT&keyWord=$base64key",
-				},
-				gateSearch: {
-					view: "gateEdit?action=VIEW&keyWord=$key",
-					edit: "gateEdit?action=EDIT&keyWord=$key",
-					keyCellIndex: 2
-				},
-				gateSkillDispatchSearch: {
-					view: "gateSkillDispatchEdit?action=VIEW&dispatchId=$id",
-					edit: "gateSkillDispatchEdit?action=EDIT&dispatchId=$id",
-				},
-				gateTreeSearch: {
-					view: "gateTreeEdit?id=$id&label=$key&isCreation=0",
-					edit: "gateTreeEdit?id=$id&label=$key&isCreation=0",
-				},
-				/* miniDirectorySearch: { // cannot find annuaireId in page
-					view: "miniDirectoryEdit?action=VIEW&annuaireId=8",
-					edit: "miniDirectoryEdit?action=EDIT&annuaireId=3",
-				}, */
-				reasonForCallSearch: {
-					view: "reasonForCallListEdit?action=VIEW&rootId=$id",
-					edit: "reasonForCallListEdit?action=EDIT&rootId=$id",
-				},
-				/* smsTemplateSearch: { // unknown pattern, no examples
-				}, */
-				otherActivitySearch: {
-					view: "otherActivityEdit?activityId=$key",
-					edit: "otherActivityEdit?activityId=$key",
-				},
-				skillSearch: {
-					view: "skillDisplay?action=VIEW&keyWord=$key",
-					edit: "skillEdit?action=EDIT&keyWord=$key",
-					keyCellIndex: 2
-				},
-				userSearch: {
-					view: "userEdit?action=VIEW&isTemplate=false&idUtilisateurAEditer=$key",
-					edit: "userEdit?action=EDIT&isTemplate=false&userFromLdap=false&idUtilisateurAEditer=$key",
-					keyCellIndex: 2
-				},
-				userTemplateSearch: {
-					view: "userEdit?action=VIEW&isTemplate=true&idUtilisateurAEditer=$key",
-					edit: "userEdit?action=EDIT&isTemplate=true&idUtilisateurAEditer=$key",
-					keyCellIndex: 3
-				},
-			};
-			var listType = location.pathname.match(/[^/]+$/)[0];
-			if (openObjectURL[listType]) {
-				var url = openObjectURL[listType][event.altKey ? "edit" : "view"];
-				if (url.match(/\$id/)) {
-					url = url.replace("$id", event.target.parentNode.parentNode.getAttribute("data-recordid"));
-				}
-				if (url.match(/\$(base64)?key/)) {
-					var key = event.target.parentNode.parentNode.querySelector("td:nth-child(" + (openObjectURL[listType].keyCellIndex || 1) + ") > div[unselectable='on']").textContent;
-					url = url.replace("$base64key", btoa(event.target.parentNode.parentNode.querySelector("td:nth-child(" + (openObjectURL[listType].keyCellIndex || 1) + ") > div[unselectable='on']").textContent));
-					url = url.replace("$key", encodeURIComponent(event.target.parentNode.parentNode.querySelector("td:nth-child(" + (openObjectURL[listType].keyCellIndex || 1) + ") > div[unselectable='on']").textContent));
-				}
-				open(url);
-			} else {
-				// fallback to native buttons, view or edit in current tab
-				var actionIcons = {
-					view: event.target.parentNode.parentNode.querySelector("img.iconView"),
-					edit: event.target.parentNode.parentNode.querySelector("img.iconModify"),
-				};
-				if (event.altKey && actionIcons.edit) {
-					actionIcons.edit.click();
-				} else if (actionIcons.view) {
-					actionIcons.view.click();
-				}
-			}
 		}
 	}
 });
+
+function openInTab(row, action) {
+	var openObjectURL = {
+		agentGroupSearch: {
+			view: "agentGroupEdit?action=VIEW&idString=$key",
+			edit: "agentGroupEdit?action=UPDATE&idString=$key",
+		},
+		agentGroupTreeSearch: {
+			view: "agentGroupOrganizationEdit?id=$id&label=$key&isCreation=0",
+			edit: "agentGroupOrganizationEdit?id=$id&label=$key&isCreation=0",
+		},
+		calendarSearch: {
+			view: "calendarEdit?action=VIEW&CalID=$key",
+			edit: "calendarEdit?action=EDIT&CalID=$key",
+		},
+		/* callbackIdSearch: { // cannot find callerId in page
+			view: "callbackIdEdit?action=VIEW&callerId=61",
+			edit: "callbackIdEdit?action=EDIT&callerId=61",
+		}, */
+		/* caseSearch: { // unknown pattern, no examples
+		}, */
+		channelSearch: {
+			view: "channelEdit?action=VIEW&id=$id",
+			edit: "channelEdit?action=EDIT&id=$id",
+		},
+		/* chatGroupSearch: { // cannot find idString in page
+			view: "chatGroupEdit?action=VIEW&idString=10",
+			edit: "chatGroupEdit?action=UPDATE&idString=10",
+		}, */
+		/* codificationSearch: { // cannot find codificationId in page
+			view: "codificationEdit?action=VIEW&codificationId=3",
+			edit: "codificationEdit?action=EDIT&codificationId=3",
+		}, */
+		/* commonQueueSearch: { // unknown pattern, no examples
+		}, */
+		ddiSearch: {
+			view: "ddiEdit?action=VIEW&keyWord=$base64key",
+			edit: "ddiEdit?action=EDIT&keyWord=$base64key",
+		},
+		gateSearch: {
+			view: "gateEdit?action=VIEW&keyWord=$key",
+			edit: "gateEdit?action=EDIT&keyWord=$key",
+			keyCellIndex: 2
+		},
+		gateSkillDispatchSearch: {
+			view: "gateSkillDispatchEdit?action=VIEW&dispatchId=$id",
+			edit: "gateSkillDispatchEdit?action=EDIT&dispatchId=$id",
+		},
+		gateTreeSearch: {
+			view: "gateTreeEdit?id=$id&label=$key&isCreation=0",
+			edit: "gateTreeEdit?id=$id&label=$key&isCreation=0",
+		},
+		/* miniDirectorySearch: { // cannot find annuaireId in page
+			view: "miniDirectoryEdit?action=VIEW&annuaireId=8",
+			edit: "miniDirectoryEdit?action=EDIT&annuaireId=3",
+		}, */
+		reasonForCallSearch: {
+			view: "reasonForCallListEdit?action=VIEW&rootId=$id",
+			edit: "reasonForCallListEdit?action=EDIT&rootId=$id",
+		},
+		/* smsTemplateSearch: { // unknown pattern, no examples
+		}, */
+		otherActivitySearch: {
+			view: "otherActivityEdit?activityId=$key",
+			edit: "otherActivityEdit?activityId=$key",
+		},
+		skillSearch: {
+			view: "skillDisplay?action=VIEW&keyWord=$key",
+			edit: "skillEdit?action=EDIT&keyWord=$key",
+			keyCellIndex: 2
+		},
+		userSearch: {
+			view: "userEdit?action=VIEW&isTemplate=false&idUtilisateurAEditer=$key",
+			edit: "userEdit?action=EDIT&isTemplate=false&userFromLdap=false&idUtilisateurAEditer=$key",
+			keyCellIndex: 2
+		},
+		userTemplateSearch: {
+			view: "userEdit?action=VIEW&isTemplate=true&idUtilisateurAEditer=$key",
+			edit: "userEdit?action=EDIT&isTemplate=true&idUtilisateurAEditer=$key",
+			keyCellIndex: 3
+		},
+	};
+	var listType = location.pathname.match(/[^/]+$/)[0];
+	if (openObjectURL[listType]) {
+		var url = openObjectURL[listType][action];
+		if (url.match(/\$id/)) {
+			url = url.replace("$id", row.getAttribute("data-recordid"));
+		}
+		if (url.match(/\$(base64)?key/)) {
+			var key = row.querySelector("td:nth-child(" + (openObjectURL[listType].keyCellIndex || 1) + ") > div[unselectable='on']").textContent;
+			url = url.replace("$base64key", btoa(row.querySelector("td:nth-child(" + (openObjectURL[listType].keyCellIndex || 1) + ") > div[unselectable='on']").textContent));
+			url = url.replace("$key", encodeURIComponent(row.querySelector("td:nth-child(" + (openObjectURL[listType].keyCellIndex || 1) + ") > div[unselectable='on']").textContent));
+		}
+		// Unfortunately this blur/focus trick to open background tab does not work in Vivaldi
+		open(url).blur();
+		top.focus();
+	} else {
+		// fallback to native buttons, view or edit in current tab
+		var actionIcons = {
+			view: row.querySelector("img.iconView"),
+			edit: row.querySelector("img.iconModify"),
+		};
+		if (event.altKey && actionIcons.edit) {
+			actionIcons.edit.click();
+		} else if (actionIcons.view) {
+			actionIcons.view.click();
+		}
+	}
+}
+
+function parentRow(node) {
+	if (node.tagName && node.tagName === "TR") {
+		return node;
+	} else if (node.tagName && (node.tagName === "BODY" || node.tagName === "HTML")) {
+		return null;
+	} else {
+		return parentRow(node.parentNode);
+	}
+}
