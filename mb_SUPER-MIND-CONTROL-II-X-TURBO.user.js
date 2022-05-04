fdbad4ab16ee640655f60e1690ddcf2b199a47d9
jesus2099 <jesus2099@users.noreply.github.com>
2022-05-05 00:45:41 +0200

    Forgot "Add medium" edits in Pure release edits #664

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2022.5.3.1826
+// @version      2022.5.5
 // @description  musicbrainz.org power-ups: RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @namespace    https://github.com/jesus2099/konami-command
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -985,6 +985,7 @@ function entitySpecificEdits(entityType, searchType) {
 			/* Add cover art                                  */ refineSuffix += appendArg("314");
 			/* Add disc ID                                    */ refineSuffix += appendArg("55,232");
 			/* Add ISRCs                                      */ refineSuffix += appendArg("76");
+			/* Add medium                                     */ refineSuffix += appendArg("51");
 			/* Add release                                    */ refineSuffix += appendArg("31,216");
 			/* Add release alias                              */ refineSuffix += appendArg("318");
 			/* Add release annotation                         */ refineSuffix += appendArg("35,231");
