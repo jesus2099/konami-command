b9ee92a2b40408a743a881189a9f66d7592bc39a
jesus2099 <jesus2099@users.noreply.github.com>
2022-05-08 23:17:33 +0200

    right to be forgotten

--- mb_REVIVE-DELETED-EDITORS.user.js
+++ mb_REVIVE-DELETED-EDITORS.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. REVIVE DELETED EDITORS
-// @version      2022.3.17
+// @version      2022.5.8
 // @changelog    https://github.com/jesus2099/konami-command/commits/master/mb_REVIVE-DELETED-EDITORS.user.js
 // @description  musicbrainz.org: reveal deleted editors’ names and emphasizes your own name to standout in MB pages
 // @supportURL   https://github.com/jesus2099/konami-command/labels/mb_REVIVE-DELETED-EDITORS
