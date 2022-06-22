3e6b0ff9e8c75034b8bd5de0880cb60c30105895
jesus2099 <jesus2099@users.noreply.github.com>
2022-06-22 16:12:57 +0200

    Stop removing all even row class names #688

--- mb_REDIRECT-WHEN-UNIQUE-RESULT.user.js
+++ mb_REDIRECT-WHEN-UNIQUE-RESULT.user.js
@@ -1,31 +1,42 @@
 // ==UserScript==
 // @name         mb. REDIRECT WHEN UNIQUE RESULT
-// @version      2019.8.8.2099
+// @version      2022.6.22
 // @description  Redirect to entity (release, artist, etc.) when only 1 result and/or unique 100% scored result of your entity search
 // @namespace    https://github.com/jesus2099/konami-command
+// @supportURL   https://github.com/jesus2099/konami-command/labels/mb_REDIRECT-WHEN-UNIQUE-RESULT
+// @downloadURL  https://github.com/jesus2099/konami-command/raw/master/mb_REDIRECT-WHEN-UNIQUE-RESULT.user.js
 // @author       jesus2099
 // @licence      CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/
 // @licence      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @since        2011-06-30; fork of nikki/stars script http://web.archive.org/web/20150915074449/chatlogs.musicbrainz.org/musicbrainz/2011/2011-06/2011-06-30.html#T15-59-01-950029 / https://web.archive.org/web/20131103163409/userscripts.org/scripts/show/106156 / https://web.archive.org/web/20141011084017/userscripts-mirror.org/scripts/show/106156
-// @grant        none
+// @icon         data:image/gif;base64,R0lGODlhEAAQAOMMAAAAAP8A/wJR1MosEqGhBPyZUAD/APW1hQD///vPp///AP7++P///////////////yH5BAEKAA8ALAAAAAAQABAAAARbUMlJq0Ll6AN6z0liYNnWLV84FmUBLIsAAyqpuTEgA4VomzFUyMfaaDy9WhFw/PRoK6Zn2lFyqNio58DKSAEjQnczPtTEN+ww3AIMBrM1Qpxxud80VWDP7/sNEQA7
+// @grant        GM_info
 // @include      /^https?:\/\/(\w+\.)?musicbrainz\.org\/search/
 // @run-at       document-end
 // ==/UserScript==
 "use strict";
-/* - --- - --- - --- - START OF CONFIGURATION - --- - --- - --- - */
-var onlyWhenNoReferrer = true; // for browser defined URL searches and duckduckgo MB !bangs like !mb !mbr !mblabel etc. only, for instance
-var redirOnUniqueMatch = true; // redirect when one result
-var redirOnUniqueExactMatch = true; // case insensitive, redirect when unique 100% scored result (both name and aliases) in several results
-var backgroundMarking = ["#ffc", "#ff6"]; // odd rows, even rows
-var textShadowMarking = "1px 2px 2px #cc6";
-/* - --- - --- - --- - END OF CONFIGURATION - --- - --- - --- - */
 if (document.getElementById("headerid-query")) {
+/* - --- - --- - --- - START OF CONFIGURATION - --- - --- - --- - */
+	var onlyWhenNoReferrer = true; // for browser defined URL searches and duckduckgo MB !bangs like !mb !mbr !mblabel etc. only, for instance
+	var redirOnUniqueMatch = true; // redirect when one result
+	var redirOnUniqueExactMatch = true; // case insensitive, redirect when unique 100% scored result (both name and aliases) in several results
+/* - --- - --- - --- - END OF CONFIGURATION - --- - --- - --- - */
+	var userjs = {
+		id: GM_info.script.name.replace(/\.\s/, "_").replace(/\s/g, "-")
+	};
+	var css = document.createElement("style");
+	css.setAttribute("type", "text/css");
+	document.head.appendChild(css);
+	css = css.sheet;
+	css.insertRule("tr." + userjs.id + " { text-shadow: 1px 2px 2px #cc6; }", 0);
+	css.insertRule("tr." + userjs.id + ".odd > td { background: #ffc; }", 0);
+	css.insertRule("tr." + userjs.id + ".even > td { background: #eeb !important; }", 0);
 	var rows = document.querySelector("div#content tbody");
 	if (rows) {
 		onlyWhenNoReferrer = !onlyWhenNoReferrer || (onlyWhenNoReferrer && (document.referrer == "" || document.referrer.match(/^https?:\/\/duckduckgo\.com/)));
 		rows = rows.getElementsByTagName("tr");
 		if (rows.length == 1 && redirOnUniqueMatch) {
-			mark(rows[0]);
+			rows[0].classList.add(userjs.id);
 			if (onlyWhenNoReferrer) {
 				go(rows[0].querySelector("a > bdi").parentNode.getAttribute("href"));
 			}
@@ -34,7 +45,7 @@ if (document.getElementById("headerid-query")) {
 			var exactMatchesCount = 0;
 			for (var i = 0; i < rows.length; i++) {
 				if (parseInt(rows[i].getAttribute("data-score"), 10) > 90) {
-					mark(rows[i]);
+					rows[i].classList.add(userjs.id);
 					if (exactMatchesCount++ == 0) {
 						exactMatchURL = rows[i].querySelector("a > bdi").parentNode.getAttribute("href");
 					}
@@ -46,15 +57,6 @@ if (document.getElementById("headerid-query")) {
 		}
 	}
 }
-function mark(row) {
-	if (row.className.indexOf("ev") != -1) {
-		row.className = "";
-		row.style.setProperty("background", backgroundMarking[1]);
-	} else {
-		row.style.setProperty("background", backgroundMarking[0]);
-	}
-	row.style.setProperty("text-shadow", textShadowMarking);
-}
 function go(url) {
-	setTimeout(function() { self.location.href = url; }, 0);
+	setTimeout(function() { self.location.href = url; }, 123);
 }
