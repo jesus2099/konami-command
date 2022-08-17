e63e84670e89b601da8b179199ab23acc25afba9
jesus2099 <jesus2099@users.noreply.github.com>
2022-08-18 00:59:31 +0200

    German fix by @kellnerd to a884ba60883935a41520f09583a9a6d230aec8d7
    Co-authored-by: David Kellner <52860029+kellnerd@users.noreply.github.com>

--- mb_POWER-VOTE.user.js
+++ mb_POWER-VOTE.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. POWER VOTE
-// @version      2022.8.18
+// @version      2022.8.18.55
 // @description  musicbrainz.org: Adds some buttons to check all unvoted edits (Yes/No/Abs/None) at once in the edit search page. You can also collapse/expand (all) edits for clarity. A handy reset votes button is also available + Double click radio to vote single edit + range click with shift to vote a series of edits., Hidden (collapsed) edits will never be voted (even if range click or shift+click force vote). Fast approve with edit notes. Prevent leaving voting page with unsaved changes. Add hyperlinks after inline looked up entity green fields.
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/mb_POWER-VOTE
@@ -58,7 +58,7 @@ var texts = {
 		edit: "Bearbeiten",
 		editing_history: "Bearbeitungshistorie",
 		open_edits: "Offene Bearbeitungen",
-		reset_votes: "Stimmenz urücksetzen",
+		reset_votes: "Stimmen zurücksetzen",
 		vote_all: " // Über alle nicht abgestimmten Änderungen abstimmen (shift+Klick für alle) → ",
 	},
 	en: {
