77a7bddea8e7680c1645e308de40af6b83f27d4e
jesus2099 <jesus2099@users.noreply.github.com>
2022-08-26 01:29:30 +0200

    ACEFINR

--- gph_TURBO.user.js
+++ gph_TURBO.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         gph. TURBO
-// @version      2022.8.26
+// @version      2022.8.26.127
 // @description  Display menu, crashed by tracking blockers
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/gph_TURBO
@@ -10,7 +10,7 @@
 // @licence      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @since        2022-08-26
 // @grant        none
-// @include      /^https?://gph?\.airfrance\.fr//
+// @include      /^https?://gph?\.[acefinr]{9}\.fr//
 // @run-at       document-ready
 // ==/UserScript==
 "use strict";
