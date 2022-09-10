ee4b00bdda1e4e89c1b86c5ab664751dc99c6201
jesus2099 <jesus2099@users.noreply.github.com>
2022-09-10 23:51:10 +0200

    Keep table bottom borders untouched #705

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2022.9.10
+// @version      2022.9.10.2351
 // @description  musicbrainz.org power-ups: RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @namespace    https://github.com/jesus2099/konami-command
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -741,7 +741,7 @@ if (j2sets.ROW_HIGHLIGHTER) {
 			background-image: linear-gradient(#f9f3, #f9f3);\
 		}\
 	");
-	j2superturbo.addCSSRule("div#content table.tbl > tbody > tr { border-bottom: 1px solid transparent; }");
+	j2superturbo.addCSSRule("div#content table.tbl > tbody > tr:not(:last-child) { border-bottom: 1px solid transparent; }");
 	j2superturbo.addCSSRule("div#content table.tbl > tbody > tr:hover { border-color: black; }");
 }
 // =================================================================== MOUSE+
