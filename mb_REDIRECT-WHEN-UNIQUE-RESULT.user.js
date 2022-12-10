7961eb2554839ef51023f1d134d7e91dc593f12e
jesus2099 <jesus2099@users.noreply.github.com>
2022-12-10 17:32:09 +0100

    Clearer Chrome detection #724

--- mb_REDIRECT-WHEN-UNIQUE-RESULT.user.js
+++ mb_REDIRECT-WHEN-UNIQUE-RESULT.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. REDIRECT WHEN UNIQUE RESULT
-// @version      2022.11.10
+// @version      2022.12.10
 // @description  Redirect to entity (release, artist, etc.) when only 1 result and/or unique 100% scored result of your entity search
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/mb_REDIRECT-WHEN-UNIQUE-RESULT
@@ -99,7 +99,7 @@ function redirect(entity) {
 			}
 		}
 	});
-	if (navigator.userAgent.match(/\bChrome\b/)) {
+	if (GM_info.platform.browserName == "chrome") {
 		// “History Skip” problem: https://bugs.chromium.org/p/chromium/issues/detail?id=907167
 		history.pushState({}, "", location);
 	}
