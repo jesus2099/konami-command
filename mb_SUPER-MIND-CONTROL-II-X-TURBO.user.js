58ced7ca335f6800749f800697c33a76547521a5
jesus2099 <jesus2099@users.noreply.github.com>
2022-12-14 16:06:25 +0100

    Accept funny punctuation from some comments (MASS SET WORKS’ RECORDING DATES)

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2022.12.10
+// @version      2022.12.14
 // @description  musicbrainz.org power-ups: RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @namespace    https://github.com/jesus2099/konami-command
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -53,7 +53,7 @@ var re_date = {
 	DD: "(0(?:1|2|3|4|5|6|7|8|9)|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)",
 	D: "(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)",
 };
-re_date.ISO = "(" + re_date.YYYY + "(?:-" + re_date.MM + "(?:-" + re_date.DD + ")?)?)";
+re_date.ISO = "(" + re_date.YYYY + "(?:[-‐‑‒–—−/.] ?" + re_date.MM + "(?:[-‐‑‒–—−/.] ?" + re_date.DD + ")?)?)";
 var account = document.querySelector("div.header ul.menu li.account");
 if (account) {
 	var a = account.querySelector("a[href^='/user/']");
