eb5afc5bf78ae2687a81bb6d5ff8f5918be495bf
jesus2099 <jesus2099@users.noreply.github.com>
2023-01-31 18:50:13 +0100

    Show full cell content on hover (fix)
    word-break was no enough on some pages like Gates

--- odigo-routing_TURBO-BOOST.user.js
+++ odigo-routing_TURBO-BOOST.user.js
@@ -53,7 +53,7 @@ document.body.addEventListener("mousedown", function(event) {
 // ENABLE CELL TEXT SELECTION
 css.insertRule("tbody div[unselectable='on'] { cursor: text; }", 0);
 css.insertRule(".x-unselectable { user-select: text; }", 0);
-css.insertRule("tbody td:hover div[unselectable='on'] { word-break: break-all; }", 0);
+css.insertRule("tbody td:hover div[unselectable='on'] { white-space: unset; word-break: break-all; }", 0);
 
 // SHOW CELL CROPPED TEXT TOOLTIPS
 document.body.addEventListener("mouseover", function(event) {
