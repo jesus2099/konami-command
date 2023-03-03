8afa700a0d1e925c6407ebc9e7f1a873d0fc47b2
jesus2099 <jesus2099@users.noreply.github.com>
2023-03-03 01:19:48 +0100

    Fix EASY_DATE copy in new relationship editor

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2023.2.19
+// @version      2023.3.3
 // @description  musicbrainz.org power-ups: RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / CHECK_ALL_SUBSCRIPTIONS / EASY_DATE. paste full dates in one go / STATIC_MENU / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @namespace    https://github.com/jesus2099/konami-command
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -617,7 +617,7 @@ function EASY_DATE_cloneDateHotkey(event) {
 function EASY_DATE_cloneDate(current, hotkey) {
 	var ph = ["YYYY", "MM", "DD"];
 	for (let p = 0; p < ph.length; p++) {
-		var inps = current.parentNode.parentNode.parentNode.querySelectorAll("input[placeholder='" + ph[p] + "']");
+		var inps = current.closest("table.relationship-details > tbody").querySelectorAll("input[placeholder='" + ph[p] + "']");
 		var downwards = (current.parentNode == inps[0].parentNode);
 		if (!hotkey && !downwards) {
 			return;
