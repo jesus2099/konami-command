jesus2099/konami-command

commit 7bc848bea84af6aa145bea59c6366d090dda511b

Use fancy loading screen
Remove nested condition, obsolete since f5db64991c5bfbb7fecd6ff4f6c2a360bfcfa1cc

voicepublisher_TURBO-BOOST.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         voicepublisher. TURBO BOOST
-// @version      2023.7.19.2050
+// @version      2023.7.20
 // @description  Work-around 1 bug; Scroll active folder into view; Make versions clickable in Applications (sites) page; Download audio folders as named zip files; Call Details improvements; Pagination intuitive scroll; Shortcut to application codes
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/voicepublisher_TURBO-BOOST
@@ -362,11 +362,9 @@ function go_to_application_codes() {
 		pre_production = pre_production ? pre_production.getAttribute("href").match(/svid=(\d+)/)[1] : "63113";
 		next_menu_item.parentNode.insertBefore(createTag("li", {a: {class: hash}}, createTag("a", {a: {href: "/pages/list?svid=" + pre_production + "?rand=" + Math.random().toString().replace(/\D/, "") + "#" + hash}}, texts.application_codes)), next_menu_item);
 	}
-	if (location.hash == "#" + hash) {
-		if (location.pathname == "/pages/list") {
-			document.body.style.setProperty("opacity", ".123", "important");
-			waitForElement("div#pages_table_filter span.dataTable-filter-clear", function(clear_button) { clear_button.click(); });
-			waitForElement("tr[id^='voicexml_page_'] > td.actions > a[rel='edit']", function(edit_button) { location.replace(edit_button.getAttribute("href") + "#" + hash); });
-		}
+	if (location.pathname == "/pages/list" && location.hash == "#" + hash) {
+		downloading();
+		waitForElement("div#pages_table_filter span.dataTable-filter-clear", function(clear_button) { clear_button.click(); });
+		waitForElement("tr[id^='voicexml_page_'] > td.actions > a[rel='edit']", function(edit_button) { edit_button.click(); });
 	}
 }

commit 87c7929eed96a5206e96bdf36eaba7a028ce6da3

Restore browser back feature after landing on Application codes
Back navigation was broken by previous 7bc848bea84af6aa145bea59c6366d090dda511b
Also fix English Styling for consistency with genuine texts

voicepublisher_TURBO-BOOST.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         voicepublisher. TURBO BOOST
-// @version      2023.7.20
-// @description  Work-around 1 bug; Scroll active folder into view; Make versions clickable in Applications (sites) page; Download audio folders as named zip files; Call Details improvements; Pagination intuitive scroll; Shortcut to application codes
+// @version      2023.7.21
+// @description  Work-around 1 bug; Scroll active folder into view; Make versions clickable in Applications (sites) page; Download audio folders as named zip files; Call Details improvements; Pagination intuitive scroll; Shortcut to Application Codes
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/voicepublisher_TURBO-BOOST
 // @downloadURL  https://github.com/jesus2099/konami-command/raw/master/voicepublisher_TURBO-BOOST.user.js
@@ -28,7 +28,7 @@ userjs.css = userjs.css.sheet;
 
 var texts = {
 	en: {
-		application_codes: "Application codes",
+		application_codes: "Application Codes",
 		copy: "Copy",
 		download: "Download ",
 		call_history_crash_bug: userjs.name + " fix to call History change app crash bug",
@@ -350,7 +350,7 @@ function all_time_complete_months_new_bad() {
 
 
 // -----------------------------
-// Shortcut to application codes
+// Shortcut to Application Codes
 // -----------------------------
 function go_to_application_codes() {
 	var hash = "application_codes";
@@ -365,6 +365,6 @@ function go_to_application_codes() {
 	if (location.pathname == "/pages/list" && location.hash == "#" + hash) {
 		downloading();
 		waitForElement("div#pages_table_filter span.dataTable-filter-clear", function(clear_button) { clear_button.click(); });
-		waitForElement("tr[id^='voicexml_page_'] > td.actions > a[rel='edit']", function(edit_button) { edit_button.click(); });
+		waitForElement("tr[id^='voicexml_page_'] > td.actions > a[rel='edit']", function(edit_button) { location.replace(edit_button.getAttribute("href")); });
 	}
 }