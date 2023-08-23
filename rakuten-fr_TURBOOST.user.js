bd89db48e6d406bace0e9fb5a5aa350e3853e6ac
jesus2099 <jesus2099@users.noreply.github.com>
2023-08-23 18:32:00 +0200

    rakuten-fr_TURBOOST
    Affiche toutes les annonces et totalement,
    sur le site Rakuten France (version Priceminister)

---
+++ rakuten-fr_TURBOOST.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         rakuten (fr). TURBOOST
+// @version      2023.8.23
+// @description  Affiche toutes les annonces et totalement, sur le site Rakuten France (version Priceminister)
+// @namespace    https://github.com/jesus2099/konami-command
+// @supportURL   https://github.com/jesus2099/konami-command/labels/rakuten-fr_TURBOOST
+// @downloadURL  https://github.com/jesus2099/konami-command/raw/master/rakuten-fr_TURBOOST.user.js
+// @author       jesus2099
+// @licence      CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/
+// @licence      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
+// @since        2023-08-22; 2022 as bookmarklet
+// @match        *://fr.shopping.rakuten.com/offer/buy/*/*.html
+// @run-at       document-idle
+// ==/UserScript==
+"use strict";
+var interval_delay = 500;
+var stop_after_x = 5 /* seconds */ * (1000 / interval_delay);
+var i = setInterval(function() {
+	var click_these = document.querySelectorAll(
+		"a.dispMore:not(.uiHide)#advertPaginationBtn,"
+		+ "div.linkCollaps > span.iconCollaps.svg-icon-chevron-down, "
+		+ "div.sellerComment > div#advertDescriptionShowPartial.edito:not([style='display: none;']) > span"
+	);
+	if (click_these.length > 0) {
+		for (var c = 0; c < click_these.length; c++) {
+			click_these[c].click();
+		}
+	} else {
+		if (--stop_after_x < 1) {
+			clearInterval(i);
+		}
+	}
+}, interval_delay);
