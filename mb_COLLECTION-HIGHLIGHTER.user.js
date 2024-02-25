jesus2099/konami-command

commit 02d476a0b724f55162e6bb03e177a74abf38c095

Normalise annotation links to allow matching
Annotation links can include protocol and host.
Reset them to relative path, like the rest of MBS.

Annotation links can include spaces, trim them.

  [ url | text ] generates <a href=" url "> text </a>
  [url|text] generates <a href="url">text</a>

I didn't find where is annotation rendering code
to fix it in @metabrainz/musicbrainz-server.

Fixes #430 but blocks annotation COOL-ENTITY-LINKS.

mb_COLLECTION-HIGHLIGHTER.user.js
@@ -2,7 +2,7 @@
 var meta = {raw: function() {
 // ==UserScript==
 // @name         mb. COLLECTION HIGHLIGHTER
-// @version      2020.9.28
+// @version      2020.10.18
 // @description  musicbrainz.org: Highlights releases, release-groups, etc. that you have in your collections (anyone’s collection can be loaded) everywhere
 // @compatible   vivaldi(2.4.1488.38)+violentmonkey  my setup (office)
 // @compatible   vivaldi(1.0.435.46)+violentmonkey   my setup (home, xp)
@@ -211,6 +211,10 @@ if (cat) {
 // #                                    COLLECT LINKS TO HIGHLIGHT / DECORATE #
 // ############################################################################
 		stuff = {};
+		// Annotation link trim spaces and protocol + "//" + host
+		for (var annotationLinks = document.querySelectorAll("div#content div.annotation a"), l = 0; l < annotationLinks.length; l++) {
+			annotationLinks[l].setAttribute("href", annotationLinks[l].getAttribute("href").trim().replace(/^((https?:)?\/\/(\w+\.mbsandbox|(\w+\.)?musicbrainz)\.org)\//, "/"));
+		}
 		for (var stu = 0; stu < collectedStuff.length; stu++) {
 			var cstuff = collectedStuff[stu];
 			stuff[cstuff] = {};

commit 0c572c60e59306f0a6f4f96da3f8fb8bf75058e3

Leave wiki links in annotations intact
Only strip MB main, beta and test domains from link href attributes.
Removing the domain for all subdomains broke wiki.musicbrainz.org links.
Bug was introduced in 02d476a0b724f55162e6bb03e177a74abf38c095.

mb_COLLECTION-HIGHLIGHTER.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. COLLECTION HIGHLIGHTER
-// @version      2023.9.29
+// @version      2024.2.25
 // @description  musicbrainz.org: Highlights releases, release-groups, etc. that you have in your collections (anyone’s collection can be loaded) everywhere
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/mb_COLLECTION-HIGHLIGHTER
@@ -240,7 +240,7 @@ if (cat) {
 function findOwnedStuff() {
 	// Annotation link trim spaces and protocol + "//" + host
 	for (let annotationLinks = document.querySelectorAll("div#content div.annotation a, div#content div.annotation-preview a"), l = 0; l < annotationLinks.length; l++) {
-		annotationLinks[l].setAttribute("href", annotationLinks[l].getAttribute("href").trim().replace(/^((https?:)?\/\/(\w+\.)?musicbrainz\.org)\//, "/"));
+		annotationLinks[l].setAttribute("href", annotationLinks[l].getAttribute("href").trim().replace(/^((https?:)?\/\/((beta|test)\.)?musicbrainz\.org)\//, "/"));
 	}
 	for (let stu = 0; stu < collectedStuff.length; stu++) {
 		var cstuff = collectedStuff[stu];