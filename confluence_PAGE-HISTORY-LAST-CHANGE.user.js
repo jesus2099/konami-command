d6de197e93093273b5cb917355ee7ebc4fd7d663
jesus2099 <jesus2099@users.noreply.github.com>
2024-05-24 15:03:37 +0200

    Migrate to Atlassian Cloud version Confluence

--- confluence_PAGE-HISTORY-LAST-CHANGE.user.js
+++ confluence_PAGE-HISTORY-LAST-CHANGE.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         confluence. PAGE HISTORY LAST CHANGE
-// @version      2016.4.8.2099
-// @description  Atlassian Confluence wiki: pre-selects last change for one click comparison
+// @version      2024.5.24
+// @description  Atlassian Confluence Cloud wiki: pre-selects last change for one click comparison
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/confluence_PAGE-HISTORY-LAST-CHANGE
 // @downloadURL  https://github.com/jesus2099/konami-command/raw/master/confluence_PAGE-HISTORY-LAST-CHANGE.user.js
@@ -10,15 +10,34 @@
 // @licence      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @since        2016-04-08
 // @icon         data:image/gif;base64,R0lGODlhEAAQAMIDAAAAAIAAAP8AAP///////////////////yH5BAEKAAQALAAAAAAQABAAAAMuSLrc/jA+QBUFM2iqA2ZAMAiCNpafFZAs64Fr66aqjGbtC4WkHoU+SUVCLBohCQA7
+// @require      https://github.com/jesus2099/konami-command/raw/7df89dd5adce162c1a6b133f9bc3f0dda4a82b33/lib/SUPER.js?version=2023.3.23
 // @grant        none
-// @include      */pages/viewpreviousversions.action?pageId=*
+// @match        *://*.atlassian.net/wiki/spaces/*/history/*/*
 // @run-at       document-end
 // ==/UserScript==
 "use strict";
-if (self.location.pathname.match(/^\/pages\/viewpreviousversions.action$/) && self.location.search.match(/^\?pageId=\d+$/)) {
-	var versions = document.querySelectorAll("table#page-history-container input[type='checkbox'][name='selectedPageVersions']");
-	if (versions.length > 1) {
-		versions[0].checked = true;
-		versions[1].checked = true;
+
+if (location.pathname.match(/^\/wiki\/spaces\/[^/]+\/history\/\d+\//)) {
+	wait_for_elements([
+		"tr:nth-of-type(1) > td input[type='checkbox']",
+		"tr:nth-of-type(2) > td input[type='checkbox']"
+	], function(checkboxes) {
+		checkboxes[0].click();
+		checkboxes[1].click();
+	});
+}
+
+function wait_for_elements(elements, callback) {
+	var ready_elements = [];
+	for (var e = 0; e < elements.length; e++) {
+		waitForElement(elements[e], function(element) {
+			ready_elements.push(element);
+		});
 	}
+	var wait_for_elements_interval_id = setInterval(function() {
+		if (ready_elements.length === elements.length) {
+			clearInterval(wait_for_elements_interval_id);
+			callback(ready_elements);
+		}
+	}, 666);
 }
