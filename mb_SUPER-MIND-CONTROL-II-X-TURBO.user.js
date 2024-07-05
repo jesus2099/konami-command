b530157c5ea3a3a3693a91bd868246f172d9f075
jesus2099 <jesus2099@users.noreply.github.com>
2024-07-06 00:00:22 +0200

    Use UTC methods for the sake of consistency
    Here, as there is no time operations,
    there were no bugs like
    "All tracks show additional 30 minutes"
    mb_MASS-MERGE-RECORDINGS bug #34
    
    This change has no effect (ninja)

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2024.6.29
+// @version      2024.7.5
 // @description  musicbrainz.org power-ups: RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected works date / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / EASY_DATE. paste full dates in one go / STATIC_MENU / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @namespace    https://github.com/jesus2099/konami-command
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -1643,10 +1643,10 @@ function TRACKLIST_TOOLS_buttonHandler(event) {
 							if (!erase) {
 								var date = new Date();
 								time = times[t].match(new RegExp(durationParser));
-								date.setHours(time[1] || time[4] || 0);
-								date.setMinutes(time[2] || time[5] || 0);
-								date.setSeconds(time[3] || time[6]);
-								time = (date.getHours() * 60 + date.getMinutes()) + ":" + (date.getSeconds() < 10 ? "0" : "") + date.getSeconds();
+								date.setUTCHours(time[1] || time[4] || 0);
+								date.setUTCMinutes(time[2] || time[5] || 0);
+								date.setUTCSeconds(time[3] || time[6]);
+								time = (date.getUTCHours() * 60 + date.getUTCMinutes()) + ":" + (date.getUTCSeconds() < 10 ? "0" : "") + date.getUTCSeconds();
 							}
 							inputs[i].style.removeProperty("background-color");
 							if (inputs[i].value != time) {
