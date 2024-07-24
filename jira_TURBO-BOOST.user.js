f2469d41f6578dd1b668c1e769b7f5f456a43473
jesus2099 <jesus2099@users.noreply.github.com>
2024-07-24 10:59:02 +0200

    ☠ OBSOLETE ☠ Jira scripts will be deleted soon
    I will not update my Jira scripts any more.
    Jira is going Cloud, which is completely different stuff.
    
    - jira. MOBILE VIEW TOGGLE SWITCH
    - jira. TURBO BOOST

--- jira_TURBO-BOOST.user.js
+++ jira_TURBO-BOOST.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jira. TURBO BOOST
-// @version      2024.5.16
-// @description  Fast toggle/switch quick filters (Ctrl for native additive behaviour)
+// @version      2024.7.24
+// @description  ☠ OBSOLETE ☠ Fast toggle/switch quick filters (Ctrl for native additive behaviour)
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/jira_TURBO-BOOST
 // @downloadURL  https://github.com/jesus2099/konami-command/raw/master/jira_TURBO-BOOST.user.js
@@ -14,24 +14,4 @@
 // @run-at       document-end
 // ==/UserScript==
 "use strict";
-if (document.body.matches("body#jira")) {
-	var css = document.createElement("style");
-	css.setAttribute("type", "text/css");
-	document.head.appendChild(css);
-	css = css.sheet;
-	css.insertRule("div#content main#main dl[id^='js-'][id$='-quickfilters']:after { content: ' (click to switch / Ctrl to add) '; background-color: #ffc; }", 0);
-	css.insertRule("div#content main#main dl[id^='js-'][id$='-quickfilters']:not([aria-expanded='true']):hover { height: initial; padding-right: initial !important; background-color: #fef; }", 0);
-	css.insertRule("div#content main#main dl[id^='js-'][id$='-quickfilters']:not([aria-expanded='true']):hover > dd.ghx-quickfilter-trigger { display: none; }", 0);
-	document.body.addEventListener("click", function(event) {
-		if (!event.ctrlKey && !event.shiftKey && event.target.matches("div#content main#main dl[id^='js-'][id$='-quickfilters'] > dd > a.js-quickfilter-button[data-filter-id].ghx-active")) {
-			var active_quick_filters = document.querySelectorAll("div#content main#main dl[id^='js-'][id$='-quickfilters'] > dd > a.js-quickfilter-button[data-filter-id].ghx-active");
-			if (active_quick_filters.length > 1) {
-				for (var f = 0; f < active_quick_filters.length; f++) {
-					if (active_quick_filters[f] != event.target) {
-						active_quick_filters[f].click();
-					}
-				}
-			}
-		}
-	});
-}
+alert("Please uninstall my “jira. TURBO BOOST” userscript as I will not update my Jira scripts any more. Jira is going Cloud, which is completely different stuff.\njesus2099");
