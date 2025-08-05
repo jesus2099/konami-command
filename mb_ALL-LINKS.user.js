b11b4b73a5a3fb90f2be1ab473eeb786b90940e9
jesus2099 <jesus2099@users.noreply.github.com>
2025-08-06 01:08:29 +0200

    fix: Artist Latin script name defaults to artist name #244 #488

--- mb_ALL-LINKS.user.js
+++ mb_ALL-LINKS.user.js
@@ -1002,6 +1002,7 @@ function main() {
 					artistsortname = tokenValues["%artist-sort-name%"] = artistsortname ? artistsortname.textContent : artistname;
 					if (!artistname.match(nonLatinName)) {
 						tokenValues["%artist-family-name-first%"] = artistsortname;
+						tokenValues["%artist-latin-script-name%"] = artistname;
 					} else {
 						var tmpsn = artistsortname.split(",");
 						for (var isn = tmpsn.length - 1; isn >= 0; isn--) {
