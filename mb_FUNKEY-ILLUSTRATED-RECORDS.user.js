6b4e0691dd1cd09f9311531ffad94b1bd3b3f077
jesus2099 <jesus2099@users.noreply.github.com>
2025-11-05 19:27:43 +0100

    fix: large cover art display is borked on edits searches #936
    Revert 3a59b8c35c8f2339626d587e5c5fc88d56d4e20c
    and f886e4a6d1d2ac3dbeb1902508df3e603c3d1f80
    There are too many different edit detail styleq
    And multiplied by two (edit list vs edit page)

--- mb_FUNKEY-ILLUSTRATED-RECORDS.user.js
+++ mb_FUNKEY-ILLUSTRATED-RECORDS.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. FUNKEY ILLUSTRATED RECORDS
-// @version      2025.11.3
+// @version      2025.11.5
 // @description  musicbrainz.org: CAA front cover art archive pictures/images (release groups and releases) Big illustrated discography and/or inline everywhere possible without cluttering the pages
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/mb_FUNKEY-ILLUSTRATED-RECORDS
@@ -155,7 +155,7 @@ setTimeout(function() {
 				if (pics_settings.big && (box = box.previousSibling && box.previousSibling.tagName == "DIV" && box.previousSibling.classList.contains(userjs + "bigbox") ? box.previousSibling : box.parentNode.insertBefore(createTag("div", {a: {class: userjs + "bigbox"}}), box))) {
 					var artisttd = artistcol && getSibling(getParent(as[a], "td"), "td");
 					// textContent is faster but shows <script> content. artisttd contains React? <script> when pending AC edits. https://kellegous.com/j/2013/02/27/innertext-vs-textcontent/
-					var bigpic = box.appendChild(createTag("a", {a: {href: as[a].getAttribute("href"), title: as[a].textContent + (artisttd ? "\n" + artisttd.innerText.trim() : "")}, s: {display: "inline-block", height: "100%", margin: "8px 8px 4px 4px"}}, [
+					box.appendChild(createTag("a", {a: {href: as[a].getAttribute("href"), title: as[a].textContent + (artisttd ? "\n" + artisttd.innerText.trim() : "")}, s: {display: "inline-block", height: "100%", margin: "8px 8px 4px 4px"}}, [
 						"⌛",
 						createTag("img", {
 							a: {src: imgurl + "-250", alt: as[a].textContent},
@@ -168,11 +168,6 @@ setTimeout(function() {
 							}
 						})
 					]));
-					if (as[a].closest(".details") && !as[a].closest(".details .tbl")) {
-						// Prevent edit list display jumps
-						bigpic.style.setProperty("float", "left");
-						bigpic.style.setProperty("margin-right", "-142px");
-					}
 				}
 			}
 		}
