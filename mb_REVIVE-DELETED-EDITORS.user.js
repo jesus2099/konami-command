aa62bf9905fe24308ccb92ccd7bdac49cbd9dc0d
chaban <225617645+chaban-mb@users.noreply.github.com>
2025-11-08 21:43:42 +0100

    Update version number to 2025.11.8

--- mb_REVIVE-DELETED-EDITORS.user.js
+++ mb_REVIVE-DELETED-EDITORS.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. REVIVE DELETED EDITORS
-// @version      2025.10.22
+// @version      2025.11.8
 // @description  musicbrainz.org: reveal deleted editors’ names and emphasizes your own name to standout in MB pages
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/mb_REVIVE-DELETED-EDITORS
