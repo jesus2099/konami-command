a5705be217c4f2dc91efd5d9f7f84624e1644476
jesus2099 <jesus2099@users.noreply.github.com>
2025-11-20 19:45:30 +0100

    fix: Don’t clone date outside recording, work and release relationship edit pages #937

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2025.8.28
+// @version      2025.11.20
 // @description  musicbrainz.org power-ups: RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected recording dates / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / EASY_DATE. paste full dates in one go / STATIC_MENU / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @namespace    https://github.com/jesus2099/konami-command
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -562,7 +562,10 @@ if (j2sets.EASY_DATE && !location.pathname.match(/^\/account\/edit/)) {
 					forceValue(input, date[i]);
 					input.style.setProperty("background-color", "#cfc");
 				}
-				if (event.target.matches("input:not([name^='edit-'])[name*='.begin_date.'], input:not([name^='edit-'])[name*='.end_date.']")) {
+				if (
+					location.pathname.match(/^\/(recording|work|release)\/.+\/edit(-relationships)?$/)
+					&& event.target.matches("input:not([name^='edit-'])[name*='.begin_date.'], input:not([name^='edit-'])[name*='.end_date.']")
+				) {
 					EASY_DATE_cloneDate(event.target);
 				}
 				stop(event);
