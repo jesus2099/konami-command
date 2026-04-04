ff3895ae7c2804bff777d7f7fdecf7f9dd7bb3dd
jesus2099 <jesus2099@users.noreply.github.com>
2026-04-04 17:43:16 +0200

    fix: DOUBLE_CLICK_SUBMIT can accidentally submit unfinished release add #953

--- mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
+++ mb_SUPER-MIND-CONTROL-II-X-TURBO.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. SUPER MIND CONTROL Ⅱ X TURBO
-// @version      2026.1.9
+// @version      2026.4.4
 // @description  musicbrainz.org power-ups: RELEASE_CLONER. copy/paste releases / DOUBLE_CLICK_SUBMIT / CONTROL_ENTER_SUBMIT / TRACKLIST_TOOLS. search→replace, track length parser, remove recording relationships, set selected recording dates / LAST_SEEN_EDIT. handy for subscribed entities / COOL_SEARCH_LINKS / COPY_TOC / ROW_HIGHLIGHTER / SPOT_CAA / SPOT_AC / RECORDING_LENGTH_COLUMN / RELEASE_EVENT_COLUMN / WARN_NEW_WINDOW / SERVER_SWITCH / TAG_TOOLS / USER_STATS / EASY_DATE. paste full dates in one go / STATIC_MENU / SLOW_DOWN_RETRY / CENTER_FLAGS / RATINGS_ON_TOP / HIDE_RATINGS / UNLINK_ENTITY_HEADER / MARK_PENDING_EDIT_MEDIUMS
 // @namespace    https://github.com/jesus2099/konami-command
 // @homepage     https://github.com/jesus2099/konami-command/blob/master/mb_SUPER-MIND-CONTROL-II-X-TURBO.md
@@ -753,6 +753,11 @@ if (j2sets.DOUBLE_CLICK_SUBMIT && location.pathname.match(/^\/(cdtoc\/|cdstub\/|
 		if (
 			event.target.tagName
 			&& !getParent(event.target, "div", "edit-list") // mb_POWER-VOTE has its own DOUBLE_CLICK_SUBMIT
+			&& !(
+					userjs.releaseEditor // release editor should not be submitted from random tabs
+					|| location.pathname.match(new RegExp("^\/release\/(add|" + stre_GUID + "\/edit)$")) // in case above wasn’t detected
+					|| event.target.closest(".ui-tabs > .ui-tabs-panel") // multitab editors (currently only release editor)
+				)
 			&& (
 				event.target.tagName == "INPUT"
 				&& event.target.getAttribute("type") == "radio"
