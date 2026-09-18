422af2db15a7d7849e50e26a1ff985d43fdf2b89
jesus2099 <jesus2099@users.noreply.github.com>
2026-09-18 21:53:17 +0200

    fix: GnuDb search query stays on previous search #975

--- mb_ALL-LINKS.user.js
+++ mb_ALL-LINKS.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         mb. ALL LINKS
-// @version      2026.9.8
+// @version      2026.9.18
 // @description  Hidden links include fanpage, social network, etc. (NO duplicates) Generated autolinks (configurable) includes plain web search, auto last.fm, Discogs and lyrics searches, etc. Shows begin/end dates on URL and provides edit link. Expands Wikidata links to wikipedia articles.
 // @namespace    https://github.com/jesus2099/konami-command
 // @supportURL   https://github.com/jesus2099/konami-command/labels/mb_ALL-LINKS
@@ -760,20 +760,19 @@ var additionalSearchLinks = {
 				nl: "Andere databases"
 			},
 			items: {
-				GnuDb: [ // new syntax but now GnuDb is almost unusable because of AI attacks
-					"//gnudb.org/search.php?action=search&words=%22%artist-name%%22&mode=artist",
-					"//gnudb.org/search.php?action=search&words=%22%release-artist-credit%%22%20%22%release-name%%22",
-					"//gnudb.org/search.php?action=search&words=%22%release-group-artist-credit%%22%20%22%release-group-name%%22",
-					"//gnudb.org/search.php?action=search&words=%22%recording-name%%22&mode=song",
-					"//gnudb.org/search.php?action=search&words=%22%work-name%%22&mode=song",
+				GnuDb: [ // new syntax but now GnuDb is almost not responsive at all because of AI attacks
+					"https://jesus2099.gitlab.io/forward-request.html?_method=post&_action=https://gnudb.org/search.php&action=search&words=%22%artist-name%%22&mode=artist",
+					"https://jesus2099.gitlab.io/forward-request.html?_method=post&_action=https://gnudb.org/search.php&action=search&words=%22%release-artist-credit%%22%20%22%release-name%%22",
+					"https://jesus2099.gitlab.io/forward-request.html?_method=post&_action=https://gnudb.org/search.php&action=search&words=%22%release-group-artist-credit%%22%20%22%release-group-name%%22",
+					"https://jesus2099.gitlab.io/forward-request.html?_method=post&_action=https://gnudb.org/search.php&action=search&words=%22%recording-name%%22&mode=song",
+					"https://jesus2099.gitlab.io/forward-request.html?_method=post&_action=https://gnudb.org/search.php&action=search&words=%22%work-name%%22&mode=song",
 				],
 				"GnuDb (just title)": [
-					"//gnudb.org/search.php?action=search&words=%22%release-name%%22&mode=album",
-					"//gnudb.org/search.php?action=search&words=%22%release-group-name%%22&mode=album",
-					// "http://jesus2099.gitlab.io/forward-request.html?_action=https://gnudb.org/search.php&action=search&words=%22%release-group-name%%22&mode=album",
+					"https://jesus2099.gitlab.io/forward-request.html?_method=post&_action=https://gnudb.org/search.php&action=search&words=%22%release-name%%22&mode=album",
+					"https://jesus2099.gitlab.io/forward-request.html?_method=post&_action=https://gnudb.org/search.php&action=search&words=%22%release-group-name%%22&mode=album",
 				],
 				"GnuDb (abc)": [
-					"//gnudb.org/search.php?action=search&words=%22%artist-latin-script-name%%22&mode=artist",
+					"https://jesus2099.gitlab.io/forward-request.html?_method=post&_action=https://gnudb.org/search.php&action=search&words=%22%artist-latin-script-name%%22&mode=artist",
 				],
 			}
 		}
